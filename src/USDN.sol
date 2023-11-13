5118fa800c72edae483d74526e06a5c0ec265a6f
beeb <703631+beeb@users.noreply.github.com>
2023-11-13 17:03:56 +0100

    docs: natspec

--- src/USDN.sol
+++ src/USDN.sol
@@ -32,6 +32,8 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
 
     mapping(address account => mapping(address spender => uint256)) private _allowances;
 
+    uint8 public constant decimals = 18;
+
     uint256 private _totalShares;
     uint256 private _multiplier = 1e18;
     uint256 private constant MULTIPLIER_DIVISOR = 1e18;
@@ -51,50 +53,100 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         }
     }
 
+    /**
+     * @notice Name of the token.
+     * @return name the name string
+     */
     function name() public view returns (string memory) {
         return _name;
     }
 
+    /**
+     * @notice Symbol of the token.
+     * @return symbol the symbol string
+     */
     function symbol() public view returns (string memory) {
         return _symbol;
     }
 
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
+    /**
+     * @notice Total supply of the token.
+     * @return totalSupply the total supply
+     */
     function totalSupply() public view returns (uint256) {
         return totalShares() * _multiplier / MULTIPLIER_DIVISOR;
     }
 
+    /**
+     * @notice Balance of the token for a given account.
+     * @param account the account to query
+     * @return balance the balance
+     */
     function balanceOf(address account) public view returns (uint256) {
         return sharesOf(account) * _multiplier / MULTIPLIER_DIVISOR;
     }
 
+    /// @inheritdoc IUSDN
     function totalShares() public view returns (uint256) {
         return _totalShares;
     }
 
+    /// @inheritdoc IUSDN
     function sharesOf(address account) public view returns (uint256) {
         return _shares[account];
     }
 
+    /**
+     * @notice Move a `value` amount of tokens from the caller to `to`.
+     * @dev Emits a {Transfer} event.
+     * @param to the destination address
+     * @param value the amount of tokens to send, is internally converted to shares
+     */
     function transfer(address to, uint256 value) public returns (bool) {
         address owner = _msgSender();
         _transfer(owner, to, value);
         return true;
     }
 
+    /**
+     * @notice Get the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner` through
+     * `transferFrom`. This is zero by default.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @return allowance the remaining allowance
+     */
     function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
     }
 
+    /**
+     * @notice Set `value` as the allowance of `spender` over the caller's tokens.
+     * @dev IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @dev Emits an {Approval} event.
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow, is internally converted to shares
+     * @return success true if the operation succeeded
+     */
     function approve(address spender, uint256 value) public returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, value);
         return true;
     }
 
+    /**
+     * @notice Move a `value` amount of tokens from `from` to `to` using the allowance mechanism. `value` is then
+     * deducted from the caller's allowance.
+     * @dev Emits a {Transfer} event.
+     * @param from the source address
+     * @param to the destination address
+     * @param value the amount of tokens to send, is internally converted to shares
+     * @return success true if the operation succeeded
+     */
     function transferFrom(address from, address to, uint256 value) public returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, value);
@@ -102,6 +154,13 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         return true;
     }
 
+    /**
+     * @dev Move a `value` amount of tokens from `from` to `to`.
+     * @dev Emits a {Transfer} event.
+     * @param from the source address
+     * @param to the destination address
+     * @param value the amount of tokens to send, is internally converted to shares
+     */
     function _transfer(address from, address to, uint256 value) internal {
         if (from == address(0)) {
             revert ERC20InvalidSender(address(0));
@@ -113,6 +172,14 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         _update(from, to, value);
     }
 
+    /**
+     * @dev Transfer a `value` amount of tokens from `from` to `to`, or alternatively mint (or burn) if `from` or `to`
+     * is the zero address.
+     * @dev Emits a {Transfer} event.
+     * @param from the source address
+     * @param to the destination address
+     * @param value the amount of tokens to transfer, is internally converted to shares
+     */
     function _update(address from, address to, uint256 value) internal {
         uint256 _sharesValue = value * MULTIPLIER_DIVISOR / _multiplier;
         if (from == address(0)) {
@@ -144,6 +211,12 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         emit Transfer(from, to, value);
     }
 
+    /**
+     * @dev Create a `value` amount of tokens and assign them to `account`, by transferring it from the zero address.
+     * @dev Emits a {Transfer} event with the zero address as `from`.
+     * @param account the account to receive the tokens
+     * @param value the amount of tokens to mint, is internally converted to shares
+     */
     function _mint(address account, uint256 value) internal {
         if (account == address(0)) {
             revert ERC20InvalidReceiver(address(0));
@@ -151,6 +224,13 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         _update(address(0), account, value);
     }
 
+    /**
+     * @dev Destroy a `value` amount of tokens from `account`, by transferring it to the zero address, lowering the
+     * total supply.
+     * @dev Emits a {Transfer} event with the zero address as `to`.
+     * @param account the account to burn the tokens from
+     * @param value the amount of tokens to burn, is internally converted to shares
+     */
     function _burn(address account, uint256 value) internal {
         if (account == address(0)) {
             revert ERC20InvalidSender(address(0));
@@ -158,10 +238,26 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         _update(account, address(0), value);
     }
 
+    /**
+     * @dev Set `value` as the allowance of `spender` over the `owner`'s tokens.
+     * @dev Emits an {Approval} event.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow
+     */
     function _approve(address owner, address spender, uint256 value) internal {
         _approve(owner, spender, value, true);
     }
 
+    /**
+     * @dev Variant of {_approve} with an optional flag to enable or disable the emission of the {Approval} event.
+     * Used without event emission in {_spendAllowance} and {_transferFrom}.
+     * @dev Emits an {Approval} event if `emitEvent` is true.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow
+     * @param emitEvent whether to emit the {Approval} event
+     */
     function _approve(address owner, address spender, uint256 value, bool emitEvent) internal {
         if (owner == address(0)) {
             revert ERC20InvalidApprover(address(0));
@@ -175,6 +271,15 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         }
     }
 
+    /**
+     * @dev Update allowance of `owner` to `spender`, based on spent `value`.
+     *
+     * Does not update the allowance value in case of infinite allowance.
+     * Revert if not enough allowance is available.
+     * @param owner the account that owns the tokens
+     * @param spender the account that spent the tokens
+     * @param value the amount of tokens spent
+     */
     function _spendAllowance(address owner, address spender, uint256 value) internal {
         uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
@@ -187,15 +292,44 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         }
     }
 
+    /**
+     * @notice Destroy a `value` amount of tokens from the caller, lowering the total supply.
+     * @dev Emits a {Transfer} event with the zero address as `to`.
+     * @param value the amount of tokens to burn, is internally converted to shares
+     */
     function burn(uint256 value) public {
         _burn(_msgSender(), value);
     }
 
+    /**
+     * @notice Destroy a `value` amount of tokens from `account`, deducting from the caller's allowance, lowering the
+     * total supply.
+     * @param account the account to burn the tokens from
+     * @param value the amount of tokens to burn, is internally converted to shares
+     */
     function burnFrom(address account, uint256 value) public {
         _spendAllowance(account, _msgSender(), value);
         _burn(account, value);
     }
 
+    /**
+     * @notice Set `value` as the allowance of `spender` over `owner`'s tokens, given `owner`'s signed approval.
+     * @dev IMPORTANT: See {approve} for concerns about transaction ordering.
+     *
+     * Requirements:
+     *  - `spender` cannot be the zero address.
+     *  - `deadline` must be a timestamp in the future.
+     *  - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner` over the EIP712-formatted function args.
+     *  - the signature must use `owner`'s current nonce (see {nonces})
+     * @dev Emits an {Approval} event.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow
+     * @param deadline the deadline timestamp, type(uint256).max for no deadline
+     * @param v v of the signature
+     * @param r r of the signature
+     * @param s s of the signature
+     */
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
         public
     {
@@ -215,18 +349,33 @@ contract USDN is IUSDN, Context, IERC20, IERC20Metadata, IERC20Errors, AccessCon
         _approve(owner, spender, value);
     }
 
+    /**
+     * @notice Return the current nonce for `owner`. This value must be included whenever a signature is generated for
+     * {permit}.
+     *
+     * Every successful call to {permit} increases the nonce of `owner` by one. This prevents a signature from being
+     * used multiple times.
+     * @param owner the account to query
+     * @return nonce the current nonce for `owner`
+     */
     function nonces(address owner) public view override(IERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 
+    /**
+     * @dev The domain separator used in the encoding of the signature for {permit}, as defined by EIP-712.
+     * @return domainSeparator the domain separator
+     */
     function DOMAIN_SEPARATOR() external view returns (bytes32) {
         return _domainSeparatorV4();
     }
 
+    /// @inheritdoc IUSDN
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
+    /// @inheritdoc IUSDN
     function adjustMultiplier(uint256 multiplier) public onlyRole(ADJUSTMENT_ROLE) {
         if (multiplier <= _multiplier) {
             // Multiplier can only be increased
--- src/interfaces/IUSDN.sol
+++ src/interfaces/IUSDN.sol
@@ -3,24 +3,69 @@ pragma solidity 0.8.20;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * @dev Interface for the functions of the USDN token which are not part of the ERC-20 standard.
+ */
 interface IUSDN {
+    /**
+     * @notice Emitted when the multiplier is adjusted.
+     * @param old_multiplier multiplier before adjustment
+     * @param new_multiplier multiplier after adjustment
+     */
     event MultiplierAdjusted(uint256 old_multiplier, uint256 new_multiplier);
 
+    /**
+     * @dev Indicates that the provided multiplier is invalid. This is usually because the new value is smaller or
+     * equal to the current multiplier.
+     * @param multiplier invalid multiplier
+     */
     error InvalidMultiplier(uint256 multiplier);
 
+    /**
+     * @dev Permit deadline has expired.
+     */
     error ERC2612ExpiredSignature(uint256 deadline);
 
+    /**
+     * @dev Mismatched signature.
+     */
     error ERC2612InvalidSigner(address signer, address owner);
 
-    function totalShares() external view returns (uint256);
+    /**
+     * @notice Total number of shares in existence.
+     * @return shares the number of shares
+     */
+    function totalShares() external view returns (uint256 shares);
 
-    function sharesOf(address account) external view returns (uint256);
+    /**
+     * @notice Number of shares owned by `account`.
+     * @param account the account to query
+     * @return shares the number of shares
+     */
+    function sharesOf(address account) external view returns (uint256 shares);
 
+    /**
+     * @notice Restricted function to mint new shares, providing a token value.
+     * @dev Caller must have the MINTER_ROLE.
+     * @param to account to receive the new shares
+     * @param amount amount of tokens to mint, is internally converted to the proper shares amounts
+     */
     function mint(address to, uint256 amount) external;
 
+    /**
+     * @notice Restricted function to increase the global multiplier, which effectively grows all balances and the total
+     * supply.
+     * @param multiplier the new multiplier, must be greater than the current one
+     */
     function adjustMultiplier(uint256 multiplier) external;
 
+    /**
+     * @dev Minter role signature.
+     */
     function MINTER_ROLE() external view returns (bytes32);
 
+    /**
+     * @dev Adjustment role signature.
+     */
     function ADJUSTMENT_ROLE() external view returns (bytes32);
 }
