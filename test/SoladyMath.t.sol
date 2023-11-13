aed353fcc3fb5f45c1cf265155e37f09cb0b161c
beeb <703631+beeb@users.noreply.github.com>
2023-11-13 10:46:38 +0100

    ci: revert change because FFI is not enabled in CI

--- test/SoladyMath.t.sol
+++ test/SoladyMath.t.sol
@@ -7,11 +7,11 @@ import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
 import { TickMath } from "src/libraries/TickMath.sol";
 
-contract TestTickMath is Test {
+contract TestSoladyMath is Test {
     function testFuzzExpWad(int256 value) public {
         value = bound(value, -42_139_678_854_452_767_552, 135_305_999_368_893_231_588);
         string[] memory cmds = new string[](3);
-        cmds[0] = "./test_utils/test_utils";
+        cmds[0] = "./test_utils/target/release/test_utils";
         cmds[1] = "exp-wad";
         cmds[2] = vm.toString(value);
         bytes memory result = vm.ffi(cmds);
@@ -23,7 +23,7 @@ contract TestTickMath is Test {
     function testFuzzLnWad(uint256 value) public {
         value = bound(value, TickMath.MIN_PRICE, TickMath.MAX_PRICE);
         string[] memory cmds = new string[](3);
-        cmds[0] = "./test_utils/test_utils";
+        cmds[0] = "./test_utils/target/release/test_utils";
         cmds[1] = "ln-wad";
         cmds[2] = vm.toString(value);
         bytes memory result = vm.ffi(cmds);
@@ -37,7 +37,7 @@ contract TestTickMath is Test {
         rhs = bound(rhs, 1, 10_000_000_000_000_000_000_000_000_000_000_000_000);
         vm.assume(rhs != 0);
         string[] memory cmds = new string[](4);
-        cmds[0] = "./test_utils/test_utils";
+        cmds[0] = "./test_utils/target/release/test_utils";
         cmds[1] = "div-up";
         cmds[2] = vm.toString(lhs);
         cmds[3] = vm.toString(rhs);
