9612607bf9dd504edc38c3209c101b46c1fd0d1a
beeb <703631+beeb@users.noreply.github.com>
2023-11-14 14:25:05 +0100

    test: calculate permit hash

--- test/utils/SigUtils.sol
+++ test/utils/SigUtils.sol
@@ -8,8 +8,8 @@ contract SigUtils {
         DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
     }
 
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     struct Permit {
         address owner;
