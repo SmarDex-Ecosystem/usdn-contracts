f3b87756ae0ebda898925adb10c9ab2e2b8d78a4
beeb <703631+beeb@users.noreply.github.com>
2023-11-14 14:13:28 +0100

    feat(token): add all functions to interface

--- src/USDN.sol
+++ src/USDN.sol
@@ -53,7 +53,7 @@ contract USDN is
     // Mapping of allowances by owner and spender. This is in token units, not shares.
     mapping(address account => mapping(address spender => uint256)) private _allowances;
 
-    uint8 public constant decimals = 18;
+    uint8 public constant override(IUSDN, IERC20Metadata) decimals = 18;
 
     uint256 private _totalShares;
     // Multiplier used to convert between shares and tokens. This is a fixed-point number with 18 decimals.
@@ -77,47 +77,28 @@ contract USDN is
     /*                            ERC-20 view functions                           */
     /* -------------------------------------------------------------------------- */
 
-    /**
-     * @notice Name of the token.
-     * @return name the name string
-     */
-    function name() public pure returns (string memory) {
+    /// @inheritdoc IUSDN
+    function name() external pure override(IUSDN, IERC20Metadata) returns (string memory) {
         return NAME;
     }
 
-    /**
-     * @notice Symbol of the token.
-     * @return symbol the symbol string
-     */
-    function symbol() public pure returns (string memory) {
+    /// @inheritdoc IUSDN
+    function symbol() external pure override(IUSDN, IERC20Metadata) returns (string memory) {
         return SYMBOL;
     }
 
-    /**
-     * @notice Total supply of the token.
-     * @return totalSupply the total supply
-     */
-    function totalSupply() public view returns (uint256) {
+    /// @inheritdoc IUSDN
+    function totalSupply() external view override(IUSDN, IERC20) returns (uint256) {
         return totalShares() * _multiplier / MULTIPLIER_DIVISOR;
     }
 
-    /**
-     * @notice Balance of the token for a given account.
-     * @param account the account to query
-     * @return balance the balance
-     */
-    function balanceOf(address account) public view returns (uint256) {
+    /// @inheritdoc IUSDN
+    function balanceOf(address account) public view override(IUSDN, IERC20) returns (uint256) {
         return sharesOf(account) * _multiplier / MULTIPLIER_DIVISOR;
     }
 
-    /**
-     * @notice Get the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner` through
-     * `transferFrom`. This is zero by default.
-     * @param owner the account that owns the tokens
-     * @param spender the account that will spend the tokens
-     * @return allowance the remaining allowance
-     */
-    function allowance(address owner, address spender) public view returns (uint256) {
+    /// @inheritdoc IUSDN
+    function allowance(address owner, address spender) public view override(IUSDN, IERC20) returns (uint256) {
         return _allowances[owner][spender];
     }
 
@@ -125,24 +106,13 @@ contract USDN is
     /*                            Permit view functions                           */
     /* -------------------------------------------------------------------------- */
 
-    /**
-     * @notice Return the current nonce for `owner`. This value must be included whenever a signature is generated for
-     * {permit}.
-     *
-     * Every successful call to {permit} increases the nonce of `owner` by one. This prevents a signature from being
-     * used multiple times.
-     * @param owner the account to query
-     * @return nonce the current nonce for `owner`
-     */
-    function nonces(address owner) public view override(IERC20Permit, Nonces) returns (uint256) {
+    /// @inheritdoc IUSDN
+    function nonces(address owner) public view override(IUSDN, IERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 
-    /**
-     * @dev The domain separator used in the encoding of the signature for {permit}, as defined by EIP-712.
-     * @return domainSeparator the domain separator
-     */
-    function DOMAIN_SEPARATOR() external view returns (bytes32) {
+    /// @inheritdoc IUSDN
+    function DOMAIN_SEPARATOR() external view override(IUSDN, IERC20Permit) returns (bytes32) {
         return _domainSeparatorV4();
     }
 
@@ -164,45 +134,22 @@ contract USDN is
     /*                              ERC-20 functions                              */
     /* -------------------------------------------------------------------------- */
 
-    /**
-     * @notice Set `value` as the allowance of `spender` over the caller's tokens.
-     * @dev IMPORTANT: Beware that changing an allowance with this method brings the risk that someone may use both the
-     * old and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     * @dev Emits an {Approval} event.
-     * @param spender the account that will spend the tokens
-     * @param value the amount of tokens to allow, is internally converted to shares
-     * @return success true if the operation succeeded
-     */
-    function approve(address spender, uint256 value) public returns (bool) {
+    /// @inheritdoc IUSDN
+    function approve(address spender, uint256 value) external override(IUSDN, IERC20) returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, value);
         return true;
     }
 
-    /**
-     * @notice Move a `value` amount of tokens from the caller to `to`.
-     * @dev Emits a {Transfer} event.
-     * @param to the destination address
-     * @param value the amount of tokens to send, is internally converted to shares
-     */
-    function transfer(address to, uint256 value) public returns (bool) {
+    /// @inheritdoc IUSDN
+    function transfer(address to, uint256 value) external override(IUSDN, IERC20) returns (bool) {
         address owner = _msgSender();
         _transfer(owner, to, value);
         return true;
     }
 
-    /**
-     * @notice Move a `value` amount of tokens from `from` to `to` using the allowance mechanism. `value` is then
-     * deducted from the caller's allowance.
-     * @dev Emits a {Transfer} event.
-     * @param from the source address
-     * @param to the destination address
-     * @param value the amount of tokens to send, is internally converted to shares
-     * @return success true if the operation succeeded
-     */
-    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+    /// @inheritdoc IUSDN
+    function transferFrom(address from, address to, uint256 value) external override(IUSDN, IERC20) returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, value);
         _transfer(from, to, value);
@@ -213,26 +160,10 @@ contract USDN is
     /*                                   Permit                                   */
     /* -------------------------------------------------------------------------- */
 
-    /**
-     * @notice Set `value` as the allowance of `spender` over `owner`'s tokens, given `owner`'s signed approval.
-     * @dev IMPORTANT: See {approve} for concerns about transaction ordering.
-     *
-     * Requirements:
-     *  - `spender` cannot be the zero address.
-     *  - `deadline` must be a timestamp in the future.
-     *  - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner` over the EIP712-formatted function args.
-     *  - the signature must use `owner`'s current nonce (see {nonces})
-     * @dev Emits an {Approval} event.
-     * @param owner the account that owns the tokens
-     * @param spender the account that will spend the tokens
-     * @param value the amount of tokens to allow
-     * @param deadline the deadline timestamp, type(uint256).max for no deadline
-     * @param v v of the signature
-     * @param r r of the signature
-     * @param s s of the signature
-     */
+    /// @inheritdoc IUSDN
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
-        public
+        external
+        override(IUSDN, IERC20Permit)
     {
         if (block.timestamp > deadline) {
             revert ERC2612ExpiredSignature(deadline);
--- src/interfaces/IUSDN.sol
+++ src/interfaces/IUSDN.sol
@@ -36,6 +36,57 @@ interface IUSDNErrors {
  */
 interface IUSDN {
     /**
+     * @notice Return the token decimals.
+     * @return decimals the number of decimals of the ERC-20 token.
+     */
+    function decimals() external pure returns (uint8 decimals);
+
+    /**
+     * @notice Name of the token.
+     * @return name the name string
+     */
+    function name() external pure returns (string memory name);
+
+    /**
+     * @notice Symbol of the token.
+     * @return symbol the symbol string
+     */
+    function symbol() external pure returns (string memory symbol);
+
+    /**
+     * @notice Total supply of the token.
+     * @return totalSupply the total supply
+     */
+    function totalSupply() external view returns (uint256 totalSupply);
+
+    /**
+     * @notice Balance of the token for a given account.
+     * @param account the account to query
+     * @return balance the balance
+     */
+    function balanceOf(address account) external view returns (uint256 balance);
+
+    /**
+     * @notice Get the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner` through
+     * `transferFrom`. This is zero by default.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @return allowance the remaining allowance
+     */
+    function allowance(address owner, address spender) external view returns (uint256 allowance);
+
+    /**
+     * @notice Return the current nonce for `owner`. This value must be included whenever a signature is generated for
+     * {permit}.
+     *
+     * Every successful call to {permit} increases the nonce of `owner` by one. This prevents a signature from being
+     * used multiple times.
+     * @param owner the account to query
+     * @return nonce the current nonce for `owner`
+     */
+    function nonces(address owner) external view returns (uint256 nonce);
+
+    /**
      * @notice Total number of shares in existence.
      * @return shares the number of shares
      */
@@ -49,12 +100,58 @@ interface IUSDN {
     function sharesOf(address account) external view returns (uint256 shares);
 
     /**
-     * @notice Restricted function to mint new shares, providing a token value.
-     * @dev Caller must have the MINTER_ROLE.
-     * @param to account to receive the new shares
-     * @param amount amount of tokens to mint, is internally converted to the proper shares amounts
+     * @notice Set `value` as the allowance of `spender` over the caller's tokens.
+     * @dev IMPORTANT: Beware that changing an allowance with this method brings the risk that someone may use both the
+     * old and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @dev Emits an {Approval} event.
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow, is internally converted to shares
+     * @return success true if the operation succeeded
      */
-    function mint(address to, uint256 amount) external;
+    function approve(address spender, uint256 value) external returns (bool success);
+
+    /**
+     * @notice Move a `value` amount of tokens from the caller to `to`.
+     * @dev Emits a {Transfer} event.
+     * @param to the destination address
+     * @param value the amount of tokens to send, is internally converted to shares
+     * @return success true if the operation succeeded
+     */
+    function transfer(address to, uint256 value) external returns (bool success);
+
+    /**
+     * @notice Move a `value` amount of tokens from `from` to `to` using the allowance mechanism. `value` is then
+     * deducted from the caller's allowance.
+     * @dev Emits a {Transfer} event.
+     * @param from the source address
+     * @param to the destination address
+     * @param value the amount of tokens to send, is internally converted to shares
+     * @return success true if the operation succeeded
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool success);
+
+    /**
+     * @notice Set `value` as the allowance of `spender` over `owner`'s tokens, given `owner`'s signed approval.
+     * @dev IMPORTANT: See {approve} for concerns about transaction ordering.
+     *
+     * Requirements:
+     *  - `spender` cannot be the zero address.
+     *  - `deadline` must be a timestamp in the future.
+     *  - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner` over the EIP712-formatted function args.
+     *  - the signature must use `owner`'s current nonce (see {nonces})
+     * @dev Emits an {Approval} event.
+     * @param owner the account that owns the tokens
+     * @param spender the account that will spend the tokens
+     * @param value the amount of tokens to allow
+     * @param deadline the deadline timestamp, type(uint256).max for no deadline
+     * @param v v of the signature
+     * @param r r of the signature
+     * @param s s of the signature
+     */
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
 
     /**
      * @notice Destroy a `value` amount of tokens from the caller, lowering the total supply.
@@ -72,11 +169,25 @@ interface IUSDN {
     function burnFrom(address account, uint256 value) external;
 
     /**
+     * @notice Restricted function to mint new shares, providing a token value.
+     * @dev Caller must have the MINTER_ROLE.
+     * @param to account to receive the new shares
+     * @param amount amount of tokens to mint, is internally converted to the proper shares amounts
+     */
+    function mint(address to, uint256 amount) external;
+
+    /**
      * @notice Restricted function to increase the global multiplier, which effectively grows all balances and the total
      * supply.
      * @param multiplier the new multiplier, must be greater than the current one
      */
     function adjustMultiplier(uint256 multiplier) external;
+
+    /**
+     * @dev The domain separator used in the encoding of the signature for {permit}, as defined by EIP-712.
+     * @return domainSeparator the domain separator
+     */
+    function DOMAIN_SEPARATOR() external view returns (bytes32 domainSeparator);
 
     /**
      * @dev Minter role signature.
