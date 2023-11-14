db33b13c0a4470f8fbabd62ca50904999de46ce9
beeb <703631+beeb@users.noreply.github.com>
2023-11-14 10:14:55 +0100

    test(token): burn

--- test/USDN.t.sol
+++ test/USDN.t.sol
@@ -88,12 +88,12 @@ contract TestUSDNBurn is USDNTokenFixture {
 
     function setUp() public override {
         super.setUp();
-    }
-
-    function test_burn() public {
         usdn.grantRole(usdn.MINTER_ROLE(), address(this));
         usdn.grantRole(usdn.ADJUSTMENT_ROLE(), address(this));
         usdn.mint(USER_1, 100 ether);
+    }
+
+    function test_burn() public {
         usdn.adjustMultiplier(1.1 ether);
         assertEq(usdn.balanceOf(USER_1), 110 ether);
         assertEq(usdn.sharesOf(USER_1), 100 ether);
@@ -109,5 +109,51 @@ contract TestUSDNBurn is USDNTokenFixture {
         assertEq(usdn.balanceOf(USER_1), 0);
         assertEq(usdn.sharesOf(USER_1), 0);
         vm.stopPrank();
+    }
+
+    function test_burnGuards() public {
+        // Amount too large
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, USER_1, 100 ether, 101 ether)
+        );
+        vm.prank(USER_1);
+        usdn.burn(101 ether);
+
+        usdn.adjustMultiplier(2 ether);
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, USER_1, 200 ether, 201 ether)
+        );
+        vm.prank(USER_1);
+        usdn.burn(201 ether);
+    }
+
+    function test_burnFrom() public {
+        vm.prank(USER_1);
+        usdn.approve(address(this), 50 ether);
+
+        usdn.adjustMultiplier(2 ether);
+        assertEq(usdn.allowance(USER_1, address(this)), 50 ether);
+
+        vm.expectEmit(true, true, true, false, address(usdn));
+        emit Transfer(USER_1, address(0), 50 ether); // expected event
+        usdn.burnFrom(USER_1, 50 ether);
+    }
+
+    function test_burnFromGuards() public {
+        vm.prank(USER_1);
+        usdn.approve(address(this), 50 ether);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientAllowance.selector, address(this), 50 ether, 51 ether)
+        );
+        usdn.burnFrom(USER_1, 51 ether);
+
+        vm.prank(USER_1);
+        usdn.approve(address(this), type(uint256).max);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, USER_1, 100 ether, 150 ether)
+        );
+        usdn.burnFrom(USER_1, 150 ether);
     }
 }
