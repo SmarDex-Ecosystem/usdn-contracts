773aca2f68c96f96e3e60f0dcc4e69b48bf6b5eb
Léo FASANO <fasanol.info@gmail.com>
2023-11-14 15:44:05 +0100

    feat: UsdnVault contract

--- src/UsdnVault/UsdnVault.sol
+++ src/UsdnVault/UsdnVault.sol
@@ -24,10 +24,18 @@ import { ERC20, ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensio
 import { TickMath } from "src/libraries/TickMath128.sol";
 import { TickBitmap } from "src/libraries/TickBitmap.sol";
 import { IUsdnVault, Position } from "src/interfaces/UsdnVault/IUsdnVault.sol";
-import { UsdnVaultCore } from "./UsdnVaultCore.sol";
+import { UsdnVaultPerps } from "./UsdnVaultPerps.sol";
 import { IOracleMiddleware, PriceInfo } from "src/interfaces/IOracleMiddleware.sol";
 
-contract UsdnVault is IUsdnVault, UsdnVaultCore, Ownable {
+import {
+    ZeroAmount,
+    IncompleteTransfer,
+    FundingRateInvalid,
+    MinLeverageInvalid,
+    MaxLeverageInvalid
+} from "../utils/Errors.sol";
+
+contract UsdnVault is IUsdnVault, UsdnVaultPerps, Ownable, Initializable {
     // Safe ERC20 and Tick bitmap
     using SafeERC20 for IERC20Metadata;
     using TickBitmap for mapping(int16 => uint256);
@@ -42,6 +50,94 @@ contract UsdnVault is IUsdnVault, UsdnVaultCore, Ownable {
     /// @param _tickSpacing The positions tick spacing.
     constructor(IERC20Metadata _asset, IOracleMiddleware _oracleMiddleware, int24 _tickSpacing)
         Ownable(msg.sender)
-        UsdnVaultCore(_asset, _oracleMiddleware, _tickSpacing)
+        UsdnVaultPerps(_asset, _oracleMiddleware, _tickSpacing)
     { }
+
+    /// @notice Initialize the vault.
+    /// @dev To be called once the vault is authorized to transfer the asset.
+    function initialize(uint256 _depositShort, uint96 _depositLong, uint128 _currentPrice)
+        external
+        initializer
+        onlyOwner
+    {
+        if (_depositShort == 0) revert ZeroAmount();
+        if (_depositLong == 0) revert ZeroAmount();
+
+        balanceShort += _depositShort;
+        balanceLong += _depositLong;
+
+        // Leverage x1
+        totalExpo += _depositLong;
+
+        lastPrice = _currentPrice;
+        lastUpdateTimestamp = uint128(block.timestamp);
+
+        {
+            uint256 usdpToMint = _calcMintUsdp(_depositShort, _currentPrice);
+
+            // Initialize default positions
+            Position memory initialLong = Position({
+                user: msg.sender,
+                amount: _depositLong,
+                startPrice: _currentPrice,
+                leverage: 1 gwei,
+                validated: true,
+                isExit: false,
+                timestamp: uint40(block.timestamp)
+            });
+            int24 tick = TickMath.minUsableTick(tickSpacing); // leverage 1 = liquidation price 0 => use lowest tick
+            Position[] storage pos = longPositions[_tickHash(tick)];
+            pos.push(initialLong);
+
+            bytes32 tickHash = _tickHash(tick);
+            totalExpoByTick[tickHash] += _depositLong; // leverage 1x
+            ++positionsInTick[tickHash];
+
+            totalLongPositions += 1;
+            tickBitmap.flipTick(tick, tickSpacing);
+            maxInitializedTick = tick;
+
+            usdn.mint(address(0xdead), usdpToMint);
+        }
+
+        uint256 balanceBefore = asset.balanceOf(address(this));
+        asset.safeTransferFrom(msg.sender, address(this), _depositShort + _depositLong);
+
+        if (asset.balanceOf(address(this)) != balanceBefore + _depositShort + _depositLong) {
+            revert IncompleteTransfer(asset.balanceOf(address(this)), balanceBefore + _depositShort + _depositLong);
+        }
+    }
+
+    /// @notice Manually set the funding rate.
+    /// @param _fundingRatePerSecond The new funding rate.
+    function setFundingRate(int256 _fundingRatePerSecond) external onlyOwner {
+        if (_fundingRatePerSecond <= 0) revert FundingRateInvalid();
+        if (_fundingRatePerSecond > 11_574_074_074) revert FundingRateInvalid(); // 0.1% daily max
+        fundingRatePerSecond = _fundingRatePerSecond;
+    }
+
+    /// @notice Set the min and max leverage.
+    /// @param _minLeverage The new min leverage.
+    /// @param _maxLeverage The new max leverage.
+    function setMinMaxLeverage(uint256 _minLeverage, uint256 _maxLeverage) external onlyOwner {
+        if (_minLeverage <= 1 * 10 ** LEVERAGE_DECIMALS + 1) {
+            revert MinLeverageInvalid();
+        }
+        if (_maxLeverage <= 1 * 10 ** LEVERAGE_DECIMALS + 1) {
+            revert MaxLeverageInvalid();
+        }
+        if (_maxLeverage > 20 * 10 ** LEVERAGE_DECIMALS) {
+            revert MaxLeverageInvalid();
+        }
+        if (_minLeverage > _maxLeverage) revert MinLeverageInvalid();
+        minLeverage = _minLeverage;
+        maxLeverage = _maxLeverage;
+    }
+
+    /// @notice Set the oracle middleware address.
+    /// @param _oracleMiddleware The new oracle middleware address.
+    function setOracleMiddleware(address _oracleMiddleware) external onlyOwner {
+        // FIXME: check that this is a contract
+        oracleMiddleware = IOracleMiddleware(_oracleMiddleware);
+    }
 }
