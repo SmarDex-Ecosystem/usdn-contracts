a6273246c2c2fc2ff02de40cf7cd3e592486791f
korbav <korbav@users.noreply.github.com>
2024-01-03 12:59:28 +0000

    ci(release): Creating a CI to bump current version and release the package to CodeArtifact (#13)
    * ci(release): adding a CI to bump version and release inherent package to CodeArtifact
    
    * chore: addressing comments
    
    * chore: ignore unimportant deployment files
    
    * build: script to generate ABIs
    
    * chore: adjust gitignore
    
    * ci(release): updating the workflow to run executeAbis before publish
    
    * ci: exporting bytecode
    
    * ci(release): limiting PR creation conditions to prevent loops
    
    * ci(release): removing useless env var
    
    ---------
    
    Co-authored-by: beeb <703631+beeb@users.noreply.github.com>

---
+++ script/exportAbi.ts
@@ -0,0 +1,61 @@
+import { writeFileSync, existsSync, mkdirSync, readFileSync, rmSync } from "fs";
+import { globSync } from "glob";
+import { basename } from "path";
+import { Command } from "commander";
+
+const ABI_EXPORT_PATH = "./dist/abi";
+const BYTECODE_EXPORT_PATH = "./dist/bytecode";
+
+const program = new Command();
+
+program
+  .description("Export ABI from artifacts")
+  .option(
+    "-g, --glob <filter>",
+    "Only includes files in 'src' that match the provided glob (defaults to '**/*.sol')."
+  )
+  .option("-d, --debug", "output extra debugging")
+  .parse(process.argv);
+
+const options = program.opts();
+const glob = options.glob || "**/*.sol";
+const DEBUG: boolean = options.debug ? true : false;
+
+let solFiles = globSync(`src/${glob}`);
+if (DEBUG) console.log("files:", solFiles);
+solFiles.forEach((file, i) => {
+  solFiles[i] = basename(file, ".sol");
+});
+
+if (existsSync(ABI_EXPORT_PATH))
+  rmSync(ABI_EXPORT_PATH, { recursive: true, force: true });
+mkdirSync(ABI_EXPORT_PATH, { recursive: true });
+
+if (existsSync(BYTECODE_EXPORT_PATH))
+  rmSync(BYTECODE_EXPORT_PATH, { recursive: true, force: true });
+mkdirSync(BYTECODE_EXPORT_PATH, { recursive: true });
+
+let indexContent = "";
+
+for (const name of solFiles) {
+  try {
+    const file = readFileSync(`./out/${name}.sol/${name}.json`);
+    const artifact = JSON.parse(file.toString());
+    const { bytecode: { object: bytes } } = artifact;
+
+    const fileContent = `export const ${name}Abi = ${JSON.stringify(
+      artifact.abi,
+      null,
+      2
+    )} as const;\n`;
+
+    writeFileSync(`${ABI_EXPORT_PATH}/${name}.ts`, fileContent);
+    writeFileSync(`${BYTECODE_EXPORT_PATH}/${name}.bytecode`, bytes);
+    indexContent += `export * from './${name}';\n`;
+  } catch {
+    // Could be normal, if a solidity file does not contain a contract (only an interface)
+    console.log(`./out/${name}.sol/${name}.json does not exist`);
+  }
+}
+
+writeFileSync(`${ABI_EXPORT_PATH}/index.ts`, indexContent);
