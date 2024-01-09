26346f9e35d8f0ba4edf5f4ad8cd79de88ce8b4a
Valentin B <703631+beeb@users.noreply.github.com>
2024-01-09 11:58:25 +0100

    feat: script to setup fork (#34)
    * chore: add fork setup script
    
    * feat: add script to mint wstETH
    
    * ci: fix snapshot
    
    * ci: fix snapshot

--- test/utils/WstEth.sol
+++ test/utils/WstEth.sol
@@ -8,4 +8,14 @@ contract WstETH is ERC20, ERC20Permit {
     constructor() ERC20("Wrapped liquid staked Ether 2.0", "wstETH") ERC20Permit("Wrapped liquid staked Ether 2.0") {
         _mint(msg.sender, 4_000_000 * 10 ** decimals());
     }
+
+    /// @dev Returns the amount of stETH per wstETH (mock value)
+    function stEthPerToken() public pure returns (uint256) {
+        return 1.15 ether;
+    }
+
+    /// @dev Receive ETH and mint wstETH
+    receive() external payable {
+        _mint(address(this), msg.value * 1 ether / stEthPerToken());
+    }
 }
