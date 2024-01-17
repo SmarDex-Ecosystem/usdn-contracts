f2f67a57d0575e01ec67dcaee79fdfef554c5ca4
Léo FASANO <fasanol.info@gmail.com>
2024-01-17 11:45:36 +0100

    feat: removed CL data stream & integration test

--- test/integration/OracleMiddleware/utils/Constants.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
-
-/* -------------------------------------------------------------------------- */
-/*                             Mocked oracle data                             */
-/* -------------------------------------------------------------------------- */
-
-uint256 constant ETH_PRICE = 2000 gwei;
-uint256 constant ETH_CONF = 20 gwei;
--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -28,12 +28,7 @@ address constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 address constant USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 address constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 address constant SDEX = address(0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF);
-
-/* --------------------------------- Oracles -------------------------------- */
-
-address constant PYTH_ORACLE = address(0x4305FB66699C3B2702D4d05CF36551390A4c69C6);
-bytes32 constant PYTH_WSTETH_USD = bytes32(0x6df640f3b8963d8f8358f791f352b8364513f6ab1cca5ed3f1f7b5448980e784);
-address constant CHAINLINK_ORACLE = address(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+address constant PYTH = address(0x4305FB66699C3B2702D4d05CF36551390A4c69C6);
 bytes32 constant PYTH_ETH_USD = bytes32(0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace);
 
 /* -------------------------------------------------------------------------- */
