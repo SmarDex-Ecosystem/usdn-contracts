88f268985ef1652b7b4d07b352f090dd0163612d
beeb <703631+beeb@users.noreply.github.com>
2024-01-22 09:26:24 +0100

    test: add fuzzing test for sum of long positions invariant

---
+++ test/unit/UsdnProtocol/UsdnProtocol.core.fuzzing.t.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
+
+import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
+
+import { Position } from "src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
+
+/**
+ * @custom:feature Fuzzing tests for the core of the protocol
+ * @custom:background Given a protocol instance that was initialized with 2 longs and 1 short
+ */
+contract TestUsdnProtocolCoreFuzzing is UsdnProtocolBaseFixture {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    /**
+     * @custom:scenario The sum of all long position's value is smaller or equal to the available long balance
+     * @custom:given No time has elapsed since the initialization (no funding rates)
+     * @custom:and The price of the asset starts at 2000 dollars
+     * @custom:and 10 random long positions and 10 random deposits are created with prices between 2000 and 3000 dollars
+     * @custom:when The sum of all position values is calculated at a price between 3000 and 10000 dollars
+     * @custom:then The long side available balance is greater or equal to the sum of all position values
+     * @param finalPrice the final price of the asset, at which we want to compare the available balance with the sum of
+     * all long positions
+     */
+    function testFuzz_longAssetAvailable(uint128 finalPrice) public {
+        finalPrice = uint128(bound(uint256(finalPrice), 3000 ether, 10_000 ether));
+        emit log_named_decimal_uint("finalPrice", finalPrice, oracleMiddleware.decimals());
+        uint256 currentPrice = 2000 ether;
+
+        Position[] memory pos = new Position[](10);
+        int24[] memory ticks = new int24[](10);
+
+        // create 10 random positions on each side of the protocol
+        for (uint256 i = 0; i < 10; i++) {
+            vm.roll(1); // change random seed
+
+            // create a random long position
+            uint256 longAmount = (block.prevrandao % 9 ether) + 1 ether;
+            uint256 longLeverage = (block.prevrandao % 3) + 2;
+            uint256 longLiqPrice = currentPrice / longLeverage;
+            vm.startPrank(users[i]);
+            (int24 tick, uint256 index) =
+                protocol.initiateOpenPosition(uint96(longAmount), uint128(longLiqPrice), abi.encode(currentPrice), "");
+            protocol.validateOpenPosition(abi.encode(currentPrice), "");
+            pos[i] = protocol.getLongPosition(tick, index);
+            ticks[i] = tick;
+
+            vm.roll(1); // change random seed
+
+            // create a random short position
+            uint256 shortAmount = (block.prevrandao % 9 ether) + 1 ether;
+            protocol.initiateDeposit(uint128(shortAmount), abi.encode(currentPrice), "");
+            protocol.validateDeposit(abi.encode(currentPrice), "");
+            vm.stopPrank();
+
+            // increase the current price, each time by 100 dollars or less, the max price is 3000 dollars
+            currentPrice += block.prevrandao % 100 ether;
+        }
+
+        // calculate the value of all new long positions
+        uint256 longPosValue;
+        uint128 liqPrice;
+        for (uint256 i = 0; i < 10; i++) {
+            Position memory position = pos[i];
+            liqPrice = protocol.getEffectivePriceForTick(ticks[i]);
+            longPosValue += protocol.positionValue(finalPrice, liqPrice, position.amount, position.leverage);
+        }
+        // calculate the value of the init position
+        liqPrice = protocol.getEffectivePriceForTick(protocol.minTick());
+        longPosValue += protocol.positionValue(finalPrice, liqPrice, protocol.FIRST_LONG_AMOUNT(), defaultPosLeverage);
+        // calculate the value of the deployer's long position
+        liqPrice = protocol.getEffectivePriceForTick(protocol.getEffectiveTickForPrice(INITIAL_PRICE / 2));
+        longPosValue += protocol.positionValue(
+            finalPrice, liqPrice, INITIAL_LONG - protocol.FIRST_LONG_AMOUNT(), initialLongLeverage
+        );
+
+        emit log_named_decimal_uint("longPosValue", longPosValue, wstETH.decimals());
+        emit log_named_decimal_uint("long balance", uint256(protocol.longAssetAvailable(finalPrice)), wstETH.decimals());
+
+        // The available balance should always be able to cover the value of all long positions
+        assertGe(uint256(protocol.longAssetAvailable(finalPrice)), longPosValue, "long balance");
+    }
+}
--- test/unit/UsdnProtocol/UsdnProtocol.core.t.sol
+++ test/unit/UsdnProtocol/UsdnProtocol.core.t.sol
@@ -5,9 +5,11 @@ import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
 import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
 
+import { Position } from "src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
+
 /**
  * @custom:feature The internal functions of the core of the protocol
- * @custom:background Given a protocol instance that was initialized
+ * @custom:background Given a protocol instance that was initialized with 2 longs and 1 short
  */
 contract TestUsdnProtocolCore is UsdnProtocolBaseFixture {
     function setUp() public override {
@@ -20,62 +22,26 @@ contract TestUsdnProtocolCore is UsdnProtocolBaseFixture {
      * @custom:and The price of the asset is equal to the initial price
      * @custom:when The sum of all position values is calculated
      * @custom:then The long side available balance is equal to the sum of all position values
-     * @dev Due to imprecision in the calculations, there are in practice a few wei of difference
-     * @dev TODO: can we modify the calculations so that the difference is always favoring the protocol?
+     * @dev Due to imprecision in the calculations, there are in practice a few wei of difference, but always in favor
+     * of the protocol (see fuzzing tests)
      */
     function test_longAssetAvailable() public {
         // calculate the value of the init position
         uint128 initLiqPrice = protocol.getEffectivePriceForTick(protocol.minTick());
         uint256 initPosValue =
             protocol.positionValue(INITIAL_PRICE, initLiqPrice, protocol.FIRST_LONG_AMOUNT(), defaultPosLeverage);
-        emit log_named_decimal_uint("initPosValue", initPosValue, wstETH.decimals());
 
         // calculate the value of the deployer's long position
         uint128 longLiqPrice = protocol.getEffectivePriceForTick(protocol.getEffectiveTickForPrice(INITIAL_PRICE / 2));
         uint256 longPosValue = protocol.positionValue(
             INITIAL_PRICE, longLiqPrice, INITIAL_LONG - protocol.FIRST_LONG_AMOUNT(), initialLongLeverage
         );
-        emit log_named_decimal_uint("longPosValue", longPosValue, wstETH.decimals());
 
         // calculate the sum to know the theoretical long balance
         uint256 sumOfPositions = longPosValue + initPosValue;
 
-        // there are rounding errors when calculating the value of a position,
-        // here we have up to 1 wei of error for each position
-        assertApproxEqAbs(uint256(protocol.longAssetAvailable(INITIAL_PRICE)), sumOfPositions, 2, "long balance");
-    }
-
-    /**
-     * @custom:scenario The sum of all long position's value is equal to the long side available balance
-     * @custom:given No time has elapsed since the initialization
-     * @custom:and The price of the asset has increased to $2100
-     * @custom:when The sum of all position values is calculated
-     * @custom:then The long side available balance is equal to the sum of all position values
-     * @dev Due to imprecision in the calculations, there are in practice a few wei of difference
-     * @dev TODO: can we modify the calculations so that the difference is always favoring the protocol?
-     */
-    function test_longAssetAvailablePriceUp() public {
-        // simulate a profit for the long (ignoring funding rates)
-        uint128 currentPrice = 2100 ether;
-
-        // calculate the value of the deployer's long position
-        uint128 initLiqPrice = protocol.getEffectivePriceForTick(protocol.minTick());
-        uint256 initPosValue =
-            protocol.positionValue(currentPrice, initLiqPrice, protocol.FIRST_LONG_AMOUNT(), defaultPosLeverage);
-        emit log_named_decimal_uint("initPosValue", initPosValue, wstETH.decimals());
-
-        // calculate the value of the first long position
-        uint128 longLiqPrice = protocol.getEffectivePriceForTick(protocol.getEffectiveTickForPrice(INITIAL_PRICE / 2));
-        uint256 longPosValue = protocol.positionValue(
-            currentPrice, longLiqPrice, INITIAL_LONG - protocol.FIRST_LONG_AMOUNT(), initialLongLeverage
-        );
-        emit log_named_decimal_uint("longPosValue", longPosValue, wstETH.decimals());
-
-        // calculate the sum to know the theoretical long balance
-        uint256 sumOfPositions = longPosValue + initPosValue;
-
-        // there are rounding errors when calculating the value of a position,
-        // here we have up to 1 wei of error for each position (in this case no error)
-        assertEq(uint256(protocol.longAssetAvailable(currentPrice)), sumOfPositions, "long balance");
+        // there are rounding errors when calculating the value of a position, here we have up to 1 wei of error for
+        // each position, but always in favor of the protocol.
+        assertGe(uint256(protocol.longAssetAvailable(INITIAL_PRICE)), sumOfPositions, "long balance");
     }
 }
