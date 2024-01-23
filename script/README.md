e7b8000c8d250fe57b96682fbb3de6ce7b2d2e24
beeb <703631+beeb@users.noreply.github.com>
2024-01-23 11:57:49 +0100

    perf: keep the SignedMath library internal as it reduces bytecode size

--- script/README.md
+++ script/README.md
@@ -16,6 +16,7 @@ Environment variables can be used to control the script execution:
 
 Example for an anvil fork using the real wstETH and depositing 1 ETH for both vault side and long side:
 Will also link oracles to the real mainnet configuration:
+
 ```
 export DEPLOYER_ADDRESS=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 export WSTETH_ADDRESS=0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
@@ -33,7 +34,7 @@ Get some wstETH. Requires enough ETH to mint the wstETH for the initial deposit 
 The private key is the default first user for anvil (funded with 10k ETH)
 
 ```
-forge script --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -f anvil --via-ir script/Fork.s.sol --broadcast
+forge script --via-ir --non-interactive --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -f anvil script/Fork.s.sol --broadcast
 ```
 
 ## Deploy protocol
@@ -41,5 +42,5 @@ forge script --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae7
 Requires enough wstETH to make the deposit and open the long position + ETH for gas.
 
 ```
-forge script --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -f anvil --via-ir script/Deploy.s.sol --broadcast
+forge script --via-ir --non-interactive --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -f anvil script/Deploy.s.sol --broadcast
 ```
--- src/libraries/SignedMath.sol
+++ src/libraries/SignedMath.sol
@@ -27,7 +27,7 @@ library SignedMath {
      * @param rhs right hand side operand
      * @return res_ the result of lhs + rhs
      */
-    function safeAdd(int256 lhs, int256 rhs) external pure returns (int256 res_) {
+    function safeAdd(int256 lhs, int256 rhs) internal pure returns (int256 res_) {
         unchecked {
             res_ = lhs + rhs;
             if (lhs >= 0 && res_ < rhs) {
@@ -45,7 +45,7 @@ library SignedMath {
      * @param rhs right hand side operand
      * @return res_ the result of lhs - rhs
      */
-    function safeSub(int256 lhs, int256 rhs) external pure returns (int256 res_) {
+    function safeSub(int256 lhs, int256 rhs) internal pure returns (int256 res_) {
         unchecked {
             res_ = lhs - rhs;
             if (rhs >= 0 && res_ > lhs) {
@@ -63,7 +63,7 @@ library SignedMath {
      * @param rhs right hand side operand
      * @return res_ the result of lhs * rhs
      */
-    function safeMul(int256 lhs, int256 rhs) external pure returns (int256 res_) {
+    function safeMul(int256 lhs, int256 rhs) internal pure returns (int256 res_) {
         unchecked {
             if (lhs == 0) {
                 return 0;
@@ -87,7 +87,7 @@ library SignedMath {
      * @param rhs right hand side operand
      * @return res_ the result of lhs / rhs
      */
-    function safeDiv(int256 lhs, int256 rhs) external pure returns (int256 res_) {
+    function safeDiv(int256 lhs, int256 rhs) internal pure returns (int256 res_) {
         unchecked {
             if (rhs == 0) {
                 revert SignedMathDivideByZero(lhs);
