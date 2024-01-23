8d7e44801d932464196b384f87fa2a6ffbfac3f0
beeb <703631+beeb@users.noreply.github.com>
2024-01-23 09:53:11 +0100

    build: tentative deploy script with external libs

--- script/Fork.s.sol
+++ script/Fork.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-import { Script } from "forge-std/Script.sol";
+import { Script, console2 } from "forge-std/Script.sol";
 
 import { WstETH } from "test/utils/WstEth.sol";
 
---
+++ script/deploy.ts
@@ -0,0 +1,111 @@
+import { execSync } from 'child_process';
+import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'fs';
+import { Command } from 'commander';
+import 'dotenv/config';
+
+const program = new Command();
+
+program
+  .description('Deploy the protocol with externally linked libraries')
+  .requiredOption('-f, --from <ADDRESS>', 'The address to deploy from')
+  .option('-r, --rpc-url <URL>', 'The RPC endpoint (env: ETH_RPC_URL)')
+  .option('--private-key <RAW_PRIVATE_KEY>', 'The private key of the deployer')
+  .option('-l, --ledger', 'Use a Ledger wallet')
+  .option('-t, --trezor', 'Use a Trezor wallet')
+  .option('-v, --verify', 'Verify the contracts on Etherscan')
+  .option('-s, --setup', 'Additional setup when working on a fork')
+  .parse();
+
+const options = program.opts();
+
+const cliArgs: string[] = [];
+if (options.rpcUrl) {
+  cliArgs.push('--rpc-url');
+  cliArgs.push(options.rpcUrl);
+}
+if (options.privateKey) {
+  cliArgs.push('--private-key');
+  cliArgs.push(options.privateKey);
+}
+if (options.ledger) {
+  cliArgs.push('-l');
+}
+if (options.trezor) {
+  cliArgs.push('-t');
+}
+if (options.verify) {
+  cliArgs.push('--verify');
+}
+
+const queueLib = JSON.parse(
+  execSync(
+    `forge create --json ${['-f', options.from, ...cliArgs].join(
+      ' ',
+    )} "src/libraries/DoubleEndedQueue.sol:DoubleEndedQueue"`,
+  ).toString(),
+).deployedTo;
+
+const mathLib = JSON.parse(
+  execSync(
+    `forge create --json ${['-f', options.from, ...cliArgs].join(' ')} "src/libraries/SignedMath.sol:SignedMath"`,
+  ).toString(),
+).deployedTo;
+
+const tickLib = JSON.parse(
+  execSync(
+    `forge create --json ${['-f', options.from, ...cliArgs].join(' ')} "src/libraries/TickMath.sol:TickMath"`,
+  ).toString(),
+).deployedTo;
+
+if (options.setup) {
+  execSync(`forge script ${cliArgs.join(' ')} script/Fork.s.sol --broadcast`);
+}
+
+const deployArgs = [
+  ...cliArgs,
+  '--non-interactive',
+  /* '--libraries',
+  `"src/libraries/DoubleEndedQueue.sol:DoubleEndedQueue:${queueLib}"`,
+  '--libraries',
+  `"src/libraries/SignedMath.sol:SignedMath:${mathLib}"`,
+  '--libraries',
+  `"src/libraries/TickMath.sol:TickMath:${tickLib}"`, */
+];
+
+const resString = execSync(`forge script ${deployArgs.join(' ')} script/Deploy.s.sol --broadcast`).toString();
+
+const regex = /Transactions saved to: (\S+)/;
+const match = resString.match(regex);
+
+if (!match?.[1]) {
+  console.error('JSON filepath not found in the text.');
+  process.exit(1);
+}
+
+const jsonFilePath = match[1];
+const file = readFileSync(jsonFilePath);
+const data = JSON.parse(file.toString());
+const middlewareAddress = data.transactions[0].contractAddress;
+const usdnAddress = data.transactions[1].contractAddress;
+
+const finalArgs = [
+  ...cliArgs,
+  '--libraries',
+  `"src/libraries/DoubleEndedQueue.sol:DoubleEndedQueue:${queueLib}"`,
+  '--libraries',
+  `"src/libraries/SignedMath.sol:SignedMath:${mathLib}"`,
+  '--libraries',
+  `"src/libraries/TickMath.sol:TickMath:${tickLib}"`,
+];
+
+const finalRes = execSync(
+  `forge create --json ${[
+    '-f',
+    options.from,
+    '--constructor-args',
+    `${usdnAddress} ${process.env.WSTETH_ADDRESS} ${middlewareAddress} 100`,
+    ...finalArgs,
+  ].join(' ')} "src/UsdnProtocol/UsdnProtocol.sol:UsdnProtocol"`,
+).toString();
+
+console.log(finalRes);
