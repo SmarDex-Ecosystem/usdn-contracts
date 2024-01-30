61e40502b4fcb90e5457dcae3d201bde649583d2
beeb <703631+beeb@users.noreply.github.com>
2024-01-30 16:53:48 +0100

    feat(interfaces): add initialize function

--- src/interfaces/UsdnProtocol/IUsdnProtocol.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocol.sol
@@ -9,4 +9,23 @@ import { IUsdnProtocolErrors } from "src/interfaces/UsdnProtocol/IUsdnProtocolEr
  * @title IUsdnProtocol
  * @notice Interface for the USDN protocol.
  */
-interface IUsdnProtocol is IUsdnProtocolActions, IUsdnProtocolEvents, IUsdnProtocolErrors { }
+interface IUsdnProtocol is IUsdnProtocolActions, IUsdnProtocolEvents, IUsdnProtocolErrors {
+    /**
+     * @notice Initialize the protocol, making a first deposit and creating a first long position.
+     * @dev This function can only be called once, and no other user action can be performed until it was called.
+     * @dev Consult the current oracle middleware implementation to know the expected format for the price data, using
+     * the `ProtocolAction.Initialize` action.
+     * @dev The price validation might require payment according to the return value of the `validationCost` function
+     * of the middleware.
+     * @param depositAmount the amount of wstETH for the deposit.
+     * @param longAmount the amount of wstETH for the long.
+     * @param desiredLiqPrice the desired liquidation price for the long.
+     * @param currentPriceData the current price data.
+     */
+    function initialize(
+        uint128 depositAmount,
+        uint128 longAmount,
+        uint128 desiredLiqPrice,
+        bytes calldata currentPriceData
+    ) external payable;
+}
