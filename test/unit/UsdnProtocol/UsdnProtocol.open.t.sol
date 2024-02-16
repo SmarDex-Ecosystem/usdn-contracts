c9dd6aaccba18f558e541a32ab4c17be60483c7b
beeb <703631+beeb@users.noreply.github.com>
2024-02-16 10:40:42 +0100

    test(open): add more asserts

--- test/unit/UsdnProtocol/UsdnProtocol.open.t.sol
+++ test/unit/UsdnProtocol/UsdnProtocol.open.t.sol
@@ -23,32 +23,58 @@ contract TestUsdnProtocolOpenPosition is UsdnProtocolBaseFixture {
     /**
      * @custom:scenario The user initiates an open position action
      * @custom:given The amount of collateral is 1 wstETH and the current price is 2000$
-     * @custom:when The user initiates an open position with 1 wstETH and a desired liquidation price of 1333$ (approx
+     * @custom:when The user initiates an open position with 1 wstETH and a desired liquidation price of ~1333$ (approx
      * 3x leverage)
      * @custom:then The protocol creates the position and emits the InitiatedOpenPosition event
+     * @custom:and the state changes are as expected
      */
     function test_initiateOpenPosition() public {
         uint96 longAmount = 1 ether;
         uint128 currentPrice = 2000 ether;
-        bytes memory priceData = abi.encode(currentPrice);
         uint128 desiredLiqPrice = currentPrice * 2 / 3; // leverage approx 3x
         int24 expectedTick = protocol.getEffectiveTickForPrice(desiredLiqPrice);
-        uint128 liqPriceWithoutPenalty = protocol.getEffectivePriceForTick(
-            expectedTick - int24(protocol.liquidationPenalty()) * protocol.tickSpacing()
+        uint128 expectedLeverage = uint128(
+            (10 ** protocol.LEVERAGE_DECIMALS() * currentPrice)
+                / (
+                    currentPrice
+                        - protocol.getEffectivePriceForTick(
+                            expectedTick - int24(protocol.liquidationPenalty()) * protocol.tickSpacing()
+                        )
+                )
         );
-        uint128 expectedLeverage =
-            uint128((10 ** protocol.LEVERAGE_DECIMALS() * currentPrice) / (currentPrice - liqPriceWithoutPenalty));
 
-        vm.expectEmit();
-        emit InitiatedOpenPosition(
-            address(this), uint40(block.timestamp), expectedLeverage, longAmount, currentPrice, expectedTick, 0, 0
-        ); // expected event
-        (int24 tick, uint256 tickVersion, uint256 index) =
-            protocol.initiateOpenPosition(longAmount, desiredLiqPrice, priceData, "");
+        uint256 balanceBefore = wstETH.balanceOf(address(this));
+        uint256 protocolBalanceBefore = wstETH.balanceOf(address(protocol));
+        uint256 totalPositionsBefore = protocol.totalLongPositions();
+        uint256 totalExpoBefore = protocol.totalExpo();
+        uint256 balanceLongBefore = uint256(protocol.longAssetAvailable(currentPrice));
 
-        assertEq(tick, expectedTick);
-        assertEq(tickVersion, 0);
-        assertEq(index, 0);
+        {
+            vm.expectEmit();
+            emit InitiatedOpenPosition(
+                address(this), uint40(block.timestamp), expectedLeverage, longAmount, currentPrice, expectedTick, 0, 0
+            ); // expected event
+            (int24 tick, uint256 tickVersion, uint256 index) =
+                protocol.initiateOpenPosition(longAmount, desiredLiqPrice, abi.encode(currentPrice), "");
+
+            assertEq(tick, expectedTick);
+            assertEq(tickVersion, 0);
+            assertEq(index, 0);
+        }
+
+        assertEq(wstETH.balanceOf(address(this)), balanceBefore - longAmount);
+        assertEq(wstETH.balanceOf(address(protocol)), protocolBalanceBefore + longAmount);
+        assertEq(protocol.totalLongPositions(), totalPositionsBefore + 1);
+        assertEq(
+            protocol.totalExpo(),
+            totalExpoBefore + uint256(longAmount) * expectedLeverage / uint256(10) ** protocol.LEVERAGE_DECIMALS()
+        );
+        assertEq(
+            protocol.totalExpoByTick(expectedTick),
+            uint256(longAmount) * expectedLeverage / uint256(10) ** protocol.LEVERAGE_DECIMALS()
+        );
+        assertEq(protocol.positionsInTick(expectedTick), 1);
+        assertEq(protocol.balanceLong(), balanceLongBefore + longAmount);
     }
 
     // test refunds
