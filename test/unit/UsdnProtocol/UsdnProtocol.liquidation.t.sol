2a87795399b4d03216ab654b04e51d73cef0347e
Nicolas DECOSTERD <n.decosterd@ra2.tech>
2024-02-20 15:14:58 +0100

    chore: Remove unused import

--- test/unit/UsdnProtocol/UsdnProtocol.liquidation.t.sol
+++ test/unit/UsdnProtocol/UsdnProtocol.liquidation.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-import { USER_1, DEPLOYER } from "test/utils/Constants.sol";
+import { DEPLOYER } from "test/utils/Constants.sol";
 import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
 
 import { ILiquidationRewardsManagerErrorsEventsTypes } from
