6359e1b4b3efdab0d845b63b85c4e998602f30c0
fireboss777 <fireboss@tutanota.com>
2024-02-21 14:50:30 +0100

    fix: removed unused import

--- src/interfaces/UsdnProtocol/IUsdnProtocolEvents.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolEvents.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
-import { Position } from "src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
-
 /**
  * @title IUsdnProtocolEvents
  * @notice Events for the USDN Protocol
