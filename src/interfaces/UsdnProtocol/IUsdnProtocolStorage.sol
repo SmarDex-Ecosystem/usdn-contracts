c7d5d8ec11302fb0063ba188dbef2819548499d8
Valentin B <703631+beeb@users.noreply.github.com>
2024-02-22 16:31:04 +0100

    refactor(interfaces): relax solidity version requirement (#106)
    * refactor(interfaces): relax solidity version requirement
    
    * fix: pragma for IWstETH interface

--- src/interfaces/UsdnProtocol/IUsdnProtocolStorage.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.20;
+pragma solidity >=0.8.0;
 
 import { IUsdnProtocolEvents } from "src/interfaces/UsdnProtocol/IUsdnProtocolEvents.sol";
 import { IUsdnProtocolErrors } from "src/interfaces/UsdnProtocol/IUsdnProtocolErrors.sol";
