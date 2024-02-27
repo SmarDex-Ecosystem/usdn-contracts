2b813978fe43f308e016db805bc843032088f194
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2024-02-27 15:31:16 +0100

    chore: harmonize solidity version across source files (#110)
    Co-authored-by: Nicolas DECOSTERD <n.decosterd@ra2.tech>

--- src/OracleMiddleware/oracles/ChainlinkOracle.sol
+++ src/OracleMiddleware/oracles/ChainlinkOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
