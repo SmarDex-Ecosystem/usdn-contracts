d4c3433fb1ffdf372f9c3b3672e07787876b5f19
beeb <703631+beeb@users.noreply.github.com>
2024-03-07 19:21:02 +0100

    test(rebase): fix happy path test

--- test/unit/UsdnProtocol/Rebase.t.sol
+++ test/unit/UsdnProtocol/Rebase.t.sol
@@ -8,13 +8,17 @@ import { IUsdnEvents } from "src/interfaces/Usdn/IUsdnEvents.sol";
 
 /**
  * @custom:feature Test the rebasing of the USDN token depending on its price
- * @custom:background Given a protocol instance that was initialized with more expo in the long side and rebase enabled
+ * @custom:background Given a protocol instance that was initialized with more expo in the long side and rebase+funding
+ * enabled
  */
 contract TestUsdnProtocolRebase is UsdnProtocolBaseFixture, IUsdnEvents {
     function setUp() public {
         params = DEFAULT_PARAMS;
-        params.initialDeposit = 1 ether;
+        params.initialDeposit = 5 ether;
         params.initialLong = 10 ether;
+        params.enablePositionFees = false;
+        params.enableProtocolFees = false;
+        params.enableFunding = false;
         params.enableUsdnRebase = true;
         super._setUp(params);
         vm.prank(ADMIN);
@@ -27,32 +31,36 @@ contract TestUsdnProtocolRebase is UsdnProtocolBaseFixture, IUsdnEvents {
 
         skip(1 hours);
 
-        // price goes above rebase threshold due to funding
-        uint256 usdnPrice = protocol.usdnPrice(params.initialPrice);
+        uint128 newPrice = params.initialPrice - 100 ether;
+
+        // price goes above rebase threshold due to change in asset price
+        uint256 usdnPrice = protocol.usdnPrice(newPrice);
         assertGt(usdnPrice, protocol.getUsdnRebaseThreshold(), "initial price");
 
         // calculate expected new USDN divisor
         uint256 expectedVaultBalance =
-            uint256(protocol.vaultAssetAvailableWithFunding(params.initialPrice, uint128(block.timestamp - 30)));
+            uint256(protocol.vaultAssetAvailableWithFunding(newPrice, uint128(block.timestamp - 30)));
         uint256 expectedTotalSupply = protocol.i_calcRebaseTotalSupply(
             expectedVaultBalance,
-            params.initialPrice,
+            newPrice,
             protocol.getTargetUsdnPrice(),
             protocol.getUsdnDecimals(),
             protocol.getAssetDecimals()
         );
         uint256 expectedDivisor = usdn.totalSupply() * usdn.divisor() / expectedTotalSupply;
 
-        // vm.expectEmit();
-        // emit Rebase(usdn.MAX_DIVISOR(), expectedDivisor);
-        protocol.liquidate(abi.encode(params.initialPrice), 0);
+        // rebase (no liquidation happens)
+        vm.expectEmit();
+        emit Rebase(usdn.MAX_DIVISOR(), expectedDivisor);
+        protocol.liquidate(abi.encode(newPrice), 0);
 
         assertApproxEqAbs(
-            protocol.usdnPrice(params.initialPrice, uint128(block.timestamp - 30)),
+            protocol.usdnPrice(newPrice, uint128(block.timestamp - 30)),
             protocol.getTargetUsdnPrice(),
             1,
             "price after rebase"
         );
-        // assertEq(protocol.getBalanceVault(), expectedVaultBalance, "vault balance");
+        assertApproxEqRel(usdn.totalSupply(), expectedTotalSupply, 1, "total supply");
+        assertEq(protocol.getBalanceVault(), expectedVaultBalance, "vault balance");
     }
 }
