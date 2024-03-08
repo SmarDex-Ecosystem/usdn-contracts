24af4a487e1a00ad1e34c374dd9bb8b825ec0165
Nicolas DECOSTERD <kirienzo.eth@gmail.com>
2024-03-08 15:50:53 +0100

    chore: revert some unnecessary changes

--- test/unit/UsdnProtocol/Long.t.sol
+++ test/unit/UsdnProtocol/Long.t.sol
@@ -13,7 +13,7 @@ import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.s
  * @custom:background Given a protocol initialized with 10 wstETH in the vault and 5 wstETH in a long position with a
  * leverage of ~2x.
  */
-contract TestUsdnProtocolActionsLong is UsdnProtocolBaseFixture {
+contract TestUsdnProtocolLong is UsdnProtocolBaseFixture {
     function setUp() public {
         super._setUp(DEFAULT_PARAMS);
         wstETH.mintAndApprove(address(this), 100_000 ether, address(protocol), type(uint256).max);
@@ -300,10 +300,8 @@ contract TestUsdnProtocolActionsLong is UsdnProtocolBaseFixture {
         protocol.validateOpenPosition(abi.encode(price), "");
         skip(oracleMiddleware.getValidationDelay() + 1);
 
-        Position memory pos = protocol.getLongPosition(tick, tickVersion, index);
-
         vm.expectEmit();
         emit InitiatedClosePosition(address(this), tick, tickVersion, index, 0, 0);
-        protocol.initiateClosePosition(tick, tickVersion, index, pos.amount, abi.encode(price), "");
+        protocol.initiateClosePosition(tick, tickVersion, index, 1 ether, abi.encode(price), "");
     }
 }
