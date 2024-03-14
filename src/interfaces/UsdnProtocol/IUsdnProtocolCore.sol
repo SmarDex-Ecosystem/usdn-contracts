11b00730ae814bf1cd36d372ec36e95e11302b29
samooyo <samidarnaud@gmail.com>
2024-03-14 11:36:58 +0100

    feat(core): changed visibility of funding and fundingAsset functions

--- src/interfaces/UsdnProtocol/IUsdnProtocolCore.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolCore.sol
@@ -31,22 +31,10 @@ interface IUsdnProtocolCore is IUsdnProtocolStorage {
      * the vault side (or long side if negative). If the provided timestamp is older than the last state update, the
      * function reverts with `UsdnProtocolTimestampTooOld`.
      * @param timestamp The current timestamp
-     * @param ema The EMA of the funding rate
      * @return fund_ The magnitude of the funding (with `FUNDING_RATE_DECIMALS` decimals)
      * @return oldLongExpo_ The long trading exposure after the last state update
      */
-    function funding(uint128 timestamp, int256 ema) external view returns (int256 fund_, int256 oldLongExpo_);
-
-    /**
-     * @notice Get the predicted value of the funding (in asset units) since the last state update for the given
-     * timestamp
-     * @dev If the provided timestamp is older than the last state update, the result will be zero.
-     * @param timestamp The current timestamp
-     * @param ema The EMA of the funding rate
-     * @return fundingAsset_ The number of asset tokens of funding (with asset decimals)
-     * @return fund_ The magnitude of the funding (with `FUNDING_RATE_DECIMALS` decimals)
-     */
-    function fundingAsset(uint128 timestamp, int256 ema) external view returns (int256 fundingAsset_, int256 fund_);
+    function funding(uint128 timestamp) external view returns (int256 fund_, int256 oldLongExpo_);
 
     /**
      * @notice Get the predicted value of the long balance for the given asset price and timestamp
