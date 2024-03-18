82739a2ef3c4f8cf60f79bce0d48035e19b22ff8
Nicolas DECOSTERD <kirienzo.eth@gmail.com>
2024-03-18 11:14:25 +0100

    fix(gas-test): fix liquidation gas usage test

--- src/OracleMiddleware/LiquidationRewardsManager.sol
+++ src/OracleMiddleware/LiquidationRewardsManager.sol
@@ -45,7 +45,7 @@ contract LiquidationRewardsManager is ILiquidationRewardsManager, ChainlinkOracl
         _wstEth = wstETH;
         _rewardsParameters = RewardsParameters({
             gasUsedPerTick: 31_971,
-            otherGasUsed: 390_922,
+            otherGasUsed: 391_028,
             rebaseGasUsed: 8966,
             gasPriceLimit: 1000 gwei,
             multiplierBps: 20_000
