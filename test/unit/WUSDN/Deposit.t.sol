fe290d9b488f67ad6635e463ffb1e785d51f5aa9
lilyanb <lilyan.bastien@gmail.com>
2024-03-19 14:01:18 +0100

    test: refacto deposit and withdraw tests

--- test/unit/WUSDN/Deposit.t.sol
+++ test/unit/WUSDN/Deposit.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-import { USER_1 } from "test/utils/Constants.sol";
+import { IERC20Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+
 import { UsdnTokenFixture } from "test/unit/USDN/utils/Fixtures.sol";
 
 import { Wusdn } from "src/Wusdn.sol";
@@ -11,19 +12,22 @@ import { Wusdn } from "src/Wusdn.sol";
  * @custom:background  Given this contract has the MINTER_ROLE and mint tokens to USER_1
  */
 contract TestWusdnDeposit is UsdnTokenFixture {
+    /// Events coming from the OpenZeppelin ERC4626
+    error ERC4626ExceededMaxWithdraw(address owner, uint256 assets, uint256 max);
+
     Wusdn wusdn;
     uint256 oneUSDN;
 
     function setUp() public override {
         super.setUp();
 
-        usdn.grantRole(usdn.MINTER_ROLE(), address(this));
-        usdn.mint(USER_1, 100 ether);
-
         wusdn = new Wusdn(usdn);
 
         uint256 decimals = usdn.decimals();
         oneUSDN = 1 * 10 ** decimals;
+
+        usdn.grantRole(usdn.MINTER_ROLE(), address(this));
+        usdn.mint(address(this), 100 * oneUSDN);
     }
 
     /**
@@ -35,15 +39,48 @@ contract TestWusdnDeposit is UsdnTokenFixture {
      */
     function test_deposit_to_wusdn() public {
         uint256 shares = wusdn.previewDeposit(oneUSDN);
-        uint256 balanceBeforeDeposit = usdn.balanceOf(USER_1);
-        uint256 shareBeforeDeposit = wusdn.balanceOf(USER_1);
-        vm.startPrank(USER_1);
+        uint256 balanceBeforeDeposit = usdn.balanceOf(address(this));
+        uint256 shareBeforeDeposit = wusdn.balanceOf(address(this));
         usdn.approve(address(wusdn), type(uint256).max);
-        wusdn.deposit(oneUSDN, USER_1);
-        vm.stopPrank();
-        assertEq(balanceBeforeDeposit - usdn.balanceOf(USER_1), oneUSDN, "usdn balance of USER_1");
+        wusdn.deposit(oneUSDN, address(this));
+        assertEq(balanceBeforeDeposit - usdn.balanceOf(address(this)), oneUSDN, "usdn balance of USER_1");
         assertEq(usdn.balanceOf(address(wusdn)), oneUSDN, "usdn balance of wusdn");
-        assertEq(wusdn.balanceOf(address(USER_1)) - shareBeforeDeposit, shares, "wusdn share of USER_1");
+        assertEq(wusdn.balanceOf(address(this)) - shareBeforeDeposit, shares, "wusdn share of USER_1");
         assertEq(wusdn.totalSupply(), shares, "wusdn total shares");
+    }
+
+    /**
+     * @custom:scenario Test deposit function
+     * @custom:given The user has initiated a deposit
+     * @custom:and The validation deadline has elapsed
+     * @custom:when The test_deposit function is called
+     * @custom:then The deposit is successful
+     */
+    function test_deposit_zero_to_wusdn() public {
+        uint256 shares = wusdn.previewDeposit(0);
+        uint256 balanceBeforeDeposit = usdn.balanceOf(address(this));
+        uint256 shareBeforeDeposit = wusdn.balanceOf(address(this));
+        usdn.approve(address(wusdn), type(uint256).max);
+        wusdn.deposit(0, address(this));
+        assertEq(balanceBeforeDeposit - usdn.balanceOf(address(this)), 0, "usdn balance of USER_1");
+        assertEq(usdn.balanceOf(address(wusdn)), 0, "usdn balance of wusdn");
+        assertEq(wusdn.balanceOf(address(this)) - shareBeforeDeposit, shares, "wusdn share of USER_1");
+        assertEq(wusdn.totalSupply(), shares, "wusdn total shares");
+    }
+
+    /**
+     * @custom:scenario Test the withdraw function with insufficient balance
+     * @custom:given A user with 100 USDN deposited in wusdn
+     * @custom:when The user approve 2000 USDN and initiates a withdraw of 2000 USDN
+     * @custom:then The transaction reverts with the `ERC20InsufficientBalance` error
+     */
+    function test_RevertWhen_depositInsufficientBalance() public {
+        usdn.approve(address(wusdn), 2000 * oneUSDN);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientBalance.selector, address(this), 100 * oneUSDN, 2000 * oneUSDN
+            )
+        );
+        wusdn.deposit(2000 * oneUSDN, address(this));
     }
 }
--- test/unit/WUSDN/Withdraw.t.sol
+++ test/unit/WUSDN/Withdraw.t.sol
@@ -1,35 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-import { USER_1 } from "test/utils/Constants.sol";
 import { UsdnTokenFixture } from "test/unit/USDN/utils/Fixtures.sol";
 
 import { Wusdn } from "src/Wusdn.sol";
 
 /**
  * @custom:feature The `withdraw` function of `WUSDN`
- * @custom:background  Given this contract has the MINTER_ROLE and mint tokens to USER_1
+ * @custom:background  Given this contract has the MINTER_ROLE and mint tokens to contract
  * and the user has deposited 1 usdn in wusdn contract
  */
 contract TestWusdnWithdraw is UsdnTokenFixture {
+    /// Events coming from the OpenZeppelin ERC4626
+    error ERC4626ExceededMaxWithdraw(address owner, uint256 assets, uint256 max);
+
     Wusdn wusdn;
     uint256 oneUSDN;
 
     function setUp() public override {
         super.setUp();
 
-        usdn.grantRole(usdn.MINTER_ROLE(), address(this));
-        usdn.mint(USER_1, 100 ether);
-
         wusdn = new Wusdn(usdn);
 
         uint256 decimals = usdn.decimals();
         oneUSDN = 1 * 10 ** decimals;
 
-        vm.startPrank(USER_1);
+        usdn.grantRole(usdn.MINTER_ROLE(), address(this));
+        usdn.mint(address(this), 100 * oneUSDN);
         usdn.approve(address(wusdn), type(uint256).max);
-        wusdn.deposit(oneUSDN, USER_1);
-        vm.stopPrank();
+        wusdn.deposit(oneUSDN, address(this));
     }
 
     /**
@@ -42,14 +41,44 @@ contract TestWusdnWithdraw is UsdnTokenFixture {
     function test_withdraw_to_wusdn() public {
         uint256 shareBeforeWithdraw = wusdn.totalSupply();
         uint256 shares = wusdn.previewWithdraw(oneUSDN / 10);
-        uint256 balanceBeforeWithdraw = usdn.balanceOf(USER_1);
-        uint256 shareUser1BeforeWithdraw = wusdn.balanceOf(USER_1);
-        vm.startPrank(USER_1);
-        wusdn.withdraw(oneUSDN / 10, USER_1, USER_1);
-        vm.stopPrank();
-        assertEq(usdn.balanceOf(USER_1) - balanceBeforeWithdraw, oneUSDN / 10, "usdn balance of USER_1");
+        uint256 balanceBeforeWithdraw = usdn.balanceOf(address(this));
+        uint256 shareUser1BeforeWithdraw = wusdn.balanceOf(address(this));
+        wusdn.withdraw(oneUSDN / 10, address(this), address(this));
+        assertEq(usdn.balanceOf(address(this)) - balanceBeforeWithdraw, oneUSDN / 10, "usdn balance");
         assertEq(usdn.balanceOf(address(wusdn)), oneUSDN - oneUSDN / 10, "usdn balance of wusdn");
-        assertEq(shareUser1BeforeWithdraw - wusdn.balanceOf(USER_1), shares, "wusdn share of USER_1");
+        assertEq(shareUser1BeforeWithdraw - wusdn.balanceOf(address(this)), shares, "wusdn share");
         assertEq(shareBeforeWithdraw, wusdn.totalSupply() + shares, "wusdn total shares");
+    }
+
+    /**
+     * @custom:scenario Test the withdraw function
+     * @custom:given The user has deposited 1 usdn
+     * @custom:when The user initiates a withdraw of 0 usdn
+     * @custom:then The user's balance increases by 0 usdn
+     * @custom:then The user's share of wusdn decreases by the expected amount
+     */
+    function test_withdraw_zero_to_wusdn() public {
+        uint256 shareBeforeWithdraw = wusdn.totalSupply();
+        uint256 shares = wusdn.previewWithdraw(0);
+        uint256 balanceBeforeWithdraw = usdn.balanceOf(address(this));
+        uint256 shareUser1BeforeWithdraw = wusdn.balanceOf(address(this));
+        wusdn.withdraw(0, address(this), address(this));
+        assertEq(usdn.balanceOf(address(this)) - balanceBeforeWithdraw, 0, "usdn balance");
+        assertEq(usdn.balanceOf(address(wusdn)), oneUSDN, "usdn balance of wusdn");
+        assertEq(shareUser1BeforeWithdraw - wusdn.balanceOf(address(this)), shares, "wusdn share");
+        assertEq(shareBeforeWithdraw, wusdn.totalSupply() + shares, "wusdn total shares");
+    }
+
+    /**
+     * @custom:scenario Test the withdraw function with insufficient balance
+     * @custom:given A user with 1 USDN deposited in wusdn
+     * @custom:when The user initiates a withdraw of 2 USDN
+     * @custom:then The transaction reverts with the `ERC4626ExceededMaxWithdraw` error
+     */
+    function test_RevertWhen_withdrawInsufficientBalance() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(ERC4626ExceededMaxWithdraw.selector, address(this), 2 * oneUSDN, oneUSDN)
+        );
+        wusdn.withdraw(2 * oneUSDN, address(this), address(this));
     }
 }
