4524e9125fa526d3efbdd015963de0c22a43cc64
beeb <703631+beeb@users.noreply.github.com>
2024-03-26 14:46:56 +0100

    test(usdn): add tests for `transferSharesFrom`

--- test/unit/USDN/TransferSharesFrom.t.sol
+++ test/unit/USDN/TransferSharesFrom.t.sol
@@ -7,10 +7,10 @@ import { USER_1 } from "test/utils/Constants.sol";
 import { UsdnTokenFixture } from "test/unit/USDN/utils/Fixtures.sol";
 
 /**
- * @custom:feature The `transferShares` function of `USDN`
+ * @custom:feature The `transferSharesFrom` function of `USDN`
  * @custom:background Given a user with 100 tokens
  */
-contract TestUsdnTransferShares is UsdnTokenFixture {
+contract TestUsdnTransferSharesFrom is UsdnTokenFixture {
     function setUp() public override {
         super.setUp();
         usdn.grantRole(usdn.MINTER_ROLE(), address(this));
@@ -21,7 +21,8 @@ contract TestUsdnTransferShares is UsdnTokenFixture {
     /**
      * @custom:scenario Transfer shares after a rebase
      * @custom:given The USDN was rebased with a random divisor
-     * @custom:when The user transfers a random amount of shares to the test contract
+     * @custom:and User 1 has approved this contract to transfer their full balance
+     * @custom:when The test contract transfers a random amount of shares from user 1 to the test contract
      * @custom:then The user's balance is decreased by the transferred amount
      * @custom:and The contract's balance is increased by the transferred amount
      * @custom:and The token emits a `Transfer` event with the expected values
@@ -29,11 +30,15 @@ contract TestUsdnTransferShares is UsdnTokenFixture {
      * @param divisor The rebase divisor
      * @param sharesAmount The amount of shares to transfer
      */
-    function testFuzz_transferShares(uint256 divisor, uint256 sharesAmount) public {
+    function testFuzz_transferSharesFrom(uint256 divisor, uint256 sharesAmount) public {
         divisor = bound(divisor, usdn.MIN_DIVISOR(), usdn.MAX_DIVISOR());
         if (divisor < usdn.MAX_DIVISOR()) {
             usdn.rebase(divisor);
         }
+
+        uint256 userBalance = usdn.balanceOf(USER_1);
+        vm.prank(USER_1);
+        usdn.approve(address(this), userBalance);
 
         uint256 sharesBefore = usdn.sharesOf(USER_1);
         uint256 totalSharesBefore = usdn.totalShares();
@@ -41,8 +46,7 @@ contract TestUsdnTransferShares is UsdnTokenFixture {
         uint256 tokenAmount = usdn.convertToTokens(sharesAmount);
         vm.expectEmit(address(usdn));
         emit Transfer(USER_1, address(this), tokenAmount); // expected event
-        vm.prank(USER_1);
-        usdn.transferShares(address(this), sharesAmount);
+        usdn.transferSharesFrom(USER_1, address(this), sharesAmount);
 
         assertEq(usdn.sharesOf(USER_1), sharesBefore - sharesAmount, "balance of user");
         assertEq(usdn.sharesOf(address(this)), sharesAmount, "balance of contract");
@@ -50,34 +54,36 @@ contract TestUsdnTransferShares is UsdnTokenFixture {
     }
 
     /**
-     * @custom:scenario Transfer shares from zero address
-     * @custom:when We try to transfer shares from the zero address
-     * @custom:then The transaction reverts with the ERC20InvalidSender error
+     * @custom:scenario Transfer shares from another user to zero address
+     * @custom:given User 1 has approved this contract to transfer their tokens
+     * @custom:when We try to transfer user 1's shares to the zero address
+     * @custom:then The transaction reverts with the `ERC20InvalidReceiver` error
      */
-    function test_RevertWhen_transferSharesFromZeroAddress() public {
-        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InvalidSender.selector, address(0)));
-        usdn.i_transferShares(address(0), USER_1, 1e36, 1e18);
-    }
-
-    /**
-     * @custom:scenario Transfer shares to zero address
-     * @custom:when We try to transfer shares to the zero address
-     * @custom:then The transaction reverts with the ERC20InvalidReceiver error
-     */
-    function test_RevertWhen_transferSharesToZeroAddress() public {
-        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InvalidReceiver.selector, address(0)));
-        usdn.transferShares(address(0), 1e36);
-    }
-
-    /**
-     * @custom:scenario Transfer more shares than the balance
-     * @custom:when The user tries to transfer more shares than they have
-     * @custom:then The transaction reverts with the USDNInsufficientSharesBalance error
-     */
-    function test_RevertWhen_transferSharesInsufficientBalance() public {
-        uint256 shares = usdn.sharesOf(USER_1);
-        vm.expectRevert(abi.encodeWithSelector(UsdnInsufficientSharesBalance.selector, USER_1, shares, shares + 1));
+    function test_RevertWhen_transferSharesFromToZeroAddress() public {
         vm.prank(USER_1);
-        usdn.transferShares(address(this), shares + 1);
+        usdn.approve(address(this), type(uint256).max);
+
+        vm.expectRevert(abi.encodeWithSelector(IERC20Errors.ERC20InvalidReceiver.selector, address(0)));
+        usdn.transferSharesFrom(USER_1, address(0), 1e36);
+    }
+
+    /**
+     * @custom:scenario Transfer shares from another user with insufficient allowance
+     * @custom:given User 1 has approved this contract to transfer 1 wei of their tokens
+     * @custom:when We try to transfer 1e20 shares from user 1 to this contract
+     * @custom:then The transaction reverts with the `ERC20InsufficientAllowance` error
+     */
+    function test_RevertWhen_transferSharesFromExceedsAllowance() public {
+        vm.prank(USER_1);
+        usdn.approve(address(this), 1);
+
+        uint256 sharesAmount = 1e20;
+        uint256 tokenAmount = usdn.convertToTokens(sharesAmount);
+        assertGt(tokenAmount, 1, "token amount");
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientAllowance.selector, address(this), 1, tokenAmount)
+        );
+        usdn.transferSharesFrom(USER_1, address(this), sharesAmount);
     }
 }
