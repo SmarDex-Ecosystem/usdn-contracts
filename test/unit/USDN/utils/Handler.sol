5766cf48186408a205dfde067931c259dede858e
beeb <703631+beeb@users.noreply.github.com>
2024-03-26 15:49:26 +0100

    test(usdn): add new shares methods to invariant testing

--- test/unit/USDN/utils/Handler.sol
+++ test/unit/USDN/utils/Handler.sol
@@ -98,7 +98,6 @@ contract UsdnHandler is Usdn, Test {
     }
 
     function transferTest(address to, uint256 value) external {
-        console2.log("bound 'to' actor ID");
         if (balanceOf(msg.sender) == 0 || to == address(0)) {
             return;
         }
@@ -114,5 +113,45 @@ contract UsdnHandler is Usdn, Test {
         _sharesHandle.set(to, toShares + valueShares);
 
         _transfer(msg.sender, to, value);
+    }
+
+    function mintSharesTest(uint256 value) external {
+        if (_totalShares >= type(uint256).max) {
+            return;
+        }
+        console2.log("bound mint shares value");
+        value = bound(value, 1, type(uint256).max - _totalShares);
+        totalSharesSum += value;
+        (, uint256 lastShares) = _sharesHandle.tryGet(msg.sender);
+        _sharesHandle.set(msg.sender, lastShares + value);
+        _updateShares(address(0), msg.sender, value, convertToTokens(value));
+    }
+
+    function burnSharesTest(uint256 value) external {
+        if (sharesOf(msg.sender) == 0) {
+            return;
+        }
+        console2.log("bound burn shares value");
+        value = bound(value, 1, sharesOf(msg.sender));
+
+        totalSharesSum -= value;
+
+        uint256 lastShares = _sharesHandle.get(msg.sender);
+        _sharesHandle.set(msg.sender, lastShares - value);
+        _burnShares(msg.sender, value, convertToTokens(value));
+    }
+
+    function transferSharesTest(address to, uint256 value) external {
+        if (sharesOf(msg.sender) == 0 || to == address(0)) {
+            return;
+        }
+        console2.log("bound transfer shares value");
+        value = bound(value, 1, sharesOf(msg.sender));
+        uint256 lastShares = _sharesHandle.get(msg.sender);
+        _sharesHandle.set(msg.sender, lastShares - value);
+        (, uint256 toShares) = _sharesHandle.tryGet(to);
+        _sharesHandle.set(to, toShares + value);
+
+        _transferShares(msg.sender, to, value, convertToTokens(value));
     }
 }
