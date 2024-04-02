09612c11693ce9aeffc5ad529a21f16fc3fc7744
Nicolas DECOSTERD <kirienzo.eth@gmail.com>
2024-04-02 15:36:19 +0200

    docs(order-manager): fix the NatSpec of the OrderManagerInvalidTick error

--- src/interfaces/OrderManager/IOrderManagerErrors.sol
+++ src/interfaces/OrderManager/IOrderManagerErrors.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 interface IOrderManagerErrors {
     /// @notice Indicates that the contract was not initialized.
     error OrderManagerNotInitialized();
-    /// @notice Indicates that the provided tick is not a multiple of the tick spacing and is therefore invalid.
+    /// @notice Indicates that the provided tick is not a multiple of the tick spacing or outside of the limits.
     error OrderManagerInvalidTick(int24 tick);
     /// @notice Indicates that the current user tried to manipulate the order of another user.
     error OrderManagerInsufficientFunds(int24 tick, address user, uint232 amountInTick, uint232 amountToWithdraw);
