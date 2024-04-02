e3ce94883109cb9ac41fae911e3f68aa5625aca6
Nicolas DECOSTERD <kirienzo.eth@gmail.com>
2024-04-02 14:41:06 +0200

    chore: revert previous commit
    This reverts commit fa67d193487f15e3c35f2450cfc55a67429988bf.

--- src/interfaces/OrderManager/IOrderManagerEvents.sol
+++ src/interfaces/OrderManager/IOrderManagerEvents.sol
@@ -5,11 +5,11 @@ interface IOrderManagerEvents {
     /**
      * @notice Emitted when a user deposit assets in a tick.
      * @param user The user that deposited the assets
-     * @param amount The amount of assets deposited
+     * @param amountInTick The amount of assets in the tick for that user
      * @param tick The desired tick to open the position in
      * @param tickVersion The version of the tick
      */
-    event UserDepositedAssetsInTick(address indexed user, uint232 amount, int24 tick, uint256 tickVersion);
+    event UserDepositedAssetsInTick(address indexed user, uint232 amountInTick, int24 tick, uint256 tickVersion);
 
     /**
      * @notice Emitted when a user withdraw assets from a tick.
