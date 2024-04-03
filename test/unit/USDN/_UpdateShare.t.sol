b34f83bd921244c9b01c08528f23610185ac3f73
lilyanb <lilyan.bastien@gmail.com>
2024-04-03 16:27:04 +0200

    refactor: transfertShare test

--- test/unit/USDN/_UpdateShare.t.sol
+++ test/unit/USDN/_UpdateShare.t.sol
@@ -86,4 +86,53 @@ contract TestUsdnUpdateShare is UsdnTokenFixture {
         assertEq(usdn.sharesOf(USER_1), type(uint256).max, "shares");
         assertEq(usdn.balanceOf(USER_1), usdn.maxTokens(), "tokens");
     }
+
+    /* -------------------------------------------------------------------------- */
+    /*                               TransferShares                               */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+     * @custom:scenario Transfer shares after a rebase
+     * @custom:given The USDN was rebased with a random divisor
+     * @custom:when The user transfers a random amount of shares to the test contract
+     * @custom:then The user's balance is decreased by the transferred amount
+     * @custom:and The contract's balance is increased by the transferred amount
+     * @custom:and The token emits a `Transfer` event with the expected values
+     * @custom:and The total shares supply remains unchanged
+     * @param divisor The rebase divisor
+     * @param sharesAmount The amount of shares to transfer
+     */
+    function testFuzz_transferShares(uint256 divisor, uint256 sharesAmount) public {
+        divisor = bound(divisor, usdn.MIN_DIVISOR(), usdn.MAX_DIVISOR());
+        if (divisor < usdn.MAX_DIVISOR()) {
+            usdn.rebase(divisor);
+        }
+
+        uint256 sharesBefore = usdn.sharesOf(USER_1);
+        uint256 totalSharesBefore = usdn.totalShares();
+        sharesAmount = bound(sharesAmount, 1, sharesBefore);
+        uint256 tokenAmount = usdn.convertToTokens(sharesAmount);
+        vm.expectEmit(address(usdn));
+        emit Transfer(USER_1, address(this), tokenAmount); // expected event
+        usdn.i_updateShares(USER_1, address(this), sharesAmount, usdn.convertToTokens(sharesAmount));
+
+        assertEq(usdn.sharesOf(USER_1), sharesBefore - sharesAmount, "balance of user");
+        assertEq(usdn.sharesOf(address(this)), sharesAmount, "balance of contract");
+        assertEq(usdn.totalShares(), totalSharesBefore, "total shares");
+    }
+
+    /**
+     * @custom:scenario Transfer more shares than the balance
+     * @custom:when The user tries to transfer more shares than they have
+     * @custom:then The transaction reverts with the USDNInsufficientSharesBalance error
+     */
+    function test_RevertWhen_transferSharesInsufficientBalance() public {
+        usdn.grantRole(usdn.MINTER_ROLE(), address(this));
+        usdn.grantRole(usdn.REBASER_ROLE(), address(this));
+        usdn.mint(USER_1, 100 ether);
+        uint256 shares = usdn.sharesOf(USER_1);
+        uint256 tokens = usdn.convertToTokens(shares + 1);
+        vm.expectRevert(abi.encodeWithSelector(UsdnInsufficientSharesBalance.selector, USER_1, shares, shares + 1));
+        usdn.i_updateShares(USER_1, address(this), shares + 1, tokens);
+    }
 }
