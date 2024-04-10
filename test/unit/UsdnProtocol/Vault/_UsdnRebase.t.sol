ddeb4b52c4b1fcff0c2386f95c119878c136d019
lilyanb <lilyan.bastien@gmail.com>
2024-04-10 09:27:49 +0200

    test: remove unused import

--- test/unit/UsdnProtocol/Vault/_UsdnRebase.t.sol
+++ test/unit/UsdnProtocol/Vault/_UsdnRebase.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.20;
 
 import { Vm } from "forge-std/Vm.sol";
-import { console2 } from "forge-std/Test.sol";
 
 import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
 import { DEPLOYER, ADMIN } from "test/utils/Constants.sol";
