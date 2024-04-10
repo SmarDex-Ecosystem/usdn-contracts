40045a19ff720e3568afe13a20012a217189fc63
beeb <703631+beeb@users.noreply.github.com>
2024-04-10 08:13:47 +0200

    test(long): add tests for `getTickLiquidationPenalty`

--- test/unit/UsdnProtocol/Long/GetTickLiquidationPenalty.t.sol
+++ test/unit/UsdnProtocol/Long/GetTickLiquidationPenalty.t.sol
@@ -18,7 +18,7 @@ contract TestUsdnProtocolGetTickLiquidationPenalty is UsdnProtocolBaseFixture {
      * @custom:when We call the function
      * @custom:then The function should return the current value of the liquidation penalty setting
      */
-    function test_getTickLiquidationPenaltyEmpty() external {
+    function test_getTickLiquidationPenaltyEmpty() public {
         uint8 startPenalty = protocol.getLiquidationPenalty();
         int24 tick = 69_420;
         TickData memory tickData = protocol.getTickData(tick);
@@ -41,7 +41,7 @@ contract TestUsdnProtocolGetTickLiquidationPenalty is UsdnProtocolBaseFixture {
      * @custom:when We call the function after changing the liquidation penalty setting
      * @custom:then The function should return the initial value as stored in the tick
      */
-    function test_getTickLiquidationPenaltyPopulated() external {
+    function test_getTickLiquidationPenaltyPopulated() public {
         uint8 startPenalty = protocol.getLiquidationPenalty();
         (int24 tick,,) = setUpUserPositionInLong(
             address(this), ProtocolAction.ValidateOpenPosition, 10 ether, params.initialPrice / 2, params.initialPrice
@@ -54,5 +54,45 @@ contract TestUsdnProtocolGetTickLiquidationPenalty is UsdnProtocolBaseFixture {
 
         // check value hasn't changed
         assertEq(protocol.getTickLiquidationPenalty(tick), startPenalty, "new value");
+    }
+
+    /**
+     * @custom:scenario Check the return value of the function when the tick was liquidated
+     * @custom:given A tick that had a position and then was liquidated
+     * @custom:when We call the function after changing the liquidation penalty setting
+     * @custom:then The function should return the new value for the liquidation penalty
+     */
+    function test_getTickLiquidationPenaltyLiquidated() public {
+        uint8 startPenalty = protocol.getLiquidationPenalty();
+        (int24 tick,,) = setUpUserPositionInLong(
+            address(this), ProtocolAction.ValidateOpenPosition, 10 ether, params.initialPrice / 2, params.initialPrice
+        );
+        assertEq(protocol.getTickLiquidationPenalty(tick), startPenalty, "tick value");
+        protocol.liquidate(abi.encode(params.initialPrice / 3), 10);
+
+        // change the penalty setting
+        vm.prank(ADMIN);
+        protocol.setLiquidationPenalty(startPenalty - 1);
+
+        // the tick has now the new value
+        assertEq(protocol.getTickLiquidationPenalty(tick), startPenalty - 1, "new value");
+    }
+
+    /**
+     * @custom:scenario Check the return value of the function when the tick had a position that is now closed
+     * @custom:given A tick that had a position, but the position was later closed
+     * @custom:when We call the function after changing the liquidation penalty setting
+     * @custom:then The function should return the new value for the liquidation penalty
+     */
+    function test_getTickLiquidationPenaltyWasPopulatedNowEmpty() public {
+        uint8 startPenalty = protocol.getLiquidationPenalty();
+        (int24 tick,,) = setUpUserPositionInLong(
+            address(this), ProtocolAction.ValidateClosePosition, 10 ether, params.initialPrice / 2, params.initialPrice
+        );
+        // change the penalty setting
+        vm.prank(ADMIN);
+        protocol.setLiquidationPenalty(startPenalty - 1);
+
+        assertEq(protocol.getTickLiquidationPenalty(tick), startPenalty - 1, "final value");
     }
 }
