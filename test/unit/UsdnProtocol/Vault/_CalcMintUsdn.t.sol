b4b4ca52a4a8c9f30b808e855b5a866385a5bdec
lilyanb <lilyan.bastien@gmail.com>
2024-04-15 08:19:15 +0200

    test: refacto natspec

--- test/unit/UsdnProtocol/Vault/_CalcMintUsdn.t.sol
+++ test/unit/UsdnProtocol/Vault/_CalcMintUsdn.t.sol
@@ -15,13 +15,13 @@ contract TestUsdnProtocolCalcMintUsdn is UsdnProtocolBaseFixture {
     /**
      * @custom:scenario Compare calculations of `_calcMintUsdn` with more precise
      * values and vaultBalance equal to zero
-     * @custom:given A amount between 1 and `type(uint128).max`
+     * @custom:given An amount between 1 and `type(uint128).max`
      * @custom:and A price between 1 and `type(uint128).max`
      * @custom:and A decimals value calculated from the protocol
      * @custom:when The `_calcMintUsdn` function is called with the value
      * @custom:then The result is equal to the result of the Rust implementation
      * @param amount The amount of asset to be converted into USDN
-     * @param price The price of the asset (only used for initialization)
+     * @param price The price of the asset
      */
     function testFuzzFFI_calcMintUsdnVaultBalanceZero(uint256 amount, uint256 price) public {
         amount = bound(amount, 1, type(uint128).max);
@@ -49,14 +49,14 @@ contract TestUsdnProtocolCalcMintUsdn is UsdnProtocolBaseFixture {
 
     /**
      * @custom:scenario Compare calculations of `_calcMintUsdn` with more precise values
-     * @custom:given A amount between 1 and `type(uint128).max`
+     * @custom:given An amount between 1 and `type(uint128).max`
      * @custom:and A vaultBalance between 1 and `type(uint128).max`
      * @custom:and A usdnTotalSupply between 1 and `type(uint128).max`
      * @custom:when The `_calcMintUsdn` function is called with the value
      * @custom:then The result is equal to the result of the Rust implementation
      * @param amount The amount of asset to be converted into USDN
-     * @param vaultBalance The balance of the vault (not used for initialization)
-     * @param usdnTotalSupply The total supply of USDN (not used for initialization)
+     * @param vaultBalance The balance of the vault
+     * @param usdnTotalSupply The total supply of USDN
      */
     function testFuzzFFI_calcMintUsdn(uint256 amount, uint256 vaultBalance, uint256 usdnTotalSupply) public {
         amount = bound(amount, 1, type(uint128).max);
