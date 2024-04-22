d14d9369e7c2f3b4ea12bc41bce274b1d374471d
lilyanb <lilyan.bastien@gmail.com>
2024-04-22 08:35:08 +0200

    test: refacto natspec

--- test/unit/UsdnProtocol/Long/GetEffectiveTickForPrice.t.sol
+++ test/unit/UsdnProtocol/Long/GetEffectiveTickForPrice.t.sol
@@ -5,7 +5,7 @@ import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
 import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
 
-/// @custom:feature Test the getEffectiveTickForPrice public function of the UsdnProtocolLong contract.
+/// @custom:feature Test the getEffectiveTickForPrice public function of the UsdnProtocolLong contract
 contract TestUsdnProtocolLongGetEffectiveTickForPrice is UsdnProtocolBaseFixture {
     function setUp() public {
         super._setUp(DEFAULT_PARAMS);
@@ -25,14 +25,28 @@ contract TestUsdnProtocolLongGetEffectiveTickForPrice is UsdnProtocolBaseFixture
         // price = 10000 * liqMultiplier / 10e38 = 20000000
         uint128 price = 20_000_000;
         int24 minTick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
-        assertEq(minTick, expectedMinTick, "wrong value of minTick");
+        assertEq(minTick, expectedMinTick, "first tick should be equal to minTick");
 
         /* ---------------- priceWithMultiplier < TickMath.MIN_PRICE ---------------- */
         liqMultiplier = 2000 * 10e38;
         // price = 9999 * liqMultiplier / 10e38 = 199998000
         price = 199_998_000;
         minTick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
-        assertEq(minTick, expectedMinTick, "wrong value of minTick");
+        assertEq(minTick, expectedMinTick, "second tick should be equal to minTick");
+    }
+
+    /**
+     * @custom:scenario Call `getEffectiveTickForPrice` and return expected minUsableTick < tick_ < 0
+     * @custom:given A price and liqMultiplier
+     * @custom:when getEffectiveTickForPrice is called
+     * @custom:then The function should return expected tick_
+     */
+    function test_getEffectiveTickForPriceTickLowerThanZero() external {
+        /* ------------------------ minUsableTick < tick_ < 0 ----------------------- */
+        uint256 liqMultiplier = 2000 * 10e32;
+        uint128 price = 1_999_980_000;
+        int24 tick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
+        assertEq(tick, -161_200, "tick should be -161200");
     }
 
     /**
@@ -42,22 +56,19 @@ contract TestUsdnProtocolLongGetEffectiveTickForPrice is UsdnProtocolBaseFixture
      * @custom:then The function should return expected tick_
      */
     function test_getEffectiveTickForPriceTickGreaterThanOrEqualZero() external {
-        /* ---------------- tick_ = 0 ---------------- */
+        /* -------------------------------- tick_ = 0 ------------------------------- */
         uint256 liqMultiplier = 1999 * 10e25;
-        // price = 9999 * liqMultiplier / 10e38 = 199998000
         uint128 price = 1_999_980_000;
         int24 tick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
         assertEq(tick, 0, "tick should be 0 with liqMultiplier 1999 * 10e25");
 
         liqMultiplier = 1981 * 10e25;
-        // price = 9999 * liqMultiplier / 10e38 = 199998000
         price = 1_999_980_000;
         tick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
         assertEq(tick, 0, "tick should be 0 with liqMultiplier 1981 * 10e25");
 
-        /* ---------------- tick_ > 0 ---------------- */
+        /* -------------------------------- tick_ > 0 ------------------------------- */
         liqMultiplier = 2000 * 10e10;
-        // price = 10000 * liqMultiplier / 10e38 = 20000000
         price = 2_000_000_000;
         tick = protocol.getEffectiveTickForPrice(price, liqMultiplier);
         assertEq(tick, 345_400, "tick should be 345400");
