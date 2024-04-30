b8bd27d01b6c811180d226762e6efedb5370ebce
samooyo <samidarnaud@gmail.com>
2024-04-30 12:22:17 +0200

    docs: update natspecks

--- src/interfaces/OracleMiddleware/ILiquidationRewardsManagerErrorsEventsTypes.sol
+++ src/interfaces/OracleMiddleware/ILiquidationRewardsManagerErrorsEventsTypes.sol
@@ -7,12 +7,12 @@ interface ILiquidationRewardsManagerErrorsEventsTypes {
     /* -------------------------------------------------------------------------- */
 
     /**
-     * @notice Emitted when the rewards parameters are changed.
-     * @param gasUsedPerTick Gas used per tick to liquidate.
-     * @param otherGasUsed Gas used for the rest of the computation.
-     * @param rebaseGasUsed Gas used for the optional USDN rebase.
-     * @param gasPriceLimit Upper limit for the gas price.
-     * @param multiplierBps Multiplier for the liquidators.
+     * @notice Emitted when the rewards parameters are changed
+     * @param gasUsedPerTick Gas used per tick to liquidate
+     * @param otherGasUsed Gas used for the rest of the computation
+     * @param rebaseGasUsed Gas used for the optional USDN rebase
+     * @param gasPriceLimit Upper limit for the gas price
+     * @param multiplierBps Multiplier for the liquidators
      */
     event RewardsParametersUpdated(
         uint32 gasUsedPerTick, uint32 otherGasUsed, uint32 rebaseGasUsed, uint64 gasPriceLimit, uint32 multiplierBps
@@ -23,33 +23,52 @@ interface ILiquidationRewardsManagerErrorsEventsTypes {
     /* -------------------------------------------------------------------------- */
 
     /**
-     * @notice Parameters for the rewards calculation.
-     * @param gasUsedPerTick Gas used per tick to liquidate.
-     * @param otherGasUsed Gas used for the rest of the computation.
-     * @param rebaseGasUsed Gas used for the optional USDN rebase.
-     * @param gasPriceLimit Upper limit for the gas price.
-     * @param multiplierBps Multiplier basis points for the liquidator rewards.
+     * @notice Parameters for the rewards calculation
+     * @param gasUsedPerTick Gas used per tick to liquidate
+     * @param otherGasUsed Gas used for the rest of the computation
+     * @param rebaseGasUsed Gas used for the optional USDN rebase
+     * @param gasPriceLimit Upper limit for the gas price
+     * @param multiplierBps Multiplier basis points for the liquidator rewards
      */
     struct RewardsParameters {
         uint32 gasUsedPerTick;
         uint32 otherGasUsed;
         uint32 rebaseGasUsed;
         uint64 gasPriceLimit;
-        uint32 multiplierBps; // to be divided by BPS_DIVISOR
+        uint32 multiplierBps;
     }
 
     /* -------------------------------------------------------------------------- */
     /*                                   Errors                                   */
     /* -------------------------------------------------------------------------- */
 
-    /// @dev Indicates that the gasUsedPerTick parameter has been set to a value we consider too high.
+    /**
+     * @notice Indicates that the gasUsedPerTick parameter has been set to a value we consider too high
+     * @param value The wanted value
+     */
     error LiquidationRewardsManagerGasUsedPerTickTooHigh(uint256 value);
-    /// @dev Indicates that the otherGasUsed parameter has been set to a value we consider too high.
+
+    /**
+     * @notice Indicates that the otherGasUsed parameter has been set to a value we consider too high
+     * @param value The wanted value
+     */
     error LiquidationRewardsManagerOtherGasUsedTooHigh(uint256 value);
-    /// @dev Indicates that the rebaseGasUsed parameter has been set to a value we consider too high.
+
+    /**
+     * @notice Indicates that the rebaseGasUsed parameter has been set to a value we consider too high
+     * @param value The wanted value
+     */
     error LiquidationRewardsManagerRebaseGasUsedTooHigh(uint256 value);
-    /// @dev Indicates that the gasPriceLimit parameter has been set to a value we consider too high.
+
+    /**
+     * @notice Indicates that the gasPriceLimit parameter has been set to a value we consider too high
+     * @param value The wanted value
+     */
     error LiquidationRewardsManagerGasPriceLimitTooHigh(uint256 value);
-    /// @dev Indicates that the multiplierBps parameter has been set to a value we consider too high.
+
+    /**
+     * @notice Indicates that the multiplierBps parameter has been set to a value we consider too high
+     * @param value The wanted value
+     */
     error LiquidationRewardsManagerMultiplierBpsTooHigh(uint256 value);
 }
