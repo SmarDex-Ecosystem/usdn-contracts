adff4d9811a92f7a8dc31be7a6a71e8fe40c5797
lilyanb <lilyan.bastien@gmail.com>
2024-04-30 11:58:13 +0200

    test: refacto

--- test/unit/UsdnProtocol/Vault/_CalcBurnUsdn.t.sol
+++ test/unit/UsdnProtocol/Vault/_CalcBurnUsdn.t.sol
@@ -3,31 +3,27 @@ pragma solidity 0.8.20;
 
 import { UsdnProtocolBaseFixture } from "test/unit/UsdnProtocol/utils/Fixtures.sol";
 
-import { ProtocolAction } from "src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
-
 /**
  * @custom:feature The _calcBurnUsdn internal function of the UsdnProtocolVault contract.
  * @custom:background Given a protocol instance that was initialized with default params
- * @custom:and A user who deposited 1 wstETH at price $2000 to get 2000 USDN
  */
 contract TestUsdnProtocolVaultCalcBurnUsdn is UsdnProtocolBaseFixture {
     function setUp() public {
         super._setUp(DEFAULT_PARAMS);
-        usdn.approve(address(protocol), type(uint256).max);
     }
 
     /**
      * @custom:scenario Check calculation asset received when withdraw
      * @custom:given A protocol initialized with default params
      * @custom:when The function "_calcBurnUsdn" is called with some parameters
-     * @custom:then The total supply is calculated correctly
+     * @custom:then The asset amount is calculated correctly
      */
     function test_calcBurnUsdn() public {
         uint256 usdnShares = 10 ether;
         uint256 available = 10 ether;
         uint256 usdnTotalShares = 10 ether;
         uint256 assetExpected = protocol.i_calcBurnUsdn(usdnShares, available, usdnTotalShares);
-        assertEq(assetExpected, 10_000_000_000_000_000_000, "assetExpected must match with expected value");
+        assertEq(assetExpected, 10 ether, "assetExpected must match with expected value (10 ether)");
 
         usdnShares = 787 ether;
         available = 28 ether;
