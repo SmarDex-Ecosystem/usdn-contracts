e92be5d433ab2d17ba728daea6e15d860c6a2e5a
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2024-04-30 23:11:26 +0900

    feat(oracle-middleware)!: make the oracle middleware revert if the validation cost is not exact (#217)
    * feat(oracle)!: revert in middleware if the value sent is not strictly equal to the validation cost
    
    BREAKING CHANGE: error OracleMiddlewareInsufficientFee has been renamed OracleMiddlewareIncorrectFee
    
    * feat(oracle): revert the TX if value is sent for a chainlink call
    
    * feat(middleware): add a function to withdraw the ether from the middleware if funds gets stuck
    
    * test: use the validation cost instead of arbitrary value in the tests
    
    * test(mock-middleware): add missing revert on transfer fail
    
    * docs: fix natspec smells in IOracleMiddlewareErrors
    
    * feat(oracle-middleware): add a to parameter to the withdrawEther function
    
    * feat: make withdrawEther revert if the to parameter is the zero address
    
    * docs: fix typos and format in Natspec/comments
    
    * test(validation-fee): add sanity check to avoid an underflow in a test

--- src/OracleMiddleware/LiquidationRewardsManager.sol
+++ src/OracleMiddleware/LiquidationRewardsManager.sol
@@ -44,9 +44,9 @@ contract LiquidationRewardsManager is ILiquidationRewardsManager, ChainlinkOracl
     {
         _wstEth = wstETH;
         _rewardsParameters = RewardsParameters({
-            gasUsedPerTick: 32_107,
-            otherGasUsed: 384_487,
-            rebaseGasUsed: 8897,
+            gasUsedPerTick: 32_544,
+            otherGasUsed: 380_690,
+            rebaseGasUsed: 8881,
             gasPriceLimit: 1000 gwei,
             multiplierBps: 20_000
         });
