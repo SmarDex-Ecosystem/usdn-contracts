fa36f7be8e9ca8981a10bd945d59d4ccf82a2eb8
Nicolas DECOSTERD <kirienzo.eth@gmail.com>
2024-05-03 12:27:47 +0200

    refactor(init-open): do the max/min leverage check as early as possible

--- src/UsdnProtocol/UsdnProtocolActions.sol
+++ src/UsdnProtocol/UsdnProtocolActions.sol
@@ -819,9 +819,10 @@ abstract contract UsdnProtocolActions is IUsdnProtocolActions, UsdnProtocolLong 
         // remove liquidation penalty for leverage and total expo calculations
         uint128 liqPriceWithoutPenalty =
             getEffectivePriceForTick(_calcTickWithoutPenalty(data_.posId.tick, data_.liquidationPenalty));
-        data_.positionTotalExpo = _calculatePositionTotalExpo(amount, data_.adjustedPrice, liqPriceWithoutPenalty);
 
         _checkOpenPositionLeverage(data_.adjustedPrice, liqPriceWithoutPenalty);
+        data_.positionTotalExpo = _calculatePositionTotalExpo(amount, data_.adjustedPrice, liqPriceWithoutPenalty);
+
         _checkImbalanceLimitOpen(data_.positionTotalExpo, amount);
     }
 
