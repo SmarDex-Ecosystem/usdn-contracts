4438d93da08470e35f1ce39531db9059ecd5b5d7
lilyanb <lilyan.bastien@gmail.com>
2024-05-13 13:23:03 +0200

    test: remove unused imports

--- test/unit/UsdnProtocol/Actions/ValidateClosePosition.t.sol
+++ test/unit/UsdnProtocol/Actions/ValidateClosePosition.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-import { console2 } from "forge-std/Test.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
