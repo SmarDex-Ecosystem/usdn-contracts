be3ddab90b11bfb260ddb75a6712f31fd58b6f57
kirienzo-eth <kirienzo.eth@gmail.com>
2024-05-15 10:22:18 +0200

    docs(order-manager): fix NatSpec smells in IOrderManager

--- src/OrderManager/OrderManager.sol
+++ src/OrderManager/OrderManager.sol
@@ -36,13 +36,13 @@ contract OrderManager is Ownable, IOrderManager {
     }
 
     /// @inheritdoc IOrderManager
-    function getUsdnProtocol() external view returns (IUsdnProtocol) {
-        return _usdnProtocol;
+    function getUsdnProtocol() external view returns (IUsdnProtocol usdnProtocol_) {
+        usdnProtocol_ = _usdnProtocol;
     }
 
     /// @inheritdoc IOrderManager
-    function getPositionVersion() external view returns (uint128) {
-        return _positionVersion;
+    function getPositionVersion() external view returns (uint128 positionVersion_) {
+        positionVersion_ = _positionVersion;
     }
 
     /// @inheritdoc IOrderManager
