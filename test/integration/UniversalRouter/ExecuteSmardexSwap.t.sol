0210aad46e1393189bb8ec5d922e7c96ebfaa1ed
fireboss777 <fireboss@tutanota.com>
2024-05-21 18:21:12 +0200

    test: simple exact in

--- test/integration/UniversalRouter/ExecuteSmardexSwap.t.sol
+++ test/integration/UniversalRouter/ExecuteSmardexSwap.t.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-// import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Constants } from "@uniswap/universal-router/contracts/libraries/Constants.sol";
 
 import { Commands } from "src/UniversalRouter/libraries/Commands.sol";
 
 import { UniversalRouterBaseIntegrationFixture } from "test/integration/UniversalRouter/utils/Fixtures.sol";
-
-import { DEPLOYER, WETH, SDEX } from "test/utils/Constants.sol";
+import { WETH, SDEX } from "test/utils/Constants.sol";
 
 /**
  * @custom:feature Test smardex swap commands
@@ -32,5 +31,22 @@ contract TestExecuteSmardexSwap is UniversalRouterBaseIntegrationFixture {
      * @custom:then The `` command should be executed
      * @custom:and The weth user balance should be increased
      */
-    function test_execute_smardex_swap_exact_in() external { }
+    function test_execute_smardex_swap_exact_in() external {
+        // commands
+        bytes memory commands = abi.encodePacked(bytes1(bytes32(Commands.SMARDEX_SWAP_EXACT_IN) << (256 - 8)));
+
+        // inputs
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(address(this), BASE_AMOUNT, 0, abi.encodePacked(SDEX, WETH), false);
+
+        // transfer
+        sdex.transfer(address(router), BASE_AMOUNT);
+        uint256 balanceWethBefore = IERC20(WETH).balanceOf(address(this));
+
+        // execution
+        router.execute(commands, inputs);
+
+        // assert
+        assertGt(IERC20(WETH).balanceOf(address(this)), balanceWethBefore, "wrong weth balance");
+    }
 }
