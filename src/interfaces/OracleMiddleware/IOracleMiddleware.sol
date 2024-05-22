4ca830c2798898a9d09ad122e8281ebf5689f9c5
Valentin B <703631+beeb@users.noreply.github.com>
2024-05-22 11:22:15 +0200

    refactor(middleware): separate immutable part of the interface to another file (#275)

--- src/interfaces/OracleMiddleware/IOracleMiddleware.sol
+++ src/interfaces/OracleMiddleware/IOracleMiddleware.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { ProtocolAction } from "src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
-import { PriceInfo } from "src/interfaces/OracleMiddleware/IOracleMiddlewareTypes.sol";
+import { IBaseOracleMiddleware } from "src/interfaces/OracleMiddleware/IBaseOracleMiddleware.sol";
 import { IOracleMiddlewareErrors } from "src/interfaces/OracleMiddleware/IOracleMiddlewareErrors.sol";
 import { IOracleMiddlewareEvents } from "src/interfaces/OracleMiddleware/IOracleMiddlewareEvents.sol";
 
@@ -11,7 +10,7 @@ import { IOracleMiddlewareEvents } from "src/interfaces/OracleMiddleware/IOracle
  * @notice The oracle middleware is a contract that is called by the USDN protocol to validate price data. Using a
  * middleware allows the protocol to later upgrade to a new oracle logic without having modify the vault contract
  */
-interface IOracleMiddleware is IOracleMiddlewareErrors, IOracleMiddlewareEvents {
+interface IOracleMiddleware is IBaseOracleMiddleware, IOracleMiddlewareErrors, IOracleMiddlewareEvents {
     /* -------------------------------------------------------------------------- */
     /*                                  Constants                                 */
     /* -------------------------------------------------------------------------- */
@@ -29,56 +28,14 @@ interface IOracleMiddleware is IOracleMiddlewareErrors, IOracleMiddlewareEvents 
     function MAX_CONF_RATIO() external pure returns (uint16);
 
     /* -------------------------------------------------------------------------- */
-    /*                          Price retrieval features                          */
-    /* -------------------------------------------------------------------------- */
-
-    /**
-     * @notice Parse and validate some price data
-     * @dev The data format is specific to the middleware and is simply forwarded from the user transaction's calldata
-     * A fee amounting to exactly validationCost(data, action) must be sent or the transaction will revert
-     * @param targetTimestamp The target timestamp for validating the price data. For validation actions, this is the
-     * timestamp of the initiation
-     * @param action Type of action for which the price is requested. The middleware may use this to alter the
-     * validation of the price or the returned price
-     * @param data Price data, the format varies from middleware to middleware and can be different depending on the
-     * action
-     * @return result_ The price and timestamp as `PriceInfo`
-     */
-    function parseAndValidatePrice(uint128 targetTimestamp, ProtocolAction action, bytes calldata data)
-        external
-        payable
-        returns (PriceInfo memory result_);
-
-    /* -------------------------------------------------------------------------- */
     /*                              Generic features                              */
     /* -------------------------------------------------------------------------- */
-
-    /**
-     * @notice Get the required delay (in seconds) between the moment an action is initiated and the timestamp of the
-     * price data used to validate that action
-     * @return The validation delay
-     */
-    function getValidationDelay() external view returns (uint256);
-
-    /**
-     * @notice Returns the number of decimals for the price (constant)
-     * @return The number of decimals
-     */
-    function getDecimals() external view returns (uint8);
 
     /**
      * @notice Return the confidence ratio. This ratio is used to apply a specific portion of the confidence interval
      * provided by an oracle, which is used to adjust the precision of predictions or estimations
      */
     function getConfRatioBps() external view returns (uint16);
-
-    /**
-     * @notice Returns the ETH cost of one price validation for the given action
-     * @param data Pyth price data to be validated for which to get fee prices
-     * @param action Type of action for which the price is requested
-     * @return The ETH cost of one price validation
-     */
-    function validationCost(bytes calldata data, ProtocolAction action) external view returns (uint256);
 
     /* -------------------------------------------------------------------------- */
     /*                               Owner features                               */
