9e6daeb0574a4466e2393ef63867f0f55dcfa97b
lilyanb <lilyan.bastien@gmail.com>
2024-05-27 12:27:46 +0200

    test: add test router keep tokens

--- test/integration/UniversalRouter/ExecuteStETH.sol
+++ test/integration/UniversalRouter/ExecuteStETH.sol
@@ -62,6 +62,41 @@ contract TestForkUniversalRouterExecuteStETH is ForkUniversalRouterBaseIntegrati
     }
 
     /**
+     * @custom:scenario Test the `WRAP_STETH` command using the router balance
+     * @custom:given The initiated universal router
+     * @custom:and The router should be funded with some `stETH`
+     * @custom:when The `execute` function is called for `WRAP_STETH` command
+     * @custom:then The `WRAP_STETH` command should be executed
+     * @custom:and The `wsteth` router balance should be increased
+     */
+    function test_executeWrapStETHForRouter() external {
+        // unwrap
+        wstETH.unwrap(BASE_AMOUNT);
+        stETH.transferShares(address(router), stETH.sharesOf(address(this)));
+
+        // commands
+        bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.WRAP_STETH)));
+
+        // inputs
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(Constants.ADDRESS_THIS);
+
+        // execution
+        router.execute(commands, inputs);
+
+        // assert
+        assertApproxEqAbs(
+            wstETH.balanceOf(address(router)),
+            stETH.getPooledEthByShares(stETH.getSharesByPooledEth(BASE_AMOUNT)),
+            1,
+            "wrong wstETH balance(router)"
+        );
+        assertApproxEqAbs(stETH.sharesOf(address(router)), 0, 1, "wrong stETH balance(router)");
+        assertEq(wstETH.balanceOf(address(this)), 0, "wrong wstETH balance(user)");
+        assertEq(stETH.sharesOf(address(this)), 0, "wrong stETH balance(user)");
+    }
+
+    /**
      * @custom:scenario Test the `UNWRAP_WSTETH` command using the router balance
      * @custom:given The initiated universal router
      * @custom:and The router should be funded with some `wstETH`
@@ -92,6 +127,39 @@ contract TestForkUniversalRouterExecuteStETH is ForkUniversalRouterBaseIntegrati
         );
         assertEq(stETH.sharesOf(address(router)), 0, "wrong stETH balance(router)");
         assertEq(wstETH.balanceOf(address(router)), 0, "wrong wstETH balance(router)");
+    }
+
+    /**
+     * @custom:scenario Test the `UNWRAP_WSTETH` command using the router balance
+     * @custom:given The initiated universal router
+     * @custom:and The router should be funded with some `wstETH`
+     * @custom:when The `execute` function is called for `UNWRAP_WSTETH` command
+     * @custom:then The `UNWRAP_WSTETH` command should be executed
+     * @custom:and The `stETH` router balance should be increased
+     */
+    function test_executeUnwrapStETHForRouter() external {
+        // transfer
+        wstETH.transfer(address(router), BASE_AMOUNT);
+        uint256 sharesOfStETHBefore = stETH.sharesOf(address(this));
+
+        // commands
+        bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.UNWRAP_WSTETH)));
+
+        // inputs
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(Constants.ADDRESS_THIS, stETH.getPooledEthByShares(BASE_AMOUNT));
+
+        // execution
+        router.execute(commands, inputs);
+
+        // assert
+        assertEq(
+            stETH.sharesOf(address(router)),
+            sharesOfStETHBefore + stETH.getSharesByPooledEth(stETH.getPooledEthByShares(BASE_AMOUNT)),
+            "wrong stETH balance(router)"
+        );
+        assertEq(stETH.sharesOf(address(this)), 0, "wrong stETH balance(user)");
+        assertEq(wstETH.balanceOf(address(this)), 0, "wrong wstETH balance(user)");
     }
 
     /**
