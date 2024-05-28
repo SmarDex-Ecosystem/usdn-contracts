c3b350c79760c735a1b2ce9b14c7d7d9763fa4a6
lilyanb <lilyan.bastien@gmail.com>
2024-05-28 15:50:45 +0200

    feat: change comments

--- src/UniversalRouter/base/Dispatcher.sol
+++ src/UniversalRouter/base/Dispatcher.sol
@@ -198,6 +198,10 @@ abstract contract Dispatcher is Payments, V2SwapRouter, V3SwapRouter, UsdnProtoc
                         }
                     }
                 } else {
+                    // Comment for the eights actions(INITIATE and VALIDATE) of the USDN protocol.
+                    // We don't allow the transaction to revert if the actions was not successful (due to pending
+                    // liquidations), so we ignore the success boolean. This is because it's important to perform
+                    // liquidations if they are needed, and it would be a big waste of gas for the user to revert
                     if (command == Commands.INITIATE_DEPOSIT) {
                         (
                             uint256 amount,
@@ -206,9 +210,6 @@ abstract contract Dispatcher is Payments, V2SwapRouter, V3SwapRouter, UsdnProtoc
                             bytes memory currentPriceData,
                             PreviousActionsData memory previousActionsData
                         ) = abi.decode(inputs, (uint256, address, address, bytes, PreviousActionsData));
-                        // we don't allow the transaction to revert if the deposit was not successful (due to pending
-                        // liquidations), so we ignore the success boolean. This is because it's important to perform
-                        // liquidations if they are needed, and it would be a big waste of gas for the user to revert
                         _usdnInitiateDeposit(amount, map(to), map(validator), currentPriceData, previousActionsData);
                     } else if (command == Commands.INITIATE_WITHDRAWAL) {
                         (
