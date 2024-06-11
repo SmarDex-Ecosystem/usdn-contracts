8107ea27acbcb44014195bab6297062f8c57d7c3
lilyanb <lilyan.bastien@gmail.com>
2024-06-11 13:28:27 +0200

    fix: two detector

--- src/Rebalancer/Rebalancer.sol
+++ src/Rebalancer/Rebalancer.sol
@@ -43,7 +43,7 @@ contract Rebalancer is Ownable, ERC165, IOwnershipCallback, IRebalancer {
     IUsdnProtocol internal immutable _usdnProtocol;
 
     /// @notice The current position version
-    // slither-disable-next-line constable-states
+    // slither-disable-next-line uninitialized-state,constable-states
     uint128 internal _positionVersion;
 
     /// @notice The amount of assets waiting to be used in the next version of the position
@@ -53,7 +53,7 @@ contract Rebalancer is Ownable, ERC165, IOwnershipCallback, IRebalancer {
     uint256 internal _maxLeverage;
 
     /// @notice The version of the last position that got liquidated
-    // slither-disable-next-line constable-states
+    // slither-disable-next-line uninitialized-state,constable-states
     uint128 internal _lastLiquidatedVersion;
 
     /// @notice The minimum amount of assets to be deposited by a user
