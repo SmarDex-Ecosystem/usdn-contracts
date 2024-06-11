409a20543bcadd93ee560ba80ca137e6d7421fb5
lilyanb <lilyan.bastien@gmail.com>
2024-06-11 11:45:27 +0200

    fix: uninitialized-state

--- src/Rebalancer/Rebalancer.sol
+++ src/Rebalancer/Rebalancer.sol
@@ -43,6 +43,7 @@ contract Rebalancer is Ownable, ERC165, IOwnershipCallback, IRebalancer {
     IUsdnProtocol internal immutable _usdnProtocol;
 
     /// @notice The current position version
+    // slither-disable-next-line uninitialized-state
     uint128 internal _positionVersion;
 
     /// @notice The amount of assets waiting to be used in the next version of the position
@@ -52,6 +53,7 @@ contract Rebalancer is Ownable, ERC165, IOwnershipCallback, IRebalancer {
     uint256 internal _maxLeverage;
 
     /// @notice The version of the last position that got liquidated
+    // slither-disable-next-line uninitialized-state
     uint128 internal _lastLiquidatedVersion;
 
     /// @notice The minimum amount of assets to be deposited by a user
