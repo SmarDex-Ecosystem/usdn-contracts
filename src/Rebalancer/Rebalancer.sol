d29b39289945ceca8327bfedd1c3e777dbdc4ef0
Alfred GAILLARD <a.gaillard@ra2.tech>
2024-06-13 10:34:42 +0200

    fix(rebalancer): double import

--- src/Rebalancer/Rebalancer.sol
+++ src/Rebalancer/Rebalancer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
