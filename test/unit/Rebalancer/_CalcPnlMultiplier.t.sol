8d2c2d61e201b142dc05ba4bebfebd5ede47b114
kirienzo-eth <kirienzo.eth@gmail.com>
2024-06-13 13:05:14 +0200

    refactor: use relative path for improts in new test files

--- test/unit/Rebalancer/_CalcPnlMultiplier.t.sol
+++ test/unit/Rebalancer/_CalcPnlMultiplier.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import { RebalancerFixture } from "test/unit/Rebalancer/utils/Fixtures.sol";
+import { RebalancerFixture } from "./utils/Fixtures.sol";
 
 /**
  * @custom:feature The {_calcPnlMultiplier} function of the rebalancer contract
