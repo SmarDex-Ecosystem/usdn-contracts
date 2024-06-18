34d83195b08b357750c11c3b4d3f273a8096ddb1
fireboss777 <fireboss@tutanota.com>
2024-06-18 16:33:59 +0200

    chore: typo

--- src/interfaces/Rebalancer/IRebalancerErrors.sol
+++ src/interfaces/Rebalancer/IRebalancerErrors.sol
@@ -5,7 +5,7 @@ interface IRebalancerErrors {
     /// @dev Indicates that the user assets are used in a position
     error RebalancerUserNotPending();
 
-    /// @dev IndicatesIndicates that the user assets are not used in a position
+    /// @dev Indicates that the user assets are not used in a position
     error RebalancerUserPending();
 
     /// @dev Indicates that the `to` address is invalid
