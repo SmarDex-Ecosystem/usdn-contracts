bae05e71d30cd21dcabda6b81c2d3cd38c92bcac
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2024-06-18 16:26:29 +0200

    test: add tests for _calcImbalanceOpenBps (#355)
    * test: add tests for _calcImbalanceOpenBps
    
    * test(calc-imbalance): fix the boundaries of the fuzzing tests
    
    * docs: fix the NatSpec in the 0 vault balance test of _calcImbalanceOpenBps
    
    * fix: fix inconsistency between NatSpec and checks
    
    * test: improve NatSpecs base on feedback

---
+++ test/unit/UsdnProtocol/Long/_CalcImbalanceOpenBps.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import { UsdnProtocolBaseFixture } from "../utils/Fixtures.sol";
+
+/**
+ * @custom:feature Test the {_calcImbalanceOpenBps} internal function of the long layer
+ * @custom:background An initialized usdn protocol contract with default parameters
+ */
+contract TestUsdnProtocolLongCalcImbalanceOpenBps is UsdnProtocolBaseFixture {
+    function setUp() public {
+        super._setUp(DEFAULT_PARAMS);
+    }
+
+    /**
+     * @custom:scenario Calculate the imbalance with no trading expo
+     * @custom:when The vault balance is 0
+     * @custom:and The long balance is 100 ether
+     * @custom:and The total expo is 200 ether
+     * @custom:then The imbalance is infinite
+     * @custom:and int256.max is returned
+     */
+    function test_calcImbalanceOpenBpsWith0VaultBalance() public {
+        int256 vaultBalance = 0;
+        int256 longBalance = 100 ether;
+        uint256 totalExpo = 200 ether;
+        int256 imbalanceBps = protocol.i_calcImbalanceOpenBps(vaultBalance, longBalance, totalExpo);
+        assertEq(imbalanceBps, type(int256).max, "The imbalance should be infinite (int256.max)");
+    }
+
+    /**
+     * @custom:scenario Calculate the imbalance with different values
+     * @custom:given The long balance is 100 ether
+     * @custom:and The total expo is 300 ether
+     * @custom:when The vault balance is 200 ether
+     * @custom:then The imbalance is 0
+     * @custom:when The vault balance is 133.33 ether
+     * @custom:then The imbalance is 50%
+     * @custom:when The vault balance is 300 ether
+     * @custom:then The imbalance is -50%
+     */
+    function test_calcImbalanceOpenBps() public {
+        int256 vaultBalance = 200 ether;
+        int256 longBalance = 100 ether;
+        uint256 totalExpo = 300 ether;
+        int256 imbalanceBps = protocol.i_calcImbalanceOpenBps(vaultBalance, longBalance, totalExpo);
+        assertEq(imbalanceBps, 0, "The imbalance should be 0");
+
+        vaultBalance = 133_333_333_333_333_333_333;
+        imbalanceBps = protocol.i_calcImbalanceOpenBps(vaultBalance, longBalance, totalExpo);
+        assertEq(imbalanceBps, int256(BPS_DIVISOR / 2), "The imbalance should be 50%");
+
+        vaultBalance = 400 ether;
+        imbalanceBps = protocol.i_calcImbalanceOpenBps(vaultBalance, longBalance, totalExpo);
+        assertEq(imbalanceBps, -int256(BPS_DIVISOR / 2), "The imbalance should be -50%");
+    }
+
+    /**
+     * @custom:scenario Calculate the imbalance with different values
+     * @custom:when The vault balance is between 1 and int256.max / BPS_DIVISOR
+     * @custom:and The long balance is between 0 and int256.max / BPS_DIVISOR
+     * @custom:and The total expo is between the long balance and int256.max / BPS_DIVISOR
+     * @custom:then The returned imbalance is equal to the expected one
+     * @param vaultBalance The vault balance
+     * @param longBalance The long balance
+     * @param totalExpo The total expo
+     */
+    function testFuzz_calcImbalanceOpenBps(uint256 vaultBalance, uint256 longBalance, uint256 totalExpo) public {
+        vaultBalance = bound(vaultBalance, 1, uint256(type(int256).max) / BPS_DIVISOR);
+        longBalance = bound(longBalance, 0, uint256(type(int256).max) / BPS_DIVISOR);
+        totalExpo = bound(totalExpo, longBalance, uint256(type(int256).max) / BPS_DIVISOR);
+
+        int256 expectedImbalance =
+            (int256(totalExpo - longBalance) - int256(vaultBalance)) * int256(BPS_DIVISOR) / int256(vaultBalance);
+
+        int256 imbalanceBps = protocol.i_calcImbalanceOpenBps(int256(vaultBalance), int256(longBalance), totalExpo);
+        assertEq(imbalanceBps, expectedImbalance, "The imbalance should be equal to the expected one");
+    }
+}
