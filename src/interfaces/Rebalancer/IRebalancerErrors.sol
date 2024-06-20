0f707b8a081468c83a239ecc69cb44fc8d811925
beeb <703631+beeb@users.noreply.github.com>
2024-06-20 14:41:34 +0200

    refactor(rebalancer)!: remove unused errors

--- src/interfaces/Rebalancer/IRebalancerErrors.sol
+++ src/interfaces/Rebalancer/IRebalancerErrors.sol
@@ -2,14 +2,8 @@
 pragma solidity >=0.8.0;
 
 interface IRebalancerErrors {
-    /// @dev Indicates that the user assets are used in a position
-    error RebalancerUserInPosition();
-
     /// @dev Indicates that the user assets are not used in a position
     error RebalancerUserPending();
-
-    /// @dev Indicates that the user assets are used in a position
-    error RebalancerUserNotPending();
 
     /// @dev Indicates that the `to` address is invalid
     error RebalancerInvalidAddressTo();
