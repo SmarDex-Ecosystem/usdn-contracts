5da6a2801f66c897b15666ce95dc451e0aa3c782
samooyo <samidarnaud@gmail.com>
2024-06-24 13:07:26 +0200

    fix: imports

--- src/interfaces/UsdnProtocol/IOwnershipCallback.sol
+++ src/interfaces/UsdnProtocol/IOwnershipCallback.sol
@@ -3,18 +3,18 @@ pragma solidity >=0.8.0;
 
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-import { IUsdnProtocolTypes } from "./IUsdnProtocolTypes.sol";
+import { IUsdnProtocolTypes as Types } from "./IUsdnProtocolTypes.sol";
 
 /**
  * @notice This interface can be implemented by contracts that wish to be notified when they become owner of a USDN
  * protocol position
  * @dev The contract must implement the ERC-165 interface detection mechanism
  */
-interface IOwnershipCallback is IERC165, IUsdnProtocolTypes {
+interface IOwnershipCallback is IERC165 {
     /**
      * @notice This function is called by the USDN protocol on the new position owner after a transfer of ownership
      * @param oldOwner The previous owner of the position
      * @param posId The unique position identifier
      */
-    function ownershipCallback(address oldOwner, PositionId calldata posId) external;
+    function ownershipCallback(address oldOwner, Types.PositionId calldata posId) external;
 }
