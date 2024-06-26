18d72b5cb6f5b809aa094439f20c84e0affe4be1
beeb <703631+beeb@users.noreply.github.com>
2024-06-26 10:47:18 +0200

    test: natspec

--- test/unit/UsdnProtocol/Actions/_CheckInitiateClosePosition.t.sol
+++ test/unit/UsdnProtocol/Actions/_CheckInitiateClosePosition.t.sol
@@ -40,6 +40,11 @@ contract TestUsdnProtocolCheckInitiateClosePosition is UsdnProtocolBaseFixture, 
         assertGt(AMOUNT, protocol.getMinLongPosition(), "position amount");
     }
 
+    /**
+     * @custom:scenario Check a valid initiate close position
+     * @custom:when The user initiates a close position with valid parameters
+     * @custom:then The function should not revert
+     */
     function test_checkInitiateClosePosition() public view {
         uint128 amountToClose = AMOUNT - uint128(protocol.getMinLongPosition());
 
@@ -47,21 +52,42 @@ contract TestUsdnProtocolCheckInitiateClosePosition is UsdnProtocolBaseFixture, 
         protocol.i_checkInitiateClosePosition(address(this), USER_1, USER_2, AMOUNT, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close with a zero "to" address
+     * @custom:when The user initiates a close position with a zero "to" address
+     * @custom:then The function should revert with `UsdnProtocolInvalidAddressTo`
+     */
     function test_RevertWhen_checkInitiateClosePositionToZero() public {
         vm.expectRevert(UsdnProtocolInvalidAddressTo.selector);
         protocol.i_checkInitiateClosePosition(address(this), address(0), USER_2, AMOUNT, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close with a zero "validator" address
+     * @custom:when The user initiates a close position with a zero "validator" address
+     * @custom:then The function should revert with `UsdnProtocolInvalidAddressValidator`
+     */
     function test_RevertWhen_checkInitiateClosePositionValidatorZero() public {
         vm.expectRevert(UsdnProtocolInvalidAddressValidator.selector);
         protocol.i_checkInitiateClosePosition(address(this), USER_1, address(0), AMOUNT, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close with a wrong owner
+     * @custom:when The protocol initiates a close position with a wrong owner (should never happen)
+     * @custom:then The function should revert with `UsdnProtocolUnauthorized`
+     */
     function test_RevertWhen_checkInitiateClosePositionWrongOwner() public {
         vm.expectRevert(UsdnProtocolUnauthorized.selector);
         protocol.i_checkInitiateClosePosition(USER_1, USER_1, USER_2, AMOUNT, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close of a position that was not validated
+     * @custom:given A position that was initiated but not validated
+     * @custom:when The user initiates a close position of a position that was not validated
+     * @custom:then The function should revert with `UsdnProtocolPositionNotValidated`
+     */
     function test_RevertWhen_checkInitiateClosePositionPending() public {
         posId = setUpUserPositionInLong(
             OpenParams(
@@ -73,6 +99,11 @@ contract TestUsdnProtocolCheckInitiateClosePosition is UsdnProtocolBaseFixture, 
         protocol.i_checkInitiateClosePosition(address(this), USER_1, USER_2, AMOUNT, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close of a position with invalid amount
+     * @custom:when The user initiates a close position with an amount that is too big
+     * @custom:then The function should revert with `UsdnProtocolAmountToCloseHigherThanPositionAmount`
+     */
     function test_RevertWhen_checkInitiateClosePositionAmountTooBig() public {
         uint128 amountToClose = 2 * AMOUNT;
         vm.expectRevert(
@@ -81,28 +112,64 @@ contract TestUsdnProtocolCheckInitiateClosePosition is UsdnProtocolBaseFixture, 
         protocol.i_checkInitiateClosePosition(address(this), USER_1, USER_2, amountToClose, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close of a position with zero amount
+     * @custom:when The user initiates a close position with an amount of zero
+     * @custom:then The function should revert with `UsdnProtocolAmountToCloseIsZero`
+     */
     function test_RevertWhen_checkInitiateClosePositionAmountZero() public {
         vm.expectRevert(UsdnProtocolAmountToCloseIsZero.selector);
         protocol.i_checkInitiateClosePosition(address(this), USER_1, USER_2, 0, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close of a position with a remaining amount that is too low
+     * @custom:when The user initiates a partial close with an amount that would leave the position below the minimum
+     * @custom:then The function should revert with `UsdnProtocolLongPositionTooSmall`
+     */
     function test_RevertWhen_checkInitiateClosePositionRemainingLow() public {
         uint128 amountToClose = AMOUNT - uint128(protocol.getMinLongPosition()) + 1;
         vm.expectRevert(UsdnProtocolLongPositionTooSmall.selector);
         protocol.i_checkInitiateClosePosition(address(this), USER_1, USER_2, amountToClose, pos);
     }
 
-    function test_checkInitiateClosePositionFromRebalancer() public {
+    /**
+     * @custom:scenario Check an initiate close of a position from the rebalancer with a remaining amount that is too
+     * low
+     * @custom:given USER_1 has a position in the rebalancer with an amount that leaves the position below the min
+     * @custom:and The "to" address is the rebalancer user
+     * @custom:when The rebalancer initiates a close position with the full amount of USER_1
+     * @custom:then The function should not revert
+     */
+    function test_checkInitiateClosePositionFromRebalancerBelowMin() public {
         uint128 amountToClose = AMOUNT - uint128(protocol.getMinLongPosition()) + 1;
         _setUpRebalancerPosition(uint88(amountToClose));
         // note: the "to" below must be the rebalancer user (USER_1)
         protocol.i_checkInitiateClosePosition(address(mockedRebalancer), USER_1, USER_2, amountToClose, pos);
+    }
 
-        amountToClose -= 1;
+    /**
+     * @custom:scenario Check an initiate close of a position from the rebalancer (partial)
+     * @custom:given USER_1 has a position in the rebalancer with an amount that leaves the position above the min
+     * @custom:and The "to" address is not the rebalancer user
+     * @custom:when The rebalancer initiates a partial close position with a remaining amount that is above the min
+     * @custom:then The function should not revert
+     */
+    function test_checkInitiateClosePositionFromRebalancer() public {
+        uint128 amountToClose = AMOUNT - uint128(protocol.getMinLongPosition());
+        _setUpRebalancerPosition(uint88(amountToClose + 1));
         // if the rebalancer position remains above the position limit, then the "to" can be anyone
         protocol.i_checkInitiateClosePosition(address(mockedRebalancer), address(this), USER_2, amountToClose, pos);
     }
 
+    /**
+     * @custom:scenario Check an initiate close of a position from the rebalancer with a remaining amount that is too
+     * low
+     * @custom:given USER_1 has a position in the rebalancer with an amount that would leave the position below the min
+     * @custom:and The "to" address is the rebalancer user
+     * @custom:when The rebalancer initiates a partial close position with an amount below their position amount
+     * @custom:then The function should revert with `UsdnProtocolLongPositionTooSmall`
+     */
     function test_RevertWhen_checkInitiateClosePositionFromRebalancerTooSmall() public {
         uint128 amountToClose = AMOUNT - uint128(protocol.getMinLongPosition()) + 1;
         _setUpRebalancerPosition(uint88(amountToClose + 1));
@@ -111,6 +178,10 @@ contract TestUsdnProtocolCheckInitiateClosePosition is UsdnProtocolBaseFixture, 
         protocol.i_checkInitiateClosePosition(address(mockedRebalancer), USER_1, USER_2, amountToClose, pos);
     }
 
+    /**
+     * @notice Helper function to setup the mock rebalancer
+     * @param userAmount The amount to set for the user deposit
+     */
     function _setUpRebalancerPosition(uint88 userAmount) internal {
         assertLe(userAmount, AMOUNT, "rebalancer user amount");
         posId = setUpUserPositionInLong(
