b55f04a6205927697b661f94c4f78672cc0a0f5d
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-06-28 16:24:59 +0200

    feat: add basic wrapper

--- test/echidna/models/EchidnaAssert.sol
+++ test/echidna/models/EchidnaAssert.sol
@@ -12,6 +12,7 @@ import { Usdn } from "../../../src/Usdn/Usdn.sol";
 import { UsdnProtocol } from "../../../src/UsdnProtocol/UsdnProtocol.sol";
 import { IWstETH } from "../../../src/interfaces/IWstETH.sol";
 import { IUsdnProtocolTypes } from "../../../src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
+import { Permit2TokenBitfield } from "../../../src/libraries/Permit2TokenBitfield.sol";
 
 import { SDEX, WSTETH } from "../../utils/Constants.sol";
 import { Sdex } from "../../utils/Sdex.sol";
@@ -41,6 +42,13 @@ contract Setup is Test {
     uint256 public constant ACCOUNT_ETH_AMOUNT = 100 ether;
     IHevm public constant hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
+    Permit2TokenBitfield.Bitfield constant NO_PERMIT2 = Permit2TokenBitfield.Bitfield.wrap(0);
+    IUsdnProtocolTypes.PreviousActionsData internal EMPTY_PREVIOUS_DATA =
+        IUsdnProtocolTypes.PreviousActionsData({ priceData: new bytes[](0), rawIndices: new uint128[](0) });
+
+    mapping(address => address[]) public destinationsToken;
+    address[2] public validators = [DEPLOYER, ATTACKER];
+
     Sdex public constant sdex = Sdex(SDEX);
     IWETH public constant weth = IWETH(WETH_ADDRESS);
     IWstETH public constant wstEth = IWstETH(WSTETH);
@@ -55,10 +63,14 @@ contract Setup is Test {
         uint256 INIT_DEPOSIT_AMOUNT = 10 ether;
         uint256 INIT_LONG_AMOUNT = 10 ether;
 
-        uint256 _ethAmount = (INIT_DEPOSIT_AMOUNT + INIT_LONG_AMOUNT + 10_000) * wstEth.stEthPerToken() / 1 ether;
+        uint256 _ethAmount =
+            (INIT_DEPOSIT_AMOUNT + INIT_LONG_AMOUNT + ACCOUNT_ETH_AMOUNT + 10_000) * wstEth.stEthPerToken() / 1 ether;
         vm.deal(address(this), _ethAmount);
         (bool result,) = WSTETH.call{ value: _ethAmount }("");
         require(result, "WstETH mint failed");
+
+        wstEth.transfer(DEPLOYER, ACCOUNT_ETH_AMOUNT);
+        require(wstEth.balanceOf(DEPLOYER) == ACCOUNT_ETH_AMOUNT, "WstETH transfer failed");
 
         wstEthOracleMiddleware = new MockWstEthOracleMiddleware(
             0x4305FB66699C3B2702D4d05CF36551390A4c69C6,
@@ -93,11 +105,27 @@ contract Setup is Test {
         usdnProtocol.initialize(uint128(INIT_DEPOSIT_AMOUNT), uint128(INIT_LONG_AMOUNT), uint128(_desiredLiqPrice), "");
 
         vm.deal(DEPLOYER, ACCOUNT_ETH_AMOUNT);
+
+        destinationsToken[WSTETH] = [DEPLOYER, ATTACKER];
     }
 }
 
 contract EchidnaAssert is Setup {
-/* -------------------------------------------------------------------------- */
-/*                             Utils                                          */
-/* -------------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------- */
+    /*                             USDN Protocol                                  */
+    /* -------------------------------------------------------------------------- */
+
+    function initiateDeposit(uint128 amountRand, uint8 destRand, uint8 validatorRand) public {
+        uint128 amount = uint128(bound(amountRand, 0, wstEth.balanceOf(msg.sender)));
+
+        uint256 destIndex = bound(destRand, 0, destinationsToken[WSTETH].length - 1);
+        address dest = destinationsToken[WSTETH][destIndex];
+
+        uint256 validatorIndex = bound(validatorRand, 0, validators.length - 1);
+        address payable validator = payable(validators[validatorIndex]);
+
+        bytes memory priceData = abi.encode(2 ether);
+
+        usdnProtocol.initiateDeposit(amount, dest, validator, NO_PERMIT2, priceData, EMPTY_PREVIOUS_DATA);
+    }
 }
