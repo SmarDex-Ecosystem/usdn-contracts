e2c77232edf21608aa79405785b72e121fc7596e
beeb <703631+beeb@users.noreply.github.com>
2024-07-01 13:45:52 +0200

    test(core): add funding tests

--- test/unit/UsdnProtocol/Core/_Funding.t.sol
+++ test/unit/UsdnProtocol/Core/_Funding.t.sol
@@ -26,7 +26,65 @@ contract TestUsdnProtocolCoreFunding is UsdnProtocolBaseFixture {
 
     function test_fundingNoTimeElapsed() public {
         (int256 fund, int256 longExpo) = protocol.i_funding(s, s.lastUpdateTimestamp, EMA);
-        assertEq(fund, 0, "funding should be 0 if no time has passed");
-        assertEq(longExpo, int256(s.totalExpo - s.balanceLong), "longExpo if no time has passed");
+        assertEq(fund, 0, "funding");
+        assertEq(longExpo, int256(s.totalExpo - s.balanceLong), "longExpo");
+    }
+
+    function test_fundingTimeElapsedPositive() public {
+        s.totalExpo = 2000 ether;
+        s.balanceLong = 1000 ether;
+        s.balanceVault = 500 ether;
+        uint128 timeElapsed = 100;
+
+        int256 longTradingExpo = int256(s.totalExpo - s.balanceLong);
+        assertEq(longTradingExpo, 1000 ether, "longTradingExpo");
+        uint256 numeratorSquared = uint256((longTradingExpo - int256(s.balanceVault)) ** 2);
+        assertEq(numeratorSquared, 500 ether ** 2, "numeratorSquared");
+        uint256 denominator = uint256(longTradingExpo * longTradingExpo) * 1 days;
+        int256 expectedFunding = int256(
+            numeratorSquared * timeElapsed * s.fundingSF
+                * 10 ** (Constants.FUNDING_RATE_DECIMALS - Constants.FUNDING_SF_DECIMALS) / denominator
+        ) + EMA;
+
+        (int256 fund, int256 longExpo) = protocol.i_funding(s, s.lastUpdateTimestamp + timeElapsed, EMA);
+        assertEq(fund, expectedFunding, "funding");
+        assertEq(longExpo, longTradingExpo, "longExpo");
+    }
+
+    function test_fundingTimeElapsedNegative() public {
+        s.totalExpo = 2000 ether;
+        s.balanceLong = 1000 ether;
+        s.balanceVault = 1500 ether;
+        uint128 timeElapsed = 100;
+
+        int256 longTradingExpo = int256(s.totalExpo - s.balanceLong);
+        assertEq(longTradingExpo, 1000 ether, "longTradingExpo");
+        uint256 numeratorSquared = uint256((longTradingExpo - int256(s.balanceVault)) ** 2);
+        assertEq(numeratorSquared, 500 ether ** 2, "numeratorSquared");
+        uint256 denominator = s.balanceVault * s.balanceVault * 1 days;
+        int256 expectedFunding = -int256(
+            numeratorSquared * timeElapsed * s.fundingSF
+                * 10 ** (Constants.FUNDING_RATE_DECIMALS - Constants.FUNDING_SF_DECIMALS) / denominator
+        ) + EMA;
+
+        (int256 fund, int256 longExpo) = protocol.i_funding(s, s.lastUpdateTimestamp + timeElapsed, EMA);
+        assertEq(fund, expectedFunding, "funding");
+        assertEq(longExpo, longTradingExpo, "longExpo");
+    }
+
+    function test_fundingEquilibrium() public {
+        s.totalExpo = 2000 ether;
+        s.balanceLong = 1000 ether;
+        s.balanceVault = 1000 ether;
+        uint128 timeElapsed = 100;
+
+        int256 longTradingExpo = int256(s.totalExpo - s.balanceLong);
+        assertEq(longTradingExpo, 1000 ether, "longTradingExpo");
+        uint256 numeratorSquared = uint256((longTradingExpo - int256(s.balanceVault)) ** 2);
+        assertEq(numeratorSquared, 0, "numeratorSquared");
+
+        (int256 fund, int256 longExpo) = protocol.i_funding(s, s.lastUpdateTimestamp + timeElapsed, EMA);
+        assertEq(fund, EMA, "funding");
+        assertEq(longExpo, longTradingExpo, "longExpo");
     }
 }
