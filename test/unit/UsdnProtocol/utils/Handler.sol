baed1562a8eebe792f1a59ada49ebdcbf9171f70
kirienzo-eth <kirienzo.eth@gmail.com>
2024-07-03 15:58:03 +0200

    test: add validate internal functions in the handler

--- test/unit/UsdnProtocol/utils/Handler.sol
+++ test/unit/UsdnProtocol/utils/Handler.sol
@@ -157,8 +157,18 @@ contract UsdnProtocolHandler is UsdnProtocol, Test {
         );
     }
 
-    function i_validateClosePosition(address user, bytes calldata priceData) external {
-        ActionsLong._validateClosePosition(s, user, priceData);
+    function i_validateOpenPosition(address user, bytes calldata priceData)
+        external
+        returns (uint256 securityDepositValue_, bool isValidated_, bool liquidated_)
+    {
+        return ActionsLong._validateOpenPosition(s, user, priceData);
+    }
+
+    function i_validateClosePosition(address user, bytes calldata priceData)
+        external
+        returns (uint256 securityDepositValue_, bool isValidated_, bool liquidated_)
+    {
+        return ActionsLong._validateClosePosition(s, user, priceData);
     }
 
     function i_validateWithdrawal(address user, bytes calldata priceData)
@@ -166,6 +176,13 @@ contract UsdnProtocolHandler is UsdnProtocol, Test {
         returns (uint256 securityDepositValue_, bool isValidated_)
     {
         return ActionsVault._validateWithdrawal(s, user, priceData);
+    }
+
+    function i_validateDeposit(address user, bytes calldata priceData)
+        external
+        returns (uint256 securityDepositValue_, bool isValidated_)
+    {
+        return ActionsVault._validateDeposit(s, user, priceData);
     }
 
     function i_removeAmountFromPosition(
