159513c09354700081da091928a9826b3962069c
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-07-09 14:15:29 +0200

    fix: remove unused constant

--- test/echidna/models/EchidnaAssert.sol
+++ test/echidna/models/EchidnaAssert.sol
@@ -23,7 +23,6 @@ contract Setup is Test {
     address public constant DEPLOYER = address(0x10000);
     address public constant ATTACKER = address(0x20000);
     address public constant FEE_COLLECTOR = address(0x00fee);
-    uint256 public constant ACCOUNT_START_ETH_AMOUNT = 100 ether;
 
     Sdex public immutable sdex = new Sdex();
     Weth public immutable weth = new Weth();
