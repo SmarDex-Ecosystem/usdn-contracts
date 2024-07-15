9b53d1d5ba5547123c2075c1b014037a0f4fb5b2
Lilyan Bastien <91454320+lilyanB@users.noreply.github.com>
2024-07-15 09:43:18 +0200

    test: echidna validate withdrawal (#439)
    * test: add model
    
    * test: add testt in echidnaAssert.t.sol
    
    * test: fix test
    
    * test: simplify balance
    
    * test: use securityDepositValue stored
    
    * test: use success_
    
    * test: add assert
    
    * test: simplify assert

--- test/echidna/EchidnaAssert.t.sol
+++ test/echidna/EchidnaAssert.t.sol
@@ -3,19 +3,26 @@ pragma solidity ^0.8.0;
 
 import { Test } from "forge-std/Test.sol";
 
+import { Usdn } from "../../../src/Usdn/Usdn.sol";
+import { MockOracleMiddleware } from "../../../test/unit/UsdnProtocol/utils/MockOracleMiddleware.sol";
 import { UsdnProtocol } from "../../src/UsdnProtocol/UsdnProtocol.sol";
 import { UsdnProtocolVaultLibrary as Vault } from "../../src/UsdnProtocol/libraries/UsdnProtocolVaultLibrary.sol";
-
 import { IUsdn } from "../../src/interfaces/Usdn/IUsdn.sol";
 import { IUsdnProtocolTypes } from "../../src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
+import { WstETH } from "../utils/WstEth.sol";
 import { EchidnaAssert } from "./models/EchidnaAssert.sol";
 
 contract TestEchidna is Test {
     EchidnaAssert public echidna;
     UsdnProtocol public usdnProtocol;
+    MockOracleMiddleware public wstEthOracleMiddleware;
+    WstETH public wsteth;
+    Usdn public usdn;
 
     address internal DEPLOYER;
     address internal ATTACKER;
+
+    uint152 usdnShares = 100_000 ether;
 
     function setUp() public {
         echidna = new EchidnaAssert();
@@ -23,6 +30,12 @@ contract TestEchidna is Test {
         ATTACKER = echidna.ATTACKER();
 
         usdnProtocol = echidna.usdnProtocol();
+        wstEthOracleMiddleware = echidna.wstEthOracleMiddleware();
+        wsteth = echidna.wsteth();
+        usdn = echidna.usdn();
+
+        vm.prank(address(usdnProtocol));
+        usdn.mintShares(DEPLOYER, usdnShares);
     }
 
     function test_canInitiateDeposit() public {
@@ -46,10 +59,6 @@ contract TestEchidna is Test {
     }
 
     function test_canInitiateWithdrawal() public {
-        uint152 usdnShares = 0.1 ether;
-        IUsdn usdn = usdnProtocol.getUsdn();
-        vm.prank(address(usdnProtocol));
-        usdn.mintShares(DEPLOYER, usdnShares);
         vm.prank(DEPLOYER);
         echidna.initiateWithdrawal(usdnShares, 10 ether, 0, 0, 1000 ether);
 
@@ -59,5 +68,36 @@ contract TestEchidna is Test {
         assertEq(action.validator, DEPLOYER, "action validator");
         assertEq(action.var1, int24(Vault._calcWithdrawalAmountLSB(usdnShares)), "action amount LSB");
         assertEq(action.var2, Vault._calcWithdrawalAmountMSB(usdnShares), "action amount MSB");
+    }
+
+    function test_canValidateWithdrawal() public {
+        vm.deal(DEPLOYER, 10 ether);
+        uint256 securityDeposit = usdnProtocol.getSecurityDepositValue();
+        vm.prank(DEPLOYER);
+        usdn.approve(address(usdnProtocol), usdnShares);
+        bytes memory priceData = abi.encode(4000 ether);
+
+        vm.prank(DEPLOYER);
+        usdnProtocol.initiateWithdrawal{ value: securityDeposit }(
+            usdnShares,
+            DEPLOYER,
+            payable(DEPLOYER),
+            priceData,
+            IUsdnProtocolTypes.PreviousActionsData({ priceData: new bytes[](0), rawIndices: new uint128[](0) })
+        );
+
+        uint256 balanceBefore = DEPLOYER.balance;
+        uint256 balanceBeforeProtocol = address(usdnProtocol).balance;
+        uint256 balanceWstEthBefore = wsteth.balanceOf(DEPLOYER);
+
+        skip(wstEthOracleMiddleware.getValidationDelay() + 1);
+        vm.prank(DEPLOYER);
+        echidna.validateWithdrawal(0, 4000 ether);
+
+        IUsdnProtocolTypes.PendingAction memory action = usdnProtocol.getUserPendingAction(DEPLOYER);
+        assertTrue(action.action == IUsdnProtocolTypes.ProtocolAction.None, "action type");
+        assertEq(DEPLOYER.balance, balanceBefore + securityDeposit, "DEPLOYER balance");
+        assertEq(address(usdnProtocol).balance, balanceBeforeProtocol - securityDeposit, "protocol balance");
+        assertGt(wsteth.balanceOf(DEPLOYER), balanceWstEthBefore, "wstETH balance");
     }
 }
