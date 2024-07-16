63d2cac983f629efd67da17255686bf6f4bc9863
Paul-Alexandre Tessier <tessier.paulalexandre@orange.fr>
2024-07-16 18:44:03 +0200

    refactor: echidna project (#446)
    * test: add validateDeposit
    
    * refactor: echidna project
    
    * chore: Merge branch 'refs/heads/test/echidna' into test/echidna-validate-deposit
    
    # Conflicts:
    #	test/echidna/EchidnaAssert.t.sol
    #	test/echidna/models/EchidnaAssert.sol

--- test/echidna/functional/FuzzActions.sol
+++ test/echidna/functional/FuzzActions.sol
@@ -1,135 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import { Test } from "forge-std/Test.sol";
-
-import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
-import { SafeTransferLib } from "solady/src/utils/SafeTransferLib.sol";
-
-import { MockOracleMiddleware } from "../../unit/UsdnProtocol/utils/MockOracleMiddleware.sol";
-import { Sdex } from "../../utils/Sdex.sol";
-import { Weth } from "../../utils/WETH.sol";
-import { WstETH } from "../../utils/WstEth.sol";
-import { MockLiquidationRewardsManager } from "../mock/MockLiquidationRewardsManager.sol";
-
-import { Rebalancer } from "../../../src/Rebalancer/Rebalancer.sol";
-import { Usdn } from "../../../src/Usdn/Usdn.sol";
-import { UsdnProtocol } from "../../../src/UsdnProtocol/UsdnProtocol.sol";
-import { IWstETH } from "../../../src/interfaces/IWstETH.sol";
-import { IUsdnErrors } from "../../../src/interfaces/Usdn/IUsdnErrors.sol";
-import { IUsdnProtocolErrors } from "../../../src/interfaces/UsdnProtocol/IUsdnProtocolErrors.sol";
 import { IUsdnProtocolTypes } from "../../../src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
 import { Permit2TokenBitfield } from "../../../src/libraries/Permit2TokenBitfield.sol";
-import { SignedMath } from "../../../src/libraries/SignedMath.sol";
-import { TickMath } from "../../../src/libraries/TickMath.sol";
+import { Setup } from "../Setup.sol";
 
-contract Setup is Test {
-    address public constant DEPLOYER = address(0x10000);
-    address public constant ATTACKER = address(0x20000);
-    address public constant FEE_COLLECTOR = address(0x00fee);
-    Permit2TokenBitfield.Bitfield public constant NO_PERMIT2 = Permit2TokenBitfield.Bitfield.wrap(0);
-
-    Sdex public immutable sdex = new Sdex();
-    Weth public immutable weth = new Weth();
-    WstETH public immutable wsteth = new WstETH();
-
-    IUsdnProtocolTypes.PreviousActionsData internal EMPTY_PREVIOUS_DATA =
-        IUsdnProtocolTypes.PreviousActionsData({ priceData: new bytes[](0), rawIndices: new uint128[](0) });
-
-    mapping(address => address[]) public destinationsToken;
-    address[2] public validators = [DEPLOYER, ATTACKER];
-    IUsdnProtocolTypes.PositionId[] posIds;
-
-    MockOracleMiddleware public wstEthOracleMiddleware;
-    MockLiquidationRewardsManager public liquidationRewardsManager;
-    Usdn public usdn;
-    UsdnProtocol public usdnProtocol;
-    Rebalancer public rebalancer;
-
-    bytes4[] public INITIATE_DEPOSIT_ERRORS = [
-        IUsdnProtocolErrors.UsdnProtocolSecurityDepositTooLow.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressValidator.selector,
-        IUsdnProtocolErrors.UsdnProtocolZeroAmount.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressTo.selector,
-        IUsdnProtocolErrors.UsdnProtocolImbalanceLimitReached.selector,
-        IUsdnProtocolErrors.UsdnProtocolDepositTooSmall.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidLongExpo.selector,
-        IUsdnProtocolErrors.UsdnProtocolPendingAction.selector,
-        FixedPointMathLib.FullMulDivFailed.selector,
-        SafeTransferLib.TransferFromFailed.selector,
-        SignedMath.SignedMathDivideByZero.selector
-    ];
-    bytes4[] public INITIATE_OPEN_ERRORS = [
-        IUsdnProtocolErrors.UsdnProtocolSecurityDepositTooLow.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressTo.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressValidator.selector,
-        IUsdnProtocolErrors.UsdnProtocolZeroAmount.selector,
-        IUsdnProtocolErrors.UsdnProtocolLongPositionTooSmall.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidPendingAction.selector,
-        IUsdnErrors.UsdnInsufficientSharesBalance.selector
-    ];
-    bytes4[] public VALIDATE_OPEN_ERRORS = [IUsdnProtocolErrors.UsdnProtocolNoPendingAction.selector];
-    bytes4[] public INITIATE_WITHDRAWAL_ERRORS = [
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressTo.selector,
-        IUsdnProtocolErrors.UsdnProtocolSecurityDepositTooLow.selector,
-        IUsdnProtocolErrors.UsdnProtocolZeroAmount.selector,
-        SignedMath.SignedMathDivideByZero.selector,
-        IUsdnErrors.UsdnInsufficientSharesBalance.selector,
-        TickMath.TickMathInvalidPrice.selector
-    ];
-
-    bytes4[] public VALIDATE_WITHDRAWAL_ERRORS = [
-        IUsdnProtocolErrors.UsdnProtocolInvalidAddressValidator.selector,
-        IUsdnProtocolErrors.UsdnProtocolNoPendingAction.selector,
-        IUsdnProtocolErrors.UsdnProtocolInvalidPendingAction.selector
-    ];
-
-    constructor() payable {
-        vm.warp(1_709_251_200);
-        //TODO see to fuzz these data
-        uint256 INIT_DEPOSIT_AMOUNT = 300 ether;
-        uint256 INIT_LONG_AMOUNT = 300 ether;
-        uint128 INITIAL_PRICE = 2000 ether; // 2000 USDN = 1 ETH
-
-        uint256 ethAmount = (INIT_DEPOSIT_AMOUNT + INIT_LONG_AMOUNT) * wsteth.stEthPerToken() / 1 ether;
-        vm.deal(address(this), ethAmount);
-        (bool result,) = address(wsteth).call{ value: ethAmount }("");
-        require(result, "WstETH mint failed");
-
-        wstEthOracleMiddleware = new MockOracleMiddleware();
-
-        liquidationRewardsManager = new MockLiquidationRewardsManager(IWstETH(wsteth), uint256(2 hours + 5 minutes));
-
-        usdn = new Usdn(address(0), address(0));
-
-        usdnProtocol =
-            new UsdnProtocol(usdn, sdex, wsteth, wstEthOracleMiddleware, liquidationRewardsManager, 100, FEE_COLLECTOR);
-
-        rebalancer = new Rebalancer(usdnProtocol);
-
-        usdnProtocol.setRebalancer(rebalancer);
-
-        usdn.grantRole(usdn.MINTER_ROLE(), address(usdnProtocol));
-        usdn.grantRole(usdn.REBASER_ROLE(), address(usdnProtocol));
-        wsteth.approve(address(usdnProtocol), INIT_DEPOSIT_AMOUNT + INIT_LONG_AMOUNT);
-
-        uint256 _desiredLiqPrice = wstEthOracleMiddleware.parseAndValidatePrice(
-            "", uint128(block.timestamp), IUsdnProtocolTypes.ProtocolAction.Initialize, abi.encode(INITIAL_PRICE)
-        ).price / 2;
-
-        // leverage approx 2x
-        usdnProtocol.initialize(
-            uint128(INIT_DEPOSIT_AMOUNT),
-            uint128(INIT_LONG_AMOUNT),
-            uint128(_desiredLiqPrice),
-            abi.encode(INITIAL_PRICE)
-        );
-
-        destinationsToken[address(wsteth)] = [DEPLOYER, ATTACKER];
-    }
-}
-
-contract EchidnaAssert is Setup {
+contract FuzzActions is Setup {
     struct InitiateDepositBalanceBefore {
         uint256 senderETH;
         uint256 senderWstETH;
@@ -164,6 +40,20 @@ contract EchidnaAssert is Setup {
         uint64 securityDeposit;
     }
 
+    struct ValidateDepositBalanceBefore {
+        uint256 senderWstETH;
+        uint256 senderETH;
+        uint256 senderUsdn;
+        uint256 usdnProtocolWstETH;
+        uint256 usdnProtocolETH;
+        uint256 usdnProtocolUsdn;
+        uint256 validatorWstETH;
+        uint256 validatorETH;
+        uint256 validatorUsdn;
+        uint256 toWstETH;
+        uint256 toETH;
+        uint256 toUsdn;
+    }
     /* -------------------------------------------------------------------------- */
     /*                             USDN Protocol                                  */
     /* -------------------------------------------------------------------------- */
@@ -257,23 +147,6 @@ contract EchidnaAssert is Setup {
         }
     }
 
-    function _checkErrors(bytes memory err, bytes4[] storage errors) internal {
-        bool expected = false;
-        for (uint256 i = 0; i < errors.length; i++) {
-            if (errors[i] == bytes4(err)) {
-                expected = true;
-                break;
-            }
-        }
-        if (expected) {
-            emit log_named_bytes("Expected error ", err);
-            return;
-        } else {
-            emit log_named_bytes("DOS ", err);
-            assert(false);
-        }
-    }
-
     function initiateWithdrawal(
         uint152 usdnShares,
         uint256 ethRand,
@@ -313,6 +186,62 @@ contract EchidnaAssert is Setup {
             assert(usdn.sharesOf(address(usdnProtocol)) == balanceBefore.usdnProtocolUsdn + usdnShares);
         } catch (bytes memory err) {
             _checkErrors(err, INITIATE_WITHDRAWAL_ERRORS);
+        }
+    }
+
+    function validateDeposit(uint256 validatorRand, uint256 currentPrice) public {
+        validatorRand = bound(validatorRand, 0, validators.length - 1);
+        address payable validator = payable(validators[validatorRand]);
+
+        bytes memory priceData = abi.encode(currentPrice);
+
+        IUsdnProtocolTypes.DepositPendingAction memory pendingAction =
+            usdnProtocol.i_toDepositPendingAction(usdnProtocol.getUserPendingAction(validator));
+
+        ValidateDepositBalanceBefore memory balanceBefore = ValidateDepositBalanceBefore({
+            senderWstETH: wsteth.balanceOf(msg.sender),
+            senderETH: address(msg.sender).balance,
+            senderUsdn: usdn.sharesOf(msg.sender),
+            usdnProtocolWstETH: wsteth.balanceOf(address(usdnProtocol)),
+            usdnProtocolETH: address(usdnProtocol).balance,
+            usdnProtocolUsdn: usdn.sharesOf(address(usdnProtocol)),
+            validatorWstETH: wsteth.balanceOf(validator),
+            validatorETH: validator.balance,
+            validatorUsdn: usdn.sharesOf(validator),
+            toWstETH: wsteth.balanceOf(pendingAction.to),
+            toETH: pendingAction.to.balance,
+            toUsdn: usdn.sharesOf(pendingAction.to)
+        });
+
+        vm.prank(msg.sender);
+        try usdnProtocol.validateDeposit(validator, priceData, EMPTY_PREVIOUS_DATA) returns (bool success_) {
+            uint256 securityDeposit = usdnProtocol.getSecurityDepositValue();
+
+            if (success_) {
+                //todo maybe determine the exact amount if it can be know before the call
+                assert(usdn.sharesOf(pendingAction.to) > balanceBefore.toUsdn);
+                if (pendingAction.to != msg.sender) {
+                    assert(usdn.sharesOf(msg.sender) == balanceBefore.senderUsdn);
+                }
+                if (pendingAction.to != validator) {
+                    assert(usdn.sharesOf(validator) == balanceBefore.validatorUsdn);
+                }
+            } else {
+                assert(usdn.sharesOf(msg.sender) == balanceBefore.senderUsdn);
+                assert(usdn.sharesOf(validator) == balanceBefore.validatorUsdn);
+                assert(usdn.sharesOf(pendingAction.to) == balanceBefore.toUsdn);
+            }
+
+            assert(validator.balance == balanceBefore.validatorETH + securityDeposit);
+
+            assert(usdn.sharesOf(address(usdnProtocol)) == balanceBefore.usdnProtocolUsdn);
+
+            assert(wsteth.balanceOf(msg.sender) == balanceBefore.senderWstETH);
+            assert(wsteth.balanceOf(address(usdnProtocol)) == balanceBefore.usdnProtocolWstETH);
+            assert(wsteth.balanceOf(validator) == balanceBefore.validatorWstETH);
+            assert(wsteth.balanceOf(pendingAction.to) == balanceBefore.toWstETH);
+        } catch (bytes memory err) {
+            _checkErrors(err, VALIDATE_DEPOSIT_ERRORS);
         }
     }
 
