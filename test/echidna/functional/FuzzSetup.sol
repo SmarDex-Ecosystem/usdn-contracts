a31fde6fae9a97ead6062dc5e510329858767cae
blablalf <a.gaillard@ra2.tech>
2024-07-25 10:45:03 +0200

    test: assessing that iusdnProtocol never fails

--- test/echidna/functional/FuzzSetup.sol
+++ test/echidna/functional/FuzzSetup.sol
@@ -10,28 +10,27 @@ contract FuzzSetup is Setup {
     /* -------------------------------------------------------------------------- */
     /*                             USDN Protocol                                  */
     /* -------------------------------------------------------------------------- */
-    function deployUsdn() external {
-        try new UsdnProtocolHandler(
-            usdn, sdex, wsteth, wstEthOracleMiddleware, liquidationRewardsManager, 100, FEE_COLLECTOR
-        ) returns (UsdnProtocolHandler _usdnProtocol) {
-            usdnProtocol = _usdnProtocol;
-            rebalancer = new Rebalancer(usdnProtocol);
-
-            usdnProtocol.setRebalancer(rebalancer);
-
-            usdn.grantRole(usdn.MINTER_ROLE(), address(usdnProtocol));
-            usdn.grantRole(usdn.REBASER_ROLE(), address(usdnProtocol));
-        } catch (bytes memory err) {
-            _checkErrors(err, VALIDATE_OPEN_ERRORS);
-        }
-    }
-
     function initializeUsdn(uint256 depositAmountRand, uint256 longAmountRand, uint256 desiredLiqPriceRand) external {
+        usdnProtocol = new UsdnProtocolHandler(
+            usdn, sdex, wsteth, wstEthOracleMiddleware, liquidationRewardsManager, 100, FEE_COLLECTOR
+        );
+        rebalancer = new Rebalancer(usdnProtocol);
+
+        usdnProtocol.setRebalancer(rebalancer);
+
+        usdn.grantRole(usdn.MINTER_ROLE(), address(usdnProtocol));
+        usdn.grantRole(usdn.REBASER_ROLE(), address(usdnProtocol));
+
         wsteth.approve(address(usdnProtocol), depositAmountRand + longAmountRand);
 
         try usdnProtocol.initialize(
             uint128(depositAmountRand), uint128(longAmountRand), uint128(desiredLiqPriceRand), ""
-        ) { } catch (bytes memory err) {
+        ) {
+            // Check balances
+            assert(address(usdnProtocol).balance == depositAmountRand);
+            assert(wsteth.balanceOf(address(usdnProtocol)) == longAmountRand);
+            // assert(usdn.balanceOf(msg.sender) == usdn.balanceOf(msg.sender));
+        } catch (bytes memory err) {
             _checkErrors(err, VALIDATE_OPEN_ERRORS);
         }
     }
