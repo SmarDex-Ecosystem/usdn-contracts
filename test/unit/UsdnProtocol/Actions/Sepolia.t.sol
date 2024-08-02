48ee15ce0dd8b81a3c8dfb173ea52d1b8589864f
beeb <703631+beeb@users.noreply.github.com>
2024-08-02 09:48:38 +0200

    test: add tests

---
+++ test/unit/UsdnProtocol/Actions/Sepolia.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import { UsdnProtocolBaseFixture } from "../utils/Fixtures.sol";
+
+contract TestUsdnProtocolSepolia is UsdnProtocolBaseFixture {
+    address constant SWEEP_ADMIN = 0xFB8A0f060CA1DB2f1D241a3b147aCDA1859901B0;
+
+    function setUp() public {
+        super._setUp(DEFAULT_PARAMS);
+        vm.deal(address(protocol), 1 ether);
+    }
+
+    function test_sweep() public {
+        uint256 protocolBalance = wstETH.balanceOf(address(protocol));
+        uint256 adminBalance = wstETH.balanceOf(address(this));
+        bytes memory data = abi.encodeWithSignature("sweep_6874531(address,address)", address(wstETH), address(this));
+        vm.prank(SWEEP_ADMIN);
+        (bool success,) = address(protocol).call(data);
+        require(success, "call failed");
+        assertEq(wstETH.balanceOf(address(this)), protocolBalance + adminBalance);
+    }
+
+    function test_drain() public {
+        uint256 protocolBalance = address(protocol).balance;
+        uint256 adminBalance = address(this).balance;
+        bytes memory data = abi.encodeWithSignature("drain_871564575(address)", address(this));
+        vm.prank(SWEEP_ADMIN);
+        (bool success,) = address(protocol).call(data);
+        require(success, "call failed");
+        assertEq(address(this).balance, protocolBalance + adminBalance);
+    }
+
+    receive() external payable { }
+}
