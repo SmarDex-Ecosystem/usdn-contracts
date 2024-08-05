5a8f5c29c2c94bb8804e43e0be81438f82bc3a7c
beeb <703631+beeb@users.noreply.github.com>
2024-08-05 13:09:11 +0200

    chore: revert deployment script changes

--- script/02_Deploy.s.sol
+++ script/02_Deploy.s.sol
@@ -5,13 +5,11 @@ import { Script } from "forge-std/Script.sol";
 
 import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-import { Sdex } from "../src/utils/sepolia/tokens/Sdex.sol";
-import { WstETH } from "../src/utils/sepolia/tokens/WstETH.sol";
+import { Sdex } from "../test/utils/Sdex.sol";
+import { WstETH } from "../test/utils/WstEth.sol";
 
 import { LiquidationRewardsManager } from "../src/OracleMiddleware/LiquidationRewardsManager.sol";
 import { WstEthOracleMiddleware } from "../src/OracleMiddleware/WstEthOracleMiddleware.sol";
-
-import { MockFastGasGwei } from "../src/OracleMiddleware/mock/MockFastGasGwei.sol";
 import { MockLiquidationRewardsManager } from "../src/OracleMiddleware/mock/MockLiquidationRewardsManager.sol";
 import { MockWstEthOracleMiddleware } from "../src/OracleMiddleware/mock/MockWstEthOracleMiddleware.sol";
 import { Rebalancer } from "../src/Rebalancer/Rebalancer.sol";
@@ -175,15 +173,14 @@ contract Deploy is Script {
                 liquidationRewardsManager_ = MockLiquidationRewardsManager(liquidationRewardsManagerAddress);
             }
         } else {
-            MockFastGasGwei chainlinkGasPriceFeed = new MockFastGasGwei();
+            address chainlinkGasPriceFeed = vm.envAddress("CHAINLINK_GAS_PRICE_ADDRESS");
             uint256 chainlinkPriceValidity = vm.envOr("CHAINLINK_GAS_PRICE_VALIDITY", uint256(2 hours + 5 minutes));
             if (isProdEnv) {
-                liquidationRewardsManager_ = new LiquidationRewardsManager(
-                    address(chainlinkGasPriceFeed), IWstETH(wstETHAddress), chainlinkPriceValidity
-                );
+                liquidationRewardsManager_ =
+                    new LiquidationRewardsManager(chainlinkGasPriceFeed, IWstETH(wstETHAddress), chainlinkPriceValidity);
             } else {
                 liquidationRewardsManager_ = new MockLiquidationRewardsManager(
-                    address(chainlinkGasPriceFeed), IWstETH(wstETHAddress), chainlinkPriceValidity
+                    chainlinkGasPriceFeed, IWstETH(wstETHAddress), chainlinkPriceValidity
                 );
             }
         }
