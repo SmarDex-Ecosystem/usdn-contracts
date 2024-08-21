5f085868dbff71208374c548f5c7a83e1784dedd
lilyanb <lilyan.bastien@gmail.com>
2024-08-21 14:31:46 +0200

    test: change int256Bound

--- test/echidna/helpers/Bound.sol
+++ test/echidna/helpers/Bound.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.25;
 import { Test } from "forge-std/Test.sol";
 
 contract Bound is Test {
-    function int256Bound(uint256 value, uint256 min, uint256 max) public pure returns (int256) {
-        value = bound(value, 0, (max > min ? max : min));
-        if (value % 2 == 0) {
-            return int256(value);
-        }
-        return -int256(value);
+    function int256Bound(uint256 value, int256 min, int256 max) public pure returns (int256) {
+        uint256 diff = uint256(max - min);
+        diff = bound(value, 0, diff);
+        return min + int256(diff);
     }
 }
