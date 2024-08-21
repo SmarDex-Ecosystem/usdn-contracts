dbcc68419f550dd492b5e44fb3271750b78a757f
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-08-21 18:39:42 +0200

    feat: use admin address as sender

--- test/fuzzing/FoundryFuzzing.t.sol
+++ test/fuzzing/FoundryFuzzing.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.25;
 
 import { Test } from "forge-std/Test.sol";
 
-import { USER_1, USER_2, USER_3 } from "../utils/Constants.sol";
+import { ADMIN, USER_1, USER_2 } from "../utils/Constants.sol";
 import { FuzzingSuite } from "./FuzzingSuite.sol";
 
 contract FoundryFuzzingTest is Test {
@@ -39,7 +39,7 @@ contract FoundryFuzzingTest is Test {
 
         targetSender(USER_1);
         targetSender(USER_2);
-        targetSender(USER_3);
+        targetSender(ADMIN);
     }
 
     /// forge-config: local.invariant.fail-on-revert = false
