e08ff29a7eb44d95622dc450ad1b0290df2cd0f4
kirienzo-eth <kirienzo.eth@gmail.com>
2024-08-22 16:28:39 +0200

    fix(rebalancer): add a check in `initiateClosePosition` to revert if the user was liquidated

--- test/integration/UsdnProtocol/RebalancerInitiateClosePosition.t.sol
+++ test/integration/UsdnProtocol/RebalancerInitiateClosePosition.t.sol
@@ -4,9 +4,11 @@ pragma solidity 0.8.26;
 import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
 import { MOCK_PYTH_DATA } from "../../unit/Middlewares/utils/Constants.sol";
+import { USER_1 } from "../../utils/Constants.sol";
 import { SET_PROTOCOL_PARAMS_MANAGER } from "../../utils/Constants.sol";
 import { UsdnProtocolBaseIntegrationFixture } from "./utils/Fixtures.sol";
 
+import { IRebalancerErrors } from "../../../src/interfaces/Rebalancer/IRebalancerErrors.sol";
 import { IRebalancerEvents } from "../../../src/interfaces/Rebalancer/IRebalancerEvents.sol";
 import { IRebalancerTypes } from "../../../src/interfaces/Rebalancer/IRebalancerTypes.sol";
 
@@ -209,6 +211,94 @@ contract TestRebalancerInitiateClosePosition is
         assertEq(payable(rebalancer).balance, 0, "There should be no ether left in the rebalancer");
         assertEq(
             userBalanceBefore - securityDeposit, address(this).balance, "The overpaid amount should have been refunded"
+        );
+    }
+
+    /**
+     * @custom:scenario Call `initiateClosePosition` function after the rebalancer is liquidated
+     * @custom:given The rebalancer's position got liquidated
+     * @custom:and Another user deposits in the rebalancer
+     * @custom:and The rebalancer is triggered again
+     * @custom:when The `initiateClosePosition` function is called by the user in the liquidated version
+     * @custom:then It should revert with `RebalancerUserLiquidated` error
+     */
+    function test_RevertWhen_rebalancerUserLiquidated() public {
+        uint256 securityDeposit = protocol.getSecurityDepositValue();
+        // compensate imbalance to allow rebalancer users to close
+        (, PositionId memory newPosId) = protocol.initiateOpenPosition{ value: securityDeposit }(
+            20 ether, 1100 ether, payable(address(this)), payable(address(this)), NO_PERMIT2, "", EMPTY_PREVIOUS_DATA
+        );
+        _waitDelay();
+        protocol.validateOpenPosition{ value: securityDeposit }(payable(this), MOCK_PYTH_DATA, EMPTY_PREVIOUS_DATA);
+        // Wait 1 minute to provide a fresh price
+        skip(1 minutes);
+
+        // Set the wstETH price to liquidate the rebalancer position
+        {
+            wstEthPrice = 1200 ether;
+            uint128 ethPrice = uint128(wstETH.getWstETHByStETH(wstEthPrice)) / 1e10;
+            mockPyth.setPrice(int64(uint64(ethPrice)));
+            mockPyth.setLastPublishTime(block.timestamp);
+            wstEthPrice = uint128(wstETH.getStETHByWstETH(ethPrice * 1e10));
+        }
+
+        // liquidate the rebalancer's tick
+        uint256 oracleFee = oracleMiddleware.validationCost(MOCK_PYTH_DATA, ProtocolAction.Liquidation);
+        protocol.liquidate{ value: oracleFee }(MOCK_PYTH_DATA, 1);
+        // Sanity check
+        assertEq(
+            prevPosId.tickVersion + 1, protocol.getTickVersion(prevPosId.tick), "Rebalancer tick was not liquidated"
+        );
+
+        // another user deposits in the rebalancer to re-trigger it later
+        wstETH.mintAndApprove(USER_1, amountInRebalancer, address(rebalancer), type(uint256).max);
+        vm.startPrank(USER_1);
+        rebalancer.initiateDepositAssets(amountInRebalancer, USER_1);
+        skip(rebalancer.getTimeLimits().validationDelay);
+        rebalancer.validateDepositAssets();
+        vm.stopPrank();
+
+        // Revert with a protocol error as the tick should not be accessible anymore
+        // but the _lastLiquidatedVersion has not been updated yet
+        vm.expectRevert(abi.encodeWithSelector(UsdnProtocolOutdatedTick.selector, 1, 0));
+        rebalancer.initiateClosePosition{ value: securityDeposit }(
+            1 ether, address(this), MOCK_PYTH_DATA, EMPTY_PREVIOUS_DATA
+        );
+
+        // Wait 1 minute to provide a fresh price
+        skip(1 minutes);
+
+        // Set the wstETH price to liquidate the rebalancer position
+        {
+            wstEthPrice = 1000 ether;
+            uint128 ethPrice = uint128(wstETH.getWstETHByStETH(wstEthPrice)) / 1e10;
+            mockPyth.setPrice(int64(uint64(ethPrice)));
+            mockPyth.setLastPublishTime(block.timestamp);
+            wstEthPrice = uint128(wstETH.getStETHByWstETH(ethPrice * 1e10));
+        }
+
+        // liquidate the position we created earlier and trigger the rebalancer
+        vm.expectEmit(false, false, false, false);
+        emit PositionVersionUpdated(0, 0, 0, PositionId(0, 0, 0));
+        protocol.liquidate{ value: oracleFee }(MOCK_PYTH_DATA, 1);
+        // Sanity checks
+        assertEq(newPosId.tickVersion + 1, protocol.getTickVersion(newPosId.tick), "Position tick was not liquidated");
+        assertEq(rebalancer.getLastLiquidatedVersion(), version, "Liquidated version should have been updated");
+
+        // compensate imbalance to allow rebalancer users to close
+        protocol.initiateOpenPosition{ value: securityDeposit }(
+            20 ether, 800 ether, payable(address(this)), payable(address(this)), NO_PERMIT2, "", EMPTY_PREVIOUS_DATA
+        );
+        _waitDelay();
+        protocol.validateOpenPosition{ value: securityDeposit }(payable(this), MOCK_PYTH_DATA, EMPTY_PREVIOUS_DATA);
+
+        // Wait 1 minute to provide a fresh price
+        skip(1 minutes);
+
+        // Try to withdraw from the rebalancer again
+        vm.expectRevert(IRebalancerErrors.RebalancerUserLiquidated.selector);
+        rebalancer.initiateClosePosition{ value: securityDeposit + 1 ether }(
+            amountInRebalancer, address(this), MOCK_PYTH_DATA, EMPTY_PREVIOUS_DATA
         );
     }
 
