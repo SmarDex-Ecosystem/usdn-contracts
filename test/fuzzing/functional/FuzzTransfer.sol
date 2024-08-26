426be737b6e494917bc676b3ff00f2553829ffaf
blablalf <a.gaillard@ra2.tech>
2024-08-26 10:46:55 +0200

    style: fix imports

--- test/fuzzing/functional/FuzzTransfer.sol
+++ test/fuzzing/functional/FuzzTransfer.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import { Setup } from "../Setup.sol";
-
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { Setup } from "../Setup.sol";
 
 import { IUsdn } from "../../../src/interfaces/Usdn/IUsdn.sol";
 
