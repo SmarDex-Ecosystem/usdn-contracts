7e286f317719b039afbb20cf0c967ca201479818
beeb <703631+beeb@users.noreply.github.com>
2024-09-02 09:16:44 +0200

    chore: update logs analysis script

--- script/logsAnalysis.ts
+++ script/logsAnalysis.ts
@@ -2,53 +2,48 @@
 import { Command } from 'commander';
 import { DateTime } from 'luxon';
 import pc from 'picocolors';
-import { http, createTestClient, defineChain, formatEther, isAddressEqual, publicActions } from 'viem';
-import { OracleMiddlewareAbi, UsdnAbi, UsdnProtocolAbi } from '../dist/abi';
+import { http, createPublicClient, formatEther, isAddressEqual, publicActions, createTestClient, defineChain, webSocket } from 'viem';
+import { IRebalancerAbi, IUsdnAbi, IUsdnProtocolAbi } from '../dist/abi';
 
 function parseArgs() {
   const program = new Command();
 
   program
-    .description('Retrieve and display all logs from a fork')
+    .description('Retrieve and display all logs from the USDN protocol')
     .option(
       '-p, --protocol <address>',
       'address of the USDN protocol contract',
-      '0x24EcC5E6EaA700368B8FAC259d3fBD045f695A08',
+      '0x669c6cf087a0127e8e7c483a58538a8ec0532382',
     )
-    .option('-u, --usdn <address>', 'address of the USDN token contract', '0x0D92d35D311E54aB8EEA0394d7E773Fc5144491a')
+    .option('-u, --usdn <address>', 'address of the USDN token contract', '0x81a3ffF82e09A63ec1e3e114B04Af6C99D340Fb9')
     .option(
-      '-m, --middleware <address>',
-      'address of the oracle middleware contract',
-      '0x4278C5d322aB92F1D876Dd7Bd9b44d1748b88af2',
+      '--rebalancer <address>',
+      'address of the rebalancer contract',
+      '0xB47dDaAc48eF4Fe272b4561399628e1A04e623D3',
     )
     .option('-r, --rpc-url <url>', 'URL of the RPC node to connect to')
+    .option('-b, --blocks <blocks>', 'number of blocks to retrieve', '1000')
     .parse(process.argv);
 
   return program.opts();
 }
 
 function getClient(rpcUrl: string) {
-  const foundry = defineChain({
-    id: 31_337,
-    name: 'Foundry',
-    nativeCurrency: {
-      decimals: 18,
-      name: 'Ether',
-      symbol: 'ETH',
-    },
-    rpcUrls: {
-      default: {
-        http: [rpcUrl],
-        webSocket: ['ws://127.0.0.1:8545'],
-      },
-    },
+  const url = new URL(rpcUrl);
+  if (url.hostname === 'localhost') {
+    return createTestClient({
+      mode: 'anvil',
+      transport: http(rpcUrl),
+    }).extend(publicActions);
+  }
+  if (url.protocol === 'ws:' || url.protocol === 'wss:') {
+    return createPublicClient({
+      transport: webSocket(rpcUrl),
+    });
+  }
+  return createPublicClient({
+    transport: http(rpcUrl),
   });
-
-  return createTestClient({
-    chain: foundry,
-    mode: 'anvil',
-    transport: http(),
-  }).extend(publicActions);
 }
 
 async function main() {
@@ -61,27 +56,29 @@ async function main() {
 
   const client = getClient(options.rpcUrl);
 
-  const protocolEvents = UsdnProtocolAbi.filter((item) => item.type === 'event');
-  const usdnEvents = UsdnAbi.filter((item) => item.type === 'event');
-  const middlewareEvents = OracleMiddlewareAbi.filter((item) => item.type === 'event');
+  const protocolEvents = IUsdnProtocolAbi.filter((item) => item.type === 'event');
+  const usdnEvents = IUsdnAbi.filter((item) => item.type === 'event');
+  const rebalancerEvents = IRebalancerAbi.filter((item) => item.type === 'event');
+
+  const startBlock = await client.getBlockNumber() - BigInt(options.blocks);
 
   const protocolLogs = await client.getLogs({
     address: options.protocol,
     events: protocolEvents,
-    fromBlock: 0n,
+    fromBlock: startBlock,
   });
   const usdnLogs = await client.getLogs({
     address: options.usdn,
     events: usdnEvents,
-    fromBlock: 0n,
+    fromBlock: startBlock,
   });
-  const middlewareLogs = await client.getLogs({
-    address: options.middleware,
-    events: middlewareEvents,
-    fromBlock: 0n,
+  const rebalancerLogs = await client.getLogs({
+    address: options.rebalancer,
+    events: rebalancerEvents,
+    fromBlock: startBlock,
   });
 
-  const logs = [...protocolLogs, ...usdnLogs, ...middlewareLogs];
+  const logs = [...protocolLogs, ...usdnLogs, ...rebalancerLogs];
   logs.sort((a, b) => {
     const blockDiff = a.blockNumber - b.blockNumber;
     if (blockDiff === 0n) {
@@ -96,7 +93,7 @@ async function main() {
       ? pc.green('USDN Protocol')
       : isAddressEqual(log.address, options.usdn)
         ? pc.blue('USDN token')
-        : pc.red('Oracle Middleware');
+        : pc.red('Rebalancer');
     console.log('Contract:', contractName, pc.dim(`(${log.address})`));
     const block = await client.getBlock({ blockNumber: log.blockNumber });
     console.log('Timestamp:', block.timestamp.toString());
@@ -110,8 +107,15 @@ async function main() {
     console.log('Event name:', pc.yellow(pc.bold(log.eventName)));
     console.log('Args:');
     for (const arg of Object.entries(log.args)) {
+      // if value is an object, we stringify it
+      const value = typeof arg[1] === 'object' ? JSON.stringify(arg[1], (_, value) =>
+        typeof value === 'bigint'
+            ? value.toString()
+            : value
+      ) : arg[1];
+      // if value is a bigint, we add the value in ether
       const formattedValue = typeof arg[1] === 'bigint' && arg[1] !== 0n ? pc.dim(` (${formatEther(arg[1])})`) : '';
-      console.log(pc.cyan(`  ${arg[0]}:`), `${arg[1]}${formattedValue}`);
+      console.log(pc.cyan(`  ${arg[0]}:`), `${value}${formattedValue}`);
     }
     console.log(pc.dim('-------------------------'));
   }
