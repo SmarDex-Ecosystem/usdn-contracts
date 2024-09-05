fe538405d647b0f1a54a64aa9a8ea15e19d8ca16
beeb <703631+beeb@users.noreply.github.com>
2024-09-05 11:19:23 +0200

    test: fix

--- test/unit/UsdnProtocol/Pending.t.sol
+++ test/unit/UsdnProtocol/Pending.t.sol
@@ -323,7 +323,7 @@ contract TestUsdnProtocolPending is UsdnProtocolBaseFixture {
         DepositPendingAction memory pendingDeposit = DepositPendingAction({
             action: ProtocolAction.ValidateDeposit,
             timestamp: timestamp,
-            __unused: 0,
+            _unused: 0,
             to: USER_1,
             validator: USER_1,
             securityDepositValue: 0,
@@ -393,7 +393,7 @@ contract TestUsdnProtocolPending is UsdnProtocolBaseFixture {
         DepositPendingAction memory pendingDeposit = DepositPendingAction({
             action: ProtocolAction.ValidateDeposit,
             timestamp: timestamp,
-            __unused: 0,
+            _unused: 0,
             to: USER_1,
             validator: USER_1,
             securityDepositValue: 0,
