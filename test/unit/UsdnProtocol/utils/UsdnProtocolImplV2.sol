8d3d1e6b90b79e7a23ac305699c70a92dbf45835
samooyo <samidarnaud@gmail.com>
2024-09-05 09:44:45 +0200

    test: test the fix on CI

--- test/unit/UsdnProtocol/utils/UsdnProtocolImplV2.sol
+++ test/unit/UsdnProtocol/utils/UsdnProtocolImplV2.sol
@@ -2,7 +2,10 @@
 pragma solidity 0.8.26;
 
 import { UsdnProtocolImpl } from "../../../../src/UsdnProtocol/UsdnProtocolImpl.sol";
+
+import { TickMath } from "../../../../src/libraries/TickMath.sol";
 import { IUsdnProtocolImplV2 } from "./IUsdnProtocolImplV2.sol";
+import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 
 /// @custom:oz-upgrades-from UsdnProtocolImpl
 contract UsdnProtocolImplV2 is UsdnProtocolImpl, IUsdnProtocolImplV2 {
@@ -15,5 +18,30 @@ contract UsdnProtocolImplV2 is UsdnProtocolImpl, IUsdnProtocolImplV2 {
 
     function retBool() public pure returns (bool) {
         return true;
+    }
+
+    function makeItBig() public pure {
+        uint256 newVariable2 = 100;
+        bool test = true;
+        uint256 test2 = 100 * 100 * newVariable2;
+        int256 test3 = -100;
+        int24 tickWithPenalty_ = 1000;
+        uint24 liqPenalty = 1000;
+        int24 roundedTick_ = 100;
+        int24 tickSpacing = 100;
+        int24 tickWithPenalty = 98;
+        test = test2 > 1000;
+        tickWithPenalty_ = 0;
+
+        if (test3 < 0) {
+            roundedTick_ = -int24(int256(FixedPointMathLib.divUp(uint256(int256(-test3)), uint256(int256(tickSpacing)))))
+                * tickSpacing;
+            int24 minTickWithPenalty = TickMath.MIN_TICK + int24(liqPenalty);
+            if (roundedTick_ < minTickWithPenalty) {
+                roundedTick_ = minTickWithPenalty - (minTickWithPenalty % tickSpacing);
+            }
+        } else {
+            roundedTick_ = (tickWithPenalty / tickSpacing) * tickSpacing;
+        }
     }
 }
