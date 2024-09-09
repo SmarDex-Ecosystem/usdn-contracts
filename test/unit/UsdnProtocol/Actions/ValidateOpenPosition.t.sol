aa4e4cea5c1b84a320b6b20dbe11bcf53a03b57e
Lilyan Bastien <91454320+lilyanB@users.noreply.github.com>
2024-09-09 15:53:10 +0200

    feat: add max leverage on init open (#559)
    * feat: add maxLeverage
    
    * test: fix test
    
    * test: fix test
    
    * test: fix test
    
    * test: fix all tests
    
    * test: add corresponding test
    
    * feat: use params
    
    * feat: fix comments
    
    * feat: fix comments
    
    * test: fix decimals
    
    * feat: remove file

--- test/unit/UsdnProtocol/Actions/ValidateOpenPosition.t.sol
+++ test/unit/UsdnProtocol/Actions/ValidateOpenPosition.t.sol
@@ -178,6 +178,7 @@ contract TestUsdnProtocolActionsValidateOpenPosition is UsdnProtocolBaseFixture 
         (, PositionId memory posId) = protocol.initiateOpenPosition(
             uint128(LONG_AMOUNT),
             desiredLiqPrice,
+            protocol.getMaxLeverage(),
             to,
             payable(validator),
             NO_PERMIT2,
@@ -259,6 +260,7 @@ contract TestUsdnProtocolActionsValidateOpenPosition is UsdnProtocolBaseFixture 
         (, PositionId memory posId) = protocol.initiateOpenPosition(
             uint128(LONG_AMOUNT),
             CURRENT_PRICE * 9 / 10,
+            protocol.getMaxLeverage(),
             address(this),
             payable(this),
             NO_PERMIT2,
@@ -335,6 +337,7 @@ contract TestUsdnProtocolActionsValidateOpenPosition is UsdnProtocolBaseFixture 
         (, PositionId memory posId) = protocol.initiateOpenPosition(
             uint128(LONG_AMOUNT),
             CURRENT_PRICE * 9 / 10,
+            protocol.getMaxLeverage(),
             address(this),
             payable(this),
             NO_PERMIT2,
@@ -407,6 +410,7 @@ contract TestUsdnProtocolActionsValidateOpenPosition is UsdnProtocolBaseFixture 
         (, data.tempPosId) = protocol.initiateOpenPosition(
             uint128(LONG_AMOUNT),
             CURRENT_PRICE * 9 / 10,
+            protocol.getMaxLeverage(),
             address(this),
             payable(this),
             NO_PERMIT2,
@@ -584,6 +588,7 @@ contract TestUsdnProtocolActionsValidateOpenPosition is UsdnProtocolBaseFixture 
         protocol.initiateOpenPosition{ value: 0.5 ether }(
             uint128(LONG_AMOUNT),
             desiredLiqPrice,
+            protocol.getMaxLeverage(),
             address(this),
             USER_1,
             NO_PERMIT2,
