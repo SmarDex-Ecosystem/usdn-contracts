d864b8c76043f1f905742ea6e34d8e1fdab1983a
lilyanb <lilyan.bastien@gmail.com>
2024-09-11 13:30:16 +0200

    test: use variable

--- test/unit/UsdnProtocol/utils/Fixtures.sol
+++ test/unit/UsdnProtocol/utils/Fixtures.sol
@@ -327,7 +327,7 @@ contract UsdnProtocolBaseFixture is BaseFixture, IUsdnProtocolErrors, IEventsErr
         uint256 sharesOf = usdn.sharesOf(user);
         usdn.approve(address(protocol), usdn.convertToTokensRoundUp(sharesOf));
         protocol.initiateWithdrawal{ value: securityDepositValue }(
-            uint152(sharesOf), 0, user, payable(user), priceData, EMPTY_PREVIOUS_DATA
+            uint152(sharesOf), disableSharesOutMin, user, payable(user), priceData, EMPTY_PREVIOUS_DATA
         );
         _waitDelay();
 
