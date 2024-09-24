83515b56e014bc4421a0f2e97c490ba517d01638
Sam <46531012+samooyo@users.noreply.github.com>
2024-09-24 15:05:17 +0200

    perf: reduce protocol size (#600)
    * perf: _validateMultipleActionable in ActionsUtils
    
    * perf: moved some functions in the fallback
    
    * build: update all pkgs

--- src/UsdnProtocol/libraries/UsdnProtocolActionsLongLibrary.sol
+++ src/UsdnProtocol/libraries/UsdnProtocolActionsLongLibrary.sol
@@ -265,7 +265,7 @@ library UsdnProtocolActionsLongLibrary {
 
             // move the position to its new tick, update its total expo, and return the new tickVersion and index
             // remove position from old tick completely
-            _removeAmountFromPosition(
+            Long._removeAmountFromPosition(
                 s, data.action.tick, data.action.index, data.pos, data.pos.amount, data.pos.totalExpo
             );
             // update position total expo (because of new leverage / liq price)
@@ -392,57 +392,6 @@ library UsdnProtocolActionsLongLibrary {
         }
         // update the accumulator with the correct tick price (depending on the liquidation penalty value)
         liqMultiplierAccumulator_ = s._liqMultiplierAccumulator.add(HugeUint.wrap(unadjustedTickPrice * long.totalExpo));
-        s._liqMultiplierAccumulator = liqMultiplierAccumulator_;
-    }
-
-    /**
-     * @notice Remove the provided total amount from its position and update the tick data and position
-     * @dev Note: this method does not update the long balance
-     * If the amount to remove is greater than or equal to the position's total amount, the position is deleted instead
-     * @param s The storage of the protocol
-     * @param tick The tick to remove from
-     * @param index Index of the position in the tick array
-     * @param pos The position to remove the amount from
-     * @param amountToRemove The amount to remove from the position
-     * @param totalExpoToRemove The total expo to remove from the position
-     * @return liqMultiplierAccumulator_ The updated liquidation multiplier accumulator
-     */
-    function _removeAmountFromPosition(
-        Types.Storage storage s,
-        int24 tick,
-        uint256 index,
-        Types.Position memory pos,
-        uint128 amountToRemove,
-        uint128 totalExpoToRemove
-    ) public returns (HugeUint.Uint512 memory liqMultiplierAccumulator_) {
-        (bytes32 tickHash,) = Utils._tickHash(s, tick);
-        Types.TickData storage tickData = s._tickData[tickHash];
-        uint256 unadjustedTickPrice =
-            TickMath.getPriceAtTick(Utils.calcTickWithoutPenalty(tick, tickData.liquidationPenalty));
-        if (amountToRemove < pos.amount) {
-            Types.Position storage position = s._longPositions[tickHash][index];
-            position.totalExpo = pos.totalExpo - totalExpoToRemove;
-
-            unchecked {
-                position.amount = pos.amount - amountToRemove;
-            }
-        } else {
-            totalExpoToRemove = pos.totalExpo;
-            tickData.totalPos -= 1;
-            --s._totalLongPositions;
-
-            // remove from tick array (set to zero to avoid shifting indices)
-            delete s._longPositions[tickHash][index];
-            if (tickData.totalPos == 0) {
-                // we removed the last position in the tick
-                s._tickBitmap.unset(Utils._calcBitmapIndexFromTick(s, tick));
-            }
-        }
-
-        s._totalExpo -= totalExpoToRemove;
-        tickData.totalExpo -= totalExpoToRemove;
-        liqMultiplierAccumulator_ =
-            s._liqMultiplierAccumulator.sub(HugeUint.wrap(unadjustedTickPrice * totalExpoToRemove));
         s._liqMultiplierAccumulator = liqMultiplierAccumulator_;
     }
 
@@ -719,7 +668,7 @@ library UsdnProtocolActionsLongLibrary {
 
         s._balanceLong -= data.tempPositionValue;
 
-        _removeAmountFromPosition(
+        Long._removeAmountFromPosition(
             s, params.posId.tick, params.posId.index, data.pos, params.amountToClose, data.totalExpoToClose
         );
 
