799456bbc1814a9f7ef7a6fd261c30e9df55023d
lilyanb <lilyan.bastien@gmail.com>
2024-10-21 11:33:47 +0200

    feat: add MultiMinter and script

---
+++ script/03_DeployMultiMint.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { console } from "forge-std/Script.sol";
+import { Script } from "forge-std/Script.sol";
+
+import { IMultiMinter, IOwnable, MultiMinter } from "../src/utils/MultiMinter.sol";
+import { Sdex as SdexSepolia } from "../src/utils/sepolia/tokens/Sdex.sol";
+import { WstETH as WstETHSepolia } from "../src/utils/sepolia/tokens/WstETH.sol";
+
+contract DeployMultiMint is Script {
+    address constant SDEX_SEPOLIA = 0xad65D93735816011CE25674A988E61b74C178D0F;
+    address constant MULTIMINT_SEPOLIA = 0x96dd368a9894D1533Edd329032E39eE07d09321c;
+
+    function run() external returns (MultiMinter MultiMinter_) {
+        address deployerAddress = vm.envAddress("DEPLOYER_ADDRESS");
+        address lastMultiMintOwnerAddress = vm.envAddress("LAST_MULTIMINT_OWNER_ADDRESS");
+        uint64 nonceDeployer = vm.getNonce(deployerAddress);
+        uint64 nonce = vm.getNonce(lastMultiMintOwnerAddress);
+
+        require(block.chainid == 11_155_111, "DeployMultiMint: allowed only on the test environment");
+        require(nonceDeployer == 0, "nonceDeployer must be 0");
+        require(nonce == 0, "nonce must be 0");
+
+        vm.broadcast(deployerAddress);
+
+        WstETHSepolia wstEth_ = new WstETHSepolia();
+        MultiMinter_ = new MultiMinter(SDEX_SEPOLIA, address(wstEth_));
+        wstEth_.transferOwnership(address(MultiMinter_));
+
+        vm.stopBroadcast();
+
+        vm.broadcast(lastMultiMintOwnerAddress);
+        IMultiMinter(MULTIMINT_SEPOLIA).transferOwnershipOf(IOwnable(SDEX_SEPOLIA), address(MultiMinter_));
+        vm.stopBroadcast();
+
+        console.log("WstETHSepolia address", address(wstEth_));
+        console.log("SdexSepolia address", SDEX_SEPOLIA);
+        console.log("MultiMinter address", address(MultiMinter_));
+    }
+}
---
+++ src/utils/MultiMinter.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.26;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+interface IMintable {
+    function mint(address, uint256) external;
+}
+
+interface IWstETH is IMintable {
+    function setStEthPerToken(uint256 stEthAmount) external;
+}
+
+interface IOwnable {
+    function transferOwnership(address newOwner) external;
+}
+
+interface IMultiMinter {
+    struct Call {
+        address target;
+        bytes callData;
+    }
+
+    function mint(address adrs, uint256 amountSDEX, uint256 amountWSTETH) external;
+
+    function mint(address adrs, uint256 amountSDEX, uint256 amountWSTETH, uint256 amountETH) external payable;
+
+    function setStEthPerToken(uint256 stEthAmount) external;
+
+    function transferOwnershipOf(IOwnable contractAdr, address newOwner) external;
+
+    function sweep(address to) external;
+
+    function aggregateOnlyOwner(Call[] calldata calls)
+        external
+        payable
+        returns (uint256 blockNumber, bytes[] memory returnData);
+}
+
+contract MultiMinter is IMultiMinter, Ownable2Step {
+    IMintable immutable SDEX;
+    IWstETH immutable WSTETH;
+
+    constructor(address sdex, address asset) Ownable(msg.sender) {
+        SDEX = IMintable(sdex);
+        WSTETH = IWstETH(asset);
+    }
+
+    // ensure compatibility with old impl.
+    function mint(address adrs, uint256 amountSDEX, uint256 amountWSTETH) external onlyOwner {
+        mint(adrs, amountSDEX, amountWSTETH, 0);
+    }
+
+    function mint(address adrs, uint256 amountSDEX, uint256 amountWSTETH, uint256 amountETH) public payable onlyOwner {
+        if (amountSDEX != 0) {
+            SDEX.mint(adrs, amountSDEX);
+        }
+
+        if (amountWSTETH != 0) {
+            WSTETH.mint(adrs, amountWSTETH);
+        }
+
+        if (amountETH != 0) {
+            (bool success,) = adrs.call{ value: amountETH }("");
+            require(success, "Error while sending Ether");
+        }
+    }
+
+    function setStEthPerToken(uint256 stEthAmount) external onlyOwner {
+        WSTETH.setStEthPerToken(stEthAmount);
+    }
+
+    function transferOwnershipOf(IOwnable contractAdr, address newOwner) external onlyOwner {
+        contractAdr.transferOwnership(newOwner);
+    }
+
+    function sweep(address to) external onlyOwner {
+        (bool success,) = to.call{ value: address(this).balance }("");
+        require(success, "Error while sending Ether");
+    }
+
+    // just in case...
+    function aggregateOnlyOwner(Call[] calldata calls)
+        external
+        payable
+        onlyOwner
+        returns (uint256 blockNumber, bytes[] memory returnData)
+    {
+        blockNumber = block.number;
+        uint256 length = calls.length;
+        returnData = new bytes[](length);
+        Call calldata call;
+        for (uint256 i = 0; i < length;) {
+            bool success;
+            call = calls[i];
+            (success, returnData[i]) = call.target.call(call.callData);
+            require(success, "Multicall3: call failed");
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
