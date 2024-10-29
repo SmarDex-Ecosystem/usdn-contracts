a4d76933143b37077c6c0979b453ff2b65e44603
Lilyan Bastien <91454320+lilyanB@users.noreply.github.com>
2024-10-29 13:17:49 +0100

    chore: add natspec for blacklist asset (#648)
    * chore: add natspec for blacklist asset
    
    * chore: change natspec
    
    * chore: fix natspec

--- src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol
@@ -550,6 +550,7 @@ interface IUsdnProtocolTypes {
      * @dev A tick spacing of 1 is equivalent to a 0.01% increase in liquidation price between ticks. A tick spacing of
      * 100 is equivalent to a ~1.005% increase in liquidation price between ticks
      * @param _asset The asset ERC20 contract
+     * Assets with a blacklist are not supported because the protocol would be DoS if transfers revert
      * @param _assetDecimals The asset decimals
      * @param _priceFeedDecimals The price feed decimals (18)
      * @param _usdn The USDN ERC20 contract
