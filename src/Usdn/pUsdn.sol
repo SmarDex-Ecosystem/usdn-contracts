764baf685d0f965f1bc0db3e4c885eed7eb51c58
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-11-05 14:58:35 +0100

    docs: add natspec

--- src/Usdn/pUsdn.sol
+++ src/Usdn/pUsdn.sol
@@ -5,6 +5,11 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract pUsdn is ERC20, ERC20Permit {
+    /**
+     * @notice Create an instance of the pUSDN token
+     * @param hotWallet Address to create the liquidity pool
+     * @param multisig Address of the multisig
+     */
     constructor(address hotWallet, address multisig)
         ERC20("pre Ultimate Synthetic Delta Neutral", "pUSDN")
         ERC20Permit("pre Ultimate Synthetic Delta Neutral")
