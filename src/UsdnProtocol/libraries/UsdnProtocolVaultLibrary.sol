8c472c32a02f1a81e03e2d0249be2bc7517f48a7
samooyo <samidarnaud@gmail.com>
2024-11-20 10:26:09 +0100

    chore: remove console.log

--- src/UsdnProtocol/libraries/UsdnProtocolVaultLibrary.sol
+++ src/UsdnProtocol/libraries/UsdnProtocolVaultLibrary.sol
@@ -21,8 +21,6 @@ import { UsdnProtocolCoreLibrary as Core } from "./UsdnProtocolCoreLibrary.sol";
 import { UsdnProtocolLongLibrary as Long } from "./UsdnProtocolLongLibrary.sol";
 import { UsdnProtocolUtilsLibrary as Utils } from "./UsdnProtocolUtilsLibrary.sol";
 
-import { console } from "forge-std/Test.sol";
-
 library UsdnProtocolVaultLibrary {
     using DoubleEndedQueue for DoubleEndedQueue.Deque;
     using SafeCast for uint256;
@@ -1146,7 +1144,6 @@ library UsdnProtocolVaultLibrary {
         int256 newVaultExpo =
             s._balanceVault.toInt256().safeAdd(s._pendingBalanceVault).safeSub(withdrawalValue.toInt256());
 
-        console.log("newVaultExpo:", newVaultExpo);
         // cannot be calculated if equal to zero
         if (newVaultExpo <= 0) {
             revert IUsdnProtocolErrors.UsdnProtocolEmptyVault();
