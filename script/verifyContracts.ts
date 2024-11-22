4b11e1b8fbaacc18fafa7c7eb33f89593fb75f5a
Paulalex85 <tessier.paulalexandre@orange.fr>
2024-11-22 22:18:52 +0100

    feat: add debug and rpc parameter

--- script/verifyContracts.ts
+++ script/verifyContracts.ts
@@ -6,9 +6,9 @@ const program = new Command();
 
 program.description('Verify contract from broadcast file')
     .argument('<path>', 'path to the broadcast file')
-    // .requiredOption('-r', '--rpc-url <url>', "The RPC endpoint")
-    .requiredOption('-e', '--etherscan-api-key <key>', "The Etherscan (or equivalent) API key")
-    .option('--verifier-url <url>', "The verifier URL, if using a custom provider")
+    .requiredOption('-r, --rpc-url <rpcUrl>', "The RPC endpoint")
+    .requiredOption('-e, --etherscan-api-key <etherscanApiKey>', "The Etherscan (or equivalent) API key")
+    .option('--verifier-url <verifierUrl>', "The verifier URL, if using a custom provider")
     .option('-d, --debug', 'output extra debugging')
     .parse(process.argv);
 
@@ -17,7 +17,12 @@ const options = program.opts();
 const DEBUG = !!options.debug;
 let etherscanApiKey: string = options.etherscanApiKey;
 let verifierUrl: string = options.verifierUrl;
+let rpcUrl: string = options.rpcUrl;
 const verbose: string = DEBUG ? '--verbose' : '';
+
+if (DEBUG) console.log(`etherscanApiKey : ${etherscanApiKey}`)
+if (DEBUG) console.log(`verifierUrl : ${verifierUrl}`)
+if (DEBUG) console.log(`broadcastPath : ${broadcastPath}`)
 
 if (!existsSync(broadcastPath)) {
     console.log('\nPlease provide a valid broadcast file');
@@ -26,6 +31,7 @@ if (!existsSync(broadcastPath)) {
 
 if (etherscanApiKey) etherscanApiKey = `-e ${etherscanApiKey}`;
 if (verifierUrl) verifierUrl = `--verifier-url ${verifierUrl}`;
+if (rpcUrl) rpcUrl = `--rpc-url ${rpcUrl}`;
 
 const file = readFileSync(broadcastPath);
 const broadcast = JSON.parse(file.toString());
@@ -34,7 +40,10 @@ broadcast.transactions.filter(transaction =>
 ).forEach(transaction => {
     const address: string = transaction.contractAddress;
     const contractName: string = transaction.contractName;
-    const cli = `forge verify-contract ${address} ${contractName} --guess-constructor-args --watch ${etherscanApiKey} ${verifierUrl} ${verbose}`;
+    if (DEBUG) console.log(`transaction to verify with address : ${address} and name : ${contractName}`)
+
+    const cli = `forge verify-contract ${address} ${contractName} --guess-constructor-args --watch ${rpcUrl} ${etherscanApiKey} ${verifierUrl} ${verbose}`;
+    if (DEBUG) console.log(`cli : ${cli}`)
     exec(cli, (error, stdout, stderr) => {
         console.log(stdout);
         console.error(stderr);
