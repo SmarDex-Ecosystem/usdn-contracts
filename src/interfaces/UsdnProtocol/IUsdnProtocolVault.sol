1d2c1fda74be54d4662cbd354e473f7c6b805e54
kirienzo-eth <kirienzo.eth@gmail.com>
2024-12-02 15:36:08 +0100

    docs(vault): improve NatSpecs for the IUsdnProtocolVault interface

--- src/interfaces/UsdnProtocol/IUsdnProtocolVault.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolVault.sol
@@ -7,32 +7,35 @@ pragma solidity >=0.8.0;
  */
 interface IUsdnProtocolVault {
     /**
-     * @notice Get the predicted value of the USDN token price for the given asset price and timestamp
-     * @dev The effect of the funding and any profit or loss of the long positions since the last contract state
-     * update is taken into account
-     * @param currentPrice The current or predicted asset price
-     * @param timestamp The timestamp corresponding to `currentPrice`
-     * @return The predicted value of the USDN token price
+     * @notice Get the predicted value of the USDN token price for the given asset price and timestamp.
+     * @dev The effects of the funding and the PnL of the long positions since the last contract state update are taken
+     * into account.
+     * @param currentPrice The current or predicted asset price.
+     * @param timestamp The timestamp corresponding to `currentPrice`.
+     * @return price_ The predicted value of the USDN token price.
      */
-    function usdnPrice(uint128 currentPrice, uint128 timestamp) external view returns (uint256);
+    function usdnPrice(uint128 currentPrice, uint128 timestamp) external view returns (uint256 price_);
 
     /**
-     * @notice Get the value of the USDN token price for the given asset price and the current timestamp
-     * @dev The effect of the funding and any profit or loss of the long positions since the last contract state
-     * update is taken into account
-     * @param currentPrice The most recent/current asset price
-     * @return The value of the USDN token price
+     * @notice Get the value of the USDN token price for the given asset price at the current timestamp.
+     * @dev The effects of the funding and the PnL of the long positions since the last contract state update are taken
+     * into account.
+     * @param currentPrice The current or predicted asset price.
+     * @return price_ The value of the USDN token price.
      */
-    function usdnPrice(uint128 currentPrice) external view returns (uint256);
+    function usdnPrice(uint128 currentPrice) external view returns (uint256 price_);
 
     /**
-     * @notice Get the predicted value of the vault balance for the given asset price and timestamp
-     * @dev The effects of the funding and any profit or loss of the long positions since the last contract state
-     * update is taken into account, as well as the fees. If the provided timestamp is older than the last state
-     * update, the function reverts with `UsdnProtocolTimestampTooOld`. The value cannot be below 0
-     * @param currentPrice The current or predicted asset price
-     * @param timestamp The timestamp corresponding to `currentPrice`
-     * @return The vault balance
+     * @notice Get the amount of assets in the vault for the given asset price and timestamp.
+     * @dev The effects of the funding, the PnL of the long positions and the accumulated fees since the last contract
+     * state update are taken into account, but not liquidations. If the provided timestamp is older than the last
+     * state update, the function reverts with `UsdnProtocolTimestampTooOld`.
+     * @param currentPrice The current or predicted asset price.
+     * @param timestamp The timestamp corresponding to `currentPrice` (must not be below _lastUpdateTimestamp).
+     * @return available_ The available vault balance (cannot be below 0).
      */
-    function vaultAssetAvailableWithFunding(uint128 currentPrice, uint128 timestamp) external view returns (uint256);
+    function vaultAssetAvailableWithFunding(uint128 currentPrice, uint128 timestamp)
+        external
+        view
+        returns (uint256 available_);
 }
