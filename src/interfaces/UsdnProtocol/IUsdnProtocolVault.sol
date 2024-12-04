6ce17f1af12b9c94239fa9d014eed016aef8022a
kirienzo-eth <kirienzo.eth@gmail.com>
2024-12-04 10:03:20 +0100

    docs(vault): improve NatSpecs

--- src/interfaces/UsdnProtocol/IUsdnProtocolVault.sol
+++ src/interfaces/UsdnProtocol/IUsdnProtocolVault.sol
@@ -7,21 +7,21 @@ pragma solidity >=0.8.0;
  */
 interface IUsdnProtocolVault {
     /**
-     * @notice Gets the predicted value of the USDN token price for the given asset price and timestamp.
+     * @notice Calculates the predicted value of the USDN token price for the given asset price and timestamp.
      * @dev The effects of the funding and the PnL of the long positions since the last contract state update are taken
      * into account.
      * @param currentPrice The current or predicted asset price.
      * @param timestamp The timestamp corresponding to `currentPrice`.
-     * @return price_ The predicted value of the USDN token price.
+     * @return price_ The predicted USDN token price.
      */
     function usdnPrice(uint128 currentPrice, uint128 timestamp) external view returns (uint256 price_);
 
     /**
-     * @notice Gets the value of the USDN token price for the given asset price at the current timestamp.
+     * @notice Calculates the USDN token price based on the given asset price at the current timestamp.
      * @dev The effects of the funding and the PnL of the long positions since the last contract state update are taken
      * into account.
-     * @param currentPrice The current or predicted asset price.
-     * @return price_ The value of the USDN token price.
+     * @param currentPrice The asset price at `block.timestamp`.
+     * @return price_ The calculated USDN token price.
      */
     function usdnPrice(uint128 currentPrice) external view returns (uint256 price_);
 
@@ -31,8 +31,8 @@ interface IUsdnProtocolVault {
      * state update are taken into account, but not liquidations. If the provided timestamp is older than the last
      * state update, the function reverts with `UsdnProtocolTimestampTooOld`.
      * @param currentPrice The current or predicted asset price.
-     * @param timestamp The timestamp corresponding to `currentPrice` (must not be below _lastUpdateTimestamp).
-     * @return available_ The available vault balance (cannot be below 0).
+     * @param timestamp The timestamp corresponding to `currentPrice` (must not be earlier than _lastUpdateTimestamp).
+     * @return available_ The available vault balance (cannot be less than 0).
      */
     function vaultAssetAvailableWithFunding(uint128 currentPrice, uint128 timestamp)
         external
