7bab89fa32be708d16561d3c70cab9eefc6b35b7
samooyo <samidarnaud@gmail.com>
2024-12-10 15:24:37 +0100

    docs(iusdn-events): improve events natspecs

--- src/interfaces/Usdn/IUsdnEvents.sol
+++ src/interfaces/Usdn/IUsdnEvents.sol
@@ -5,19 +5,20 @@ import { IRebaseCallback } from "./IRebaseCallback.sol";
 
 /**
  * @title Events for the USDN token contract
- * @notice Contains all custom events emitted by the USDN token contract (omitting events from OpenZeppelin)
+ * @notice Defines all custom events emitted by the USDN token contract.
  */
 interface IUsdnEvents {
     /**
-     * @notice Emitted when the divisor is adjusted to rebase the user balances and total supply
-     * @param oldDivisor divisor before rebase
-     * @param newDivisor divisor after rebase
+     * @notice The divisor is updated, emitted during a rebase.
+     * @param oldDivisor The divisor value before the rebase.
+     * @param newDivisor The new divisor value.
      */
     event Rebase(uint256 oldDivisor, uint256 newDivisor);
 
     /**
-     * @notice Emitted when the rebase handler address is updated
-     * @param newHandler The address of the new rebase handler
+     * @notice The rebase handler address is updated.
+     * @dev The rebase handler is a contract that is called when a rebase occurs.
+     * @param newHandler The address of the new rebase handler contract.
      */
     event RebaseHandlerUpdated(IRebaseCallback newHandler);
 }
