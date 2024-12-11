10f8a2c2dd18cc67fe0f2fcbf7d122a4f46d1924
samooyo <samidarnaud@gmail.com>
2024-12-11 19:31:21 +0100

    feat(script): upgrade script for tenderly v0.24

--- script/50_Upgrade.s.sol
+++ script/50_Upgrade.s.sol
@@ -3,58 +3,40 @@ pragma solidity 0.8.26;
 
 import { Script } from "forge-std/Script.sol";
 
-import { Options, Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
-
 import { Utils } from "./utils/Utils.s.sol";
 
-import { UsdnProtocolFallback } from "../src/UsdnProtocol/UsdnProtocolFallback.sol";
 import { UsdnProtocolImpl } from "../src/UsdnProtocol/UsdnProtocolImpl.sol";
 import { UsdnProtocolConstantsLibrary as Constants } from
     "../src/UsdnProtocol/libraries/UsdnProtocolConstantsLibrary.sol";
 import { IUsdnProtocol } from "../src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
 
 contract Upgrade is Script {
-    Utils _utils = new Utils();
     address _deployerAddress;
     IUsdnProtocol _usdnProtocol;
 
-    function run() external {
-        // Check and save the required environment variables
+    function run() external returns (UsdnProtocolImpl newUsdnProtocolImpl_) {
         _handleEnvVariables();
 
-        // Make sure the address used has the permission to upgrade the protocol
+        vm.startBroadcast(_deployerAddress);
+        _usdnProtocol.grantRole(Constants.ADMIN_PROXY_UPGRADE_ROLE, _deployerAddress);
+        _usdnProtocol.grantRole(Constants.PROXY_UPGRADE_ROLE, _deployerAddress);
+
+        bytes32 oldImpl =
+            vm.load(address(_usdnProtocol), 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc);
+
         require(
             _usdnProtocol.hasRole(Constants.PROXY_UPGRADE_ROLE, _deployerAddress),
             "DEPLOYER_ADDRESS does not have the permission to upgrade the protocol"
         );
 
-        // clean and build contracts for openzeppelin module
-        _utils.cleanAndBuildContracts();
+        newUsdnProtocolImpl_ = new UsdnProtocolImpl();
+        _usdnProtocol.upgradeToAndCall(address(newUsdnProtocolImpl_), "");
 
-        // validate the Usdn protocol before deploying it
-        _utils.validateProtocol("UsdnProtocolImpl.sol", "UsdnProtocolFallback.sol");
+        bytes32 newImpl =
+            vm.load(address(_usdnProtocol), 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc);
 
-        vm.startBroadcast(_deployerAddress);
-
-        // we need to allow external library linking for the openzeppelin module
-        Options memory opts;
-        opts.unsafeAllow = "external-library-linking,state-variable-immutable";
-        opts.referenceContract = "UsdnProtocolImpl.sol";
-
-        // deploy the new fallback contract
-        UsdnProtocolFallback protocolFallback = new UsdnProtocolFallback();
-
-        Upgrades.upgradeProxy(
-            address(_usdnProtocol),
-            "UsdnProtocolImpl.sol",
-            // call the initialize function of the new implementation to upgrade the fallback contract
-            // if this function does not exist, you will need to implement it
-            // IMPORTANT: It is mandatory to add a call here, otherwise the transaction will revert
-            // the Solady implementation does not allow us to upgrade without calling a function afterwards.
-            // Consider also to upgrade the EIP712 version if the new implementation has an impact on delegation
-            abi.encodeWithSignature("initializeStorageV2(address)", (address(protocolFallback))),
-            opts
-        );
+        require(oldImpl != newImpl, "Upgrade failed");
+        require(address(uint160(uint256(newImpl))) == address(newUsdnProtocolImpl_), "Upgrade failed");
 
         vm.stopBroadcast();
     }
