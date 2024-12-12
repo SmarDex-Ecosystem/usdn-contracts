68011d644bd8db4e0ec53cc7487e5ef9e8ae308e
samooyo <samidarnaud@gmail.com>
2024-12-12 10:53:09 +0100

    docs(script): added some documentation on new script

--- script/50_Upgrade.s.sol
+++ script/50_Upgrade.s.sol
@@ -10,9 +10,14 @@ import { UsdnProtocolConstantsLibrary as Constants } from
     "../src/UsdnProtocol/libraries/UsdnProtocolConstantsLibrary.sol";
 import { IUsdnProtocol } from "../src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
 
+/**
+ * @title Upgrade script
+ * @notice This script is only made for upgrading the Usdn protocol v0.24.0 to v0.24.1 on Tenderly
+ */
 contract Upgrade is Script {
     address _deployerAddress;
     IUsdnProtocol _usdnProtocol;
+    bytes32 ImplAddrLocation = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     function run() external returns (UsdnProtocolImpl newUsdnProtocolImpl_) {
         _handleEnvVariables();
@@ -21,19 +26,17 @@ contract Upgrade is Script {
         _usdnProtocol.grantRole(Constants.ADMIN_PROXY_UPGRADE_ROLE, _deployerAddress);
         _usdnProtocol.grantRole(Constants.PROXY_UPGRADE_ROLE, _deployerAddress);
 
-        bytes32 oldImpl =
-            vm.load(address(_usdnProtocol), 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc);
-
         require(
             _usdnProtocol.hasRole(Constants.PROXY_UPGRADE_ROLE, _deployerAddress),
             "DEPLOYER_ADDRESS does not have the permission to upgrade the protocol"
         );
 
+        bytes32 oldImpl = vm.load(address(_usdnProtocol), ImplAddrLocation);
+
         newUsdnProtocolImpl_ = new UsdnProtocolImpl();
         _usdnProtocol.upgradeToAndCall(address(newUsdnProtocolImpl_), "");
 
-        bytes32 newImpl =
-            vm.load(address(_usdnProtocol), 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc);
+        bytes32 newImpl = vm.load(address(_usdnProtocol), ImplAddrLocation);
 
         require(oldImpl != newImpl, "Upgrade failed");
         require(address(uint160(uint256(newImpl))) == address(newUsdnProtocolImpl_), "Upgrade failed");
