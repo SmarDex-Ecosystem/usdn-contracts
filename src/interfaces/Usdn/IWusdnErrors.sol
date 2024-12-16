89a468b8e7cf4e66e94d22bec12967e8e90139e2
samooyo <samidarnaud@gmail.com>
2024-12-16 13:18:34 +0100

    docs(iwusdn-errors-events): improve natspecs

--- src/interfaces/Usdn/IWusdnErrors.sol
+++ src/interfaces/Usdn/IWusdnErrors.sol
@@ -2,20 +2,19 @@
 pragma solidity >=0.8.0;
 
 /**
- * @title Errors for the WUSDN token contract
- * @notice Contains all custom errors emitted by the WUSDN token contract (omitting errors from OpenZeppelin)
+ * @title Errors For The WUSDN Token Contract
+ * @notice Defines all custom errors emitted by the WUSDN token contract.
  */
 interface IWusdnErrors {
     /**
-     * @dev Indicates that the user has insufficient USDN balance to wrap `usdnAmount`
-     * @param usdnAmount The amount of USDN the user attempted to wrap
+     * @dev The user has insufficient USDN balance to wrap the given `usdnAmount`.
+     * @param usdnAmount The amount of USDN the user attempted to wrap.
      */
     error WusdnInsufficientBalance(uint256 usdnAmount);
 
     /**
-     * @dev Indicates that the user is attempting to wrap an amount of USDN shares
-     * that is lower than the minimum SHARES_RATIO required by the WUSDN token
-     * This results in a wrapped amount of zero WUSDN
+     * @dev The user is attempting to wrap an amount of USDN shares that is lower than the minimum {SHARES_RATIO}
+     * required by the WUSDN token. This results in a wrapped amount of zero WUSDN.
      */
     error WusdnWrapZeroAmount();
 }
