9447e2c53e3e5d37dae565b060a3f6ec565d5b9c
kirienzo-eth <kirienzo.eth@gmail.com>
2024-12-19 15:22:00 +0100

    docs(oracle-middleware): improve the NatSpecs of the `IOracleMiddlewareTypes` interface

--- src/interfaces/OracleMiddleware/IOracleMiddlewareTypes.sol
+++ src/interfaces/OracleMiddleware/IOracleMiddlewareTypes.sol
@@ -2,12 +2,11 @@
 pragma solidity >=0.8.0;
 
 /**
- * @notice The price and timestamp returned by the oracle middleware
- * @dev The timestamp is the timestamp of the price data, not the timestamp of the request
- * There is no need for optimization here, the struct is only used in memory and not in storage
- * @param price The validated asset price, potentially adjusted by the middleware
- * @param neutralPrice The neutral/average price of the asset
- * @param timestamp The timestamp of the price data
+ * @notice The price and timestamp returned by the oracle middleware.
+ * @dev There is no need for optimization here, the struct is only used in memory and not in storage.
+ * @param price The validated asset price, potentially adjusted by the middleware.
+ * @param neutralPrice The neutral/average price of the asset.
+ * @param timestamp The timestamp of the price data.
  */
 struct PriceInfo {
     uint256 price;
@@ -16,11 +15,10 @@ struct PriceInfo {
 }
 
 /**
- * @notice The price and timestamp returned by the chainlink oracle
- * @dev The timestamp is the timestamp of the price data, not the timestamp of the request
- * There is no need for optimization here, the struct is only used in memory and not in storage
- * @param price The asset price formatted by the middleware
- * @param timestamp The timestamp of the price data
+ * @notice The price and timestamp returned by the Chainlink oracle.
+ * @dev There is no need for optimization here, the struct is only used in memory and not in storage.
+ * @param price The asset price formatted by the middleware.
+ * @param timestamp When the price was published on chain.
  */
 struct ChainlinkPriceInfo {
     int256 price;
@@ -28,10 +26,10 @@ struct ChainlinkPriceInfo {
 }
 
 /**
- * @notice Struct representing a Pyth price with a uint256 price
- * @param price The price of the asset
- * @param conf The confidence interval around the price (in dollars, absolute value)
- * @param publishTime Unix timestamp describing when the price was published
+ * @notice Representation of a Pyth price with a uint256 price.
+ * @param price The price of the asset.
+ * @param conf The confidence interval around the price (in dollars, absolute value).
+ * @param publishTime Unix timestamp describing when the price was published.
  */
 struct FormattedPythPrice {
     uint256 price;
@@ -40,10 +38,10 @@ struct FormattedPythPrice {
 }
 
 /**
- * @notice The price and timestamp returned by the redstone oracle.
- * @dev The timestamp is the timestamp of the price data, not the timestamp of the request
- * @param price The asset price formatted by the middleware
- * @param timestamp The timestamp of the price data
+ * @notice The price and timestamp returned by the Redstone oracle.
+ * @dev There is no need for optimization here, the struct is only used in memory and not in storage.
+ * @param price The asset price formatted by the middleware.
+ * @param timestamp The timestamp of the price data.
  */
 struct RedstonePriceInfo {
     uint256 price;
@@ -51,8 +49,8 @@ struct RedstonePriceInfo {
 }
 
 /**
- * @notice Enum representing the confidence interval of a Pyth price
- * @dev Used by the middleware to determine which price to use in a confidence interval
+ * @notice The different confidence interval of a Pyth price.
+ * @dev Applied to the neutral price and available as `price`.
  */
 enum ConfidenceInterval {
     Up,
