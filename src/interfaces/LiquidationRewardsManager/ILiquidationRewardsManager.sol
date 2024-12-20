b588e781c43613e1de582067ee566f75ec4fa8da
Sam <46531012+samooyo@users.noreply.github.com>
2024-12-20 12:15:40 +0100

    docs(iliquidation-rewards-manager): improve natspecs (#792)
    * docs(iliquidation-rewards-manager): improve natspecs
    
    * docs: ILiquidationRewardsManager natpspecs
    
    * docs: ILiquidationRewardsManagerErrorsEventsTypes natspecs
    
    * docs: fixes typos
    
    * docs: added title to all interfaces
    
    * docs: fix comments on PR

--- src/interfaces/LiquidationRewardsManager/ILiquidationRewardsManager.sol
+++ src/interfaces/LiquidationRewardsManager/ILiquidationRewardsManager.sol
@@ -4,69 +4,65 @@ pragma solidity >=0.8.0;
 import { IBaseLiquidationRewardsManager } from "./IBaseLiquidationRewardsManager.sol";
 import { ILiquidationRewardsManagerErrorsEventsTypes } from "./ILiquidationRewardsManagerErrorsEventsTypes.sol";
 
+/**
+ * @title ILiquidationRewardsManager
+ * @notice Interface for managing liquidation rewards within the protocol.
+ */
 interface ILiquidationRewardsManager is IBaseLiquidationRewardsManager, ILiquidationRewardsManagerErrorsEventsTypes {
-    /* -------------------------------------------------------------------------- */
-    /*                                  Constants                                 */
-    /* -------------------------------------------------------------------------- */
-
     /**
-     * @notice Denominator for the reward multiplier, will give us a 0.01% basis point
-     * @return The BPS divisor
+     * @notice Gets the denominator used for the reward multipliers.
+     * @return The BPS divisor.
      */
     function BPS_DIVISOR() external pure returns (uint32);
 
     /**
-     * @notice Fixed amount of gas a transaction consumes
-     * @dev Is a uint256 to avoid overflows during gas usage calculations
-     * @return The base gas cost
+     * @notice Gets the fixed gas amount used as a base for transaction cost computations.
+     * @dev Stored as a uint256 to prevent overflow during gas usage computations.
+     * @return The base gas cost.
      */
     function BASE_GAS_COST() external pure returns (uint256);
 
     /**
-     * @notice Maximum gas used per tick liquidated
-     * @return The maximum gas used per tick
+     * @notice Gets the maximum allowable gas usage per liquidated tick.
+     * @return The maximum gas used per tick.
      */
     function MAX_GAS_USED_PER_TICK() external pure returns (uint256);
 
     /**
-     * @notice Maximum gas used for the rest of the computation
-     * @return The maximum gas used for the rest of the computation
+     * @notice Gets the maximum allowable gas usage for all other computations.
+     * @return The maximum gas used for additional computations.
      */
     function MAX_OTHER_GAS_USED() external pure returns (uint256);
 
     /**
-     * @notice Maximum gas used for the rebase
-     * @return The maximum gas used for the rebase
+     * @notice Gets the maximum allowable gas usage for rebase operations.
+     * @return The maximum gas used for rebase operations.
      */
     function MAX_REBASE_GAS_USED() external pure returns (uint256);
 
     /**
-     * @notice Maximum gas used for the optional rebalancer trigger
-     * @return The maximum gas used for the optional rebalancer trigger
+     * @notice Gets the maximum allowable gas usage for triggering the optional rebalancer.
+     * @return The maximum gas used for the optional rebalancer trigger.
      */
     function MAX_REBALANCER_GAS_USED() external pure returns (uint256);
 
-    /* -------------------------------------------------------------------------- */
-    /*                                  Getters Setters                           */
-    /* -------------------------------------------------------------------------- */
-
     /**
-     * @notice Returns the parameters used to calculate the rewards for a liquidation
-     * @return rewardsParameters_ The rewards parameters
+     * @notice Retrieves the current parameters used for reward calculations.
+     * @return rewardsParameters_ A struct containing the rewards parameters.
      */
     function getRewardsParameters() external view returns (RewardsParameters memory);
 
     /**
-     * @notice Set new parameters for the rewards calculation
-     * @param gasUsedPerTick Gas used per tick to liquidate
-     * @param otherGasUsed Gas used for the rest of the computation
-     * @param rebaseGasUsed Gas used for the optional USDN rebase
-     * @param rebalancerGasUsed Gas used for the optional rebalancer trigger
-     * @param baseFeeOffset Offset added to the block's base gas fee
-     * @param gasMultiplierBps Multiplier for the amount of gas used in BPS
-     * @param positionBonusMultiplierBps Multiplier for the position size bonus in BPS
-     * @param fixedReward Fixed amount added to the final reward (native currency, will be converted to wstETH)
-     * @param maxReward Upper limit for the rewards (native currency, will be converted to wstETH)
+     * @notice Updates the parameters used for calculating liquidation rewards.
+     * @param gasUsedPerTick The gas consumed per tick for liquidation.
+     * @param otherGasUsed The gas consumed for all additional computations.
+     * @param rebaseGasUsed The gas consumed for optional USDN rebase operation.
+     * @param rebalancerGasUsed The gas consumed for the optional rebalancer trigger.
+     * @param baseFeeOffset An offset added to the block's base gas fee.
+     * @param gasMultiplierBps The multiplier for the gas usage (in BPS).
+     * @param positionBonusMultiplierBps Multiplier for position size bonus (in BPS).
+     * @param fixedReward A fixed reward amount (in native currency, converted to wstETH).
+     * @param maxReward The maximum allowable reward amount (in native currency, converted to wstETH).
      */
     function setRewardsParameters(
         uint32 gasUsedPerTick,
