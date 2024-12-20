5af6185fc693802c243d06c3c8c1c708c52cef08
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2024-12-20 15:04:46 +0100

    docs(oracle-middleware): improve NatSpecs  of the oracle middleware's errors, events and types (#799)
    * docs(oracle-middleware): improve the NatSpecs of the `IOracleMiddlewareErrors` interface
    
    * docs(oracle-middleware): improve the NatSpecs of the `IOracleMiddlewareEvents` interface
    
    * docs(oracle-middleware): improve the NatSpecs of the `IOracleMiddlewareTypes` interface
    
    * docs(oracle-middleware): improve NatSpecs based on feedback
    
    * docs(oracle-middleware): improve NatSpecs based on feedbacks
    
    * docs(oracle-middleware): improve the title of the IOracleMiddlewareErrors interface

--- src/interfaces/OracleMiddleware/IOracleMiddlewareErrors.sol
+++ src/interfaces/OracleMiddleware/IOracleMiddlewareErrors.sol
@@ -2,70 +2,70 @@
 pragma solidity >=0.8.0;
 
 /**
- * @title IOracleMiddlewareErrors
- * @notice Errors for the oracle middleware
+ * @title Errors For The Middleware And Oracle Related Contracts
+ * @notice Defines all the custom errors thrown by the contracts related to the OracleMiddleware contract.
  */
 interface IOracleMiddlewareErrors {
     /**
-     * @notice Indicates that the oracle price is negative
-     * @param price The price returned by the oracle
+     * @notice The price returned by an oracle is negative.
+     * @param price The price returned by the oracle.
      */
     error OracleMiddlewareWrongPrice(int256 price);
 
     /**
-     * @notice Indicates that the oracle price is too old
-     * @param timestamp The timestamp of the price given by the oracle
+     * @notice The price returned by an oracle is too old.
+     * @param timestamp The timestamp of the price given by the oracle.
      */
     error OracleMiddlewarePriceTooOld(uint256 timestamp);
 
     /**
-     * @notice The oracle price is too recent
-     * @param timestamp The timestamp of the price given by the oracle
+     * @notice The price returned by an oracle is too recent.
+     * @param timestamp The timestamp of the price given by the oracle.
      */
     error OracleMiddlewarePriceTooRecent(uint256 timestamp);
 
     /**
-     * @notice Indicates that the pyth price reported a positive exponent (negative decimals)
-     * @param expo The price exponent
+     * @notice The Pyth price reported a positive exponent (negative decimals).
+     * @param expo The price exponent.
      */
     error OracleMiddlewarePythPositiveExponent(int32 expo);
 
-    /// @notice Indicates that the confidence ratio is too high
+    /// @notice Indicates that the confidence ratio is too high.
     error OracleMiddlewareConfRatioTooHigh();
 
-    /// @notice Indicates that an incorrect amount of ether was provided to cover the cost of price validation
+    /// @notice Indicates that an incorrect amount of tokens was provided to cover the cost of price validation.
     error OracleMiddlewareIncorrectFee();
 
     /**
-     * @notice The validation fee returned by the Pyth contract exceeded the safeguard value
-     * @param fee The value of the fee returned by Pyth
+     * @notice The validation fee returned by the Pyth contract exceeded the safeguard value.
+     * @param fee The required fee returned by Pyth.
      */
     error OracleMiddlewarePythFeeSafeguard(uint256 fee);
 
-    /// @notice The redstone price is more than triple or less than a third of the latest chainlink price
+    /// @notice The Redstone price is to divergent from the Chainlink price.
     error OracleMiddlewareRedstoneSafeguard();
 
     /**
-     * @notice Indicates that the withdrawal of the ether in the contract failed
-     * @param to The address that was supposed to receive the ether
+     * @notice The withdrawal of the Ether in the contract failed.
+     * @param to The address of the intended recipient.
      */
     error OracleMiddlewareTransferFailed(address to);
 
-    /// @notice Indicates that the address supposed to receive the ether is the zero address
+    /// @notice The recipient of a transfer is the zero address.
     error OracleMiddlewareTransferToZeroAddress();
 
     /**
-     * @notice Indicates that the recent price delay is outside of the limits
-     * @param newDelay The delay that was provided
+     * @notice The recent price delay is outside of the limits.
+     * @param newDelay The delay that was provided.
      */
     error OracleMiddlewareInvalidRecentPriceDelay(uint64 newDelay);
 
-    /// @dev Indicates that the new penaltyBps value is invalid
+    /// @dev The new penalty is invalid.
     error OracleMiddlewareInvalidPenaltyBps();
 
-    /// @notice Indicates that the chainlink roundId provided is invalid
+    /// @notice The provided Chainlink round ID is invalid.
     error OracleMiddlewareInvalidRoundId();
 
-    /// @notice The new low latency delay is invalid
+    /// @notice The new low latency delay is invalid.
     error OracleMiddlewareInvalidLowLatencyDelay();
 }
