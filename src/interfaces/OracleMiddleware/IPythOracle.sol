45470338d9b2405f18ad89025e051eb8dcf3ee15
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2024-12-23 14:43:58 +0100

    docs(oracle-middleware): improve the NatSpecs of the PythOracle contract and its interface (#800)
    * docs(oracle-middleware): improve the NatSpecs of the PythOracle contract and its interface
    
    * docs(oracle-middleware): improve the NatSpec of the returned value of _getPythPrice

--- src/interfaces/OracleMiddleware/IPythOracle.sol
+++ src/interfaces/OracleMiddleware/IPythOracle.sol
@@ -5,20 +5,20 @@ import { IPyth } from "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
 
 interface IPythOracle {
     /**
-     * @notice Get the Pyth contract address
-     * @return pyth_ The Pyth contract address
+     * @notice Gets the Pyth contract address.
+     * @return pyth_ The Pyth contract address.
      */
     function getPyth() external view returns (IPyth pyth_);
 
     /**
-     * @notice Get the Pyth price ID
-     * @return feedId_ The Pyth price feed ID
+     * @notice Gets the ID of the price feed queried by this contract.
+     * @return feedId_ The Pyth price feed ID.
      */
     function getPythFeedId() external view returns (bytes32 feedId_);
 
     /**
-     * @notice Get the recent price delay
-     * @return recentPriceDelay_ The maximum age of a recent price to be considered valid
+     * @notice Gets the recent price delay.
+     * @return recentPriceDelay_ The maximum age of a recent price to be considered valid.
      */
     function getPythRecentPriceDelay() external view returns (uint64 recentPriceDelay_);
 }
