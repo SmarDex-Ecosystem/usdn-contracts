2e25dfc06ddecdecb4af9576c16a8fdda6d4b9a1
Sam <46531012+samooyo@users.noreply.github.com>
2024-12-30 11:18:14 +0100

    docs(irebalancer): improve natspecs of IReabalancer errors, events and types (#806)
    * docs(irebalancer): improve natspecs of errors
    
    * docs: Irebalancer events natspecs
    
    * docs: Irebalancer types
    
    * docs: fixes typos
    
    * docs: fix comments on pr
    
    * docs: fix comments on PR

--- src/interfaces/Rebalancer/IRebalancerErrors.sol
+++ src/interfaces/Rebalancer/IRebalancerErrors.sol
@@ -1,61 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+/**
+ * @title Rebalancer Errors
+ * @notice Defines all custom errors thrown by the Rebalancer contract.
+ */
 interface IRebalancerErrors {
-    /// @dev Indicates that the user assets are not used in a position
+    /// @dev The user's assets are not used in a position.
     error RebalancerUserPending();
 
-    /// @dev Indicates that the user's assets were in a position version that is not liquidated
+    /// @dev The user's assets were in a position that has been liquidated.
     error RebalancerUserLiquidated();
 
-    /// @dev Indicates that the `to` address is invalid
+    /// @dev The `to` address is invalid.
     error RebalancerInvalidAddressTo();
 
-    /// @dev Indicates that the amount of assets is invalid
+    /// @dev The amount of assets is invalid.
     error RebalancerInvalidAmount();
 
-    /// @dev Indicates that the amount to deposit is insufficient
+    /// @dev The amount to deposit is insufficient.
     error RebalancerInsufficientAmount();
 
-    /// @dev Indicates that the provided maximum leverage is invalid
+    /// @dev The given maximum leverage is invalid.
     error RebalancerInvalidMaxLeverage();
 
-    /// @dev Indicates that the wanted minimum asset deposit is invalid
+    /// @dev The given minimum asset deposit is invalid.
     error RebalancerInvalidMinAssetDeposit();
 
-    /// @dev Indicates that the provided time limits are invalid
+    /// @dev The given time limits are invalid.
     error RebalancerInvalidTimeLimits();
 
-    /// @dev Indicates that the caller is not authorized to perform the action
+    /// @dev The caller is not authorized to perform the action.
     error RebalancerUnauthorized();
 
-    /// @dev Indicates that the user can't initiate or validate a deposit at the moment
+    /// @dev The user can't initiate or validate a deposit at this time.
     error RebalancerDepositUnauthorized();
 
-    /// @dev Indicates that the user still needs to validate their deposit or withdrawal
+    /// @dev The user must validate their deposit or withdrawal.
     error RebalancerActionNotValidated();
 
-    /// @dev Indicates that the user has no deposit or withdrawal that is pending validation
+    /// @dev The user has no pending deposit or withdrawal requiring validation.
     error RebalancerNoPendingAction();
 
-    /// @dev Indicates that the validation happened too early, user must wait `_timeLimits.validationDelay`
+    /// @dev Ton was attempted too early, the user must wait for `_timeLimits.validationDelay`.
     error RebalancerValidateTooEarly();
 
-    /// @dev Indicates that the validation happened too late, user must wait `_timeLimits.actionCooldown`
+    /// @dev Ton was attempted too late, the user must wait for `_timeLimits.actionCooldown`.
     error RebalancerActionCooldown();
 
-    /// @dev Indicates that the user can't initiate or validate a withdrawal at the moment
+    /// @dev The user can't initiate or validate a withdrawal at this time.
     error RebalancerWithdrawalUnauthorized();
 
-    /// @dev Indicates that the address could not accept the ether refund
+    /// @dev The address was unable to accept the Ether refund.
     error RebalancerEtherRefundFailed();
 
-    /// @dev Indicates that the signature provided for a delegation is invalid
+    /// @dev The signature provided for delegation is invalid.
     error RebalancerInvalidDelegationSignature();
 
     /**
-     * @dev Indicates that the user can't initiate a close position until the timestamp {closeLockedUntil} has passed
-     * @param closeLockedUntil The timestamp by which a user must wait to perform a {initiateClosePosition}
+     * @dev The user can't initiate a close position until the given timestamp has passed.
+     * @param closeLockedUntil The timestamp until which the user must wait to perform a close position action.
      */
     error RebalancerCloseLockedUntil(uint256 closeLockedUntil);
 }
--- src/interfaces/Rebalancer/IRebalancerTypes.sol
+++ src/interfaces/Rebalancer/IRebalancerTypes.sol
@@ -1,28 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+/**
+ * @title Rebalancer Types
+ * @notice Defines all custom types used by the Rebalancer contract.
+ */
 interface IRebalancerTypes {
     /**
-     * @notice The deposit data of a user
-     * @dev A value of zero for the `initiateTimestamp` indicates that the deposit or withdrawal was validated
-     * @param initiateTimestamp Timestamp when the deposit or withdrawal was initiated
-     * @param amount The amount of assets the user deposited
-     * @param entryPositionVersion The position version the user entered at
+     * @notice Represents the deposit data of a user.
+     * @dev A value of zero for `initiateTimestamp` indicates that the deposit or withdrawal has been validated.
+     * @param initiateTimestamp The timestamp when the deposit or withdrawal was initiated.
+     * @param amount The amount of assets deposited by the user.
+     * @param entryPositionVersion The version of the position the user entered.
      */
     struct UserDeposit {
         uint40 initiateTimestamp;
-        uint88 amount; // Max 309'485'009 tokens with 18 decimals
+        uint88 amount; // maximum 309'485'009 tokens with 18 decimals
         uint128 entryPositionVersion;
     }
 
     /**
-     * @notice The data for a version of the position
-     * @dev The difference between the amount here and the amount saved in the USDN protocol is the liquidation bonus
-     * @param amount The amount of assets used as collateral to open the position
-     * @param tick The tick of the position
-     * @param tickVersion The version of the tick
-     * @param index The index of the position in the tick list
-     * @param entryAccMultiplier The accumulated PnL multiplier of all the positions up to this one
+     * @notice Represents data for a specific version of a position.
+     * @dev The difference between `amount` here and the amount saved in the USDN protocol is the liquidation bonus.
+     * @param amount The amount of assets used as collateral to open the position.
+     * @param tick The tick of the position.
+     * @param tickVersion The version of the tick.
+     * @param index The index of the position in the tick list.
+     * @param entryAccMultiplier The accumulated PnL multiplier of all positions up to this one.
      */
     struct PositionData {
         uint128 amount;
@@ -33,19 +37,18 @@ interface IRebalancerTypes {
     }
 
     /**
-     * @notice The parameters related to the validation process of the Rebalancer deposit/withdraw
-     * @dev If the `validationDeadline` has passed, the user is blocked from interacting until the cooldown duration
-     * has elapsed (since the moment of the initiate action). After the cooldown, in case of a deposit action, the user
-     * must withdraw their funds with `resetDepositAssets`. After the cooldown, in case of a withdrawal action, the user
-     * can initiate a new withdrawal again
-     * @param validationDelay Minimum duration in seconds between an initiate action and the corresponding validate
-     * action
-     * @param validationDeadline Maximum duration in seconds between an initiate action and the corresponding validate
-     * action
-     * @param actionCooldown Duration from the initiate action during which the user can't interact with the Rebalancer
-     * if the `validationDeadline` elapsed.
-     * @param closeDelay Duration from the last rebalancer long position opening during which the user can't perform a
-     * {initiateClosePosition}
+     * @notice Defines parameters related to the validation process for rebalancer deposits and withdrawals.
+     * @dev If `validationDeadline` has passed, the user must wait until the cooldown duration has elapsed. Then, for
+     * deposit actions, the user must retrieve its funds using {resetDepositAssets}. For withdrawal actions, the user
+     * can simply initiate a new withdrawal.
+     * @param validationDelay The minimum duration in seconds between an initiate action and the corresponding validate
+     * action.
+     * @param validationDeadline The maximum duration in seconds between an initiate action and the corresponding
+     * validate action.
+     * @param actionCooldown The duration in seconds from the initiate action during which the user can't interact with
+     * the rebalancer if the `validationDeadline` is exceeded.
+     * @param closeDelay The Duration in seconds from the last rebalancer long position opening during which the user
+     * can't perform an {initiateClosePosition}.
      */
     struct TimeLimits {
         uint64 validationDelay;
