31f9502d4703346519af49e6ad703ecb91291042
kirienzo-eth <kirienzo.eth@gmail.com>
2025-01-08 05:38:29 +0100

    feat: improve the script so it fixes the broken links with format "contract.functionSignature"

--- script/utils/fixDocLinks.ts
+++ script/utils/fixDocLinks.ts
@@ -1,15 +1,12 @@
-import fs, { readFileSync } from 'node:fs';
+import fs, { readFileSync, writeFile, writeFileSync } from 'node:fs';
 
-const DOCS_BOOK_PATH = './docs/book/src/';
-const TAG_H3_HREF_REGEX = /href="([^"]+)"/;
-const TAG_H3_NAME_REGEX = /<a[^>]*>([^<]+)<\/a>/;
-const PARAMETERS_TYPE_REGEX = /<code>([^<]+)<\/code>/g;
+const DOCS_BOOK_PATH = './docs/book/src';
 
 const signaturesPerContract: Map<
   string,
   {
     path: string;
-    signatures: { name: string; href: string }[];
+    signatures: { [functionSignature: string]: { name: string; href: string } };
   }
 > = new Map();
 
@@ -35,8 +32,8 @@ function getRelevantDocFiles() {
 }
 
 function extractFunctionName(line: string) {
-  const functionName = line.match(TAG_H3_NAME_REGEX)?.[1];
-  const functionHref = line.match(TAG_H3_HREF_REGEX)?.[1];
+  const functionName = line.match(/<a[^>]*>([^<]+)<\/a>/)?.[1];
+  const functionHref = line.match(/href="([^"]+)"/)?.[1];
 
   if (!functionName || !functionHref) {
     throw `extractFunctionName: Unexpected format for line: ${line}`;
@@ -54,7 +51,8 @@ function extractFunctionParameters(fileContent: string[], startIndex: number) {
       return parameterTypes;
     }
 
-    const matches = [...line.matchAll(PARAMETERS_TYPE_REGEX)];
+    // match parameters inside code tags
+    const matches = [...line.matchAll(/<code>([^<]+)<\/code>/g)];
     if (matches?.[1]?.[1]) {
       parameterTypes.push(matches[1][1]);
     }
@@ -67,7 +65,7 @@ function buildFunctionSignature(name: string, parameterTypes: string[]) {
   return `${name}(${parameterTypes.join(',')})`;
 }
 
-/** Save the function/event/struct signatures in the `signaturesPerContract` map */
+/** Save the function/event/struct signatures of a contract in the `signaturesPerContract` map */
 function indexContractElements(docFiles: string[]) {
   for (let i = 0; i < docFiles.length; i++) {
     const docFile = docFiles[i];
@@ -77,16 +75,18 @@ function indexContractElements(docFiles: string[]) {
 
     signaturesPerContract.set(contractName, {
       path: `/src/${docFile}`,
-      signatures: [],
+      signatures: {},
     });
 
     for (let i = 0; i < content.length; i++) {
       const line = content[i];
+      // h3 tags are used only as headers for documenting a function/event/struct/etc.
       if (line.includes('<h3')) {
         const [functionName, functionHref] = extractFunctionName(line);
         const parameterTypes = extractFunctionParameters(content, i + 1);
 
         // the signature of a function that is found first is always the default choice if parameters are not specified
+        // this helps the script deal with parameters overloading
         if (signaturesPerContract.get(contractName)?.signatures[functionName] === undefined) {
           // biome-ignore lint/style/noNonNullAssertion: cannot be null because of initialization earlier
           const contractData = signaturesPerContract.get(contractName)!;
@@ -111,9 +111,52 @@ function indexContractElements(docFiles: string[]) {
 }
 
 /** Fix the broken links in the documentation */
-function fixDoc(docFiles: string[]) {}
+function fixDocFile(docFilePath: string) {
+  let content = readFileSync(`${DOCS_BOOK_PATH}/${docFilePath}`, { encoding: 'utf-8' });
+  const lines = content.split('\n');
+
+  // find where the body of the documentation begins
+  const startIndex = lines.findIndex((lineContent) => lineContent.includes('<main>'));
+
+  let isRewriteNecessary = false;
+  for (let i = startIndex; i < lines.length; i++) {
+    const line = lines[i];
+    // this signifies the end of the body of the documentation anything after that is irrelevant
+    if (line.includes('</main>')) {
+      break;
+    }
+
+    // find anything between brackets in the current line
+    const matches = [...line.matchAll(/\{([^}]*)\}/g)];
+    if (matches.length > 0) {
+      for (let j = 0; j < matches.length; j++) {
+        const match = matches[j];
+        const stringToReplace = match[0];
+        const functionSelector = match[1];
+        const [targetContractName, functionName] = functionSelector.split('.');
+
+        // find the target contract and function signature to replace the broken link
+        // if it cannot be found, ignore it
+        const targetSignature = signaturesPerContract.get(targetContractName)?.signatures[functionName];
+        if (targetSignature) {
+          isRewriteNecessary = true;
+          content = content.replaceAll(
+            stringToReplace,
+            `<a href="${targetSignature.href}">${targetSignature.name}</a>`,
+          );
+        } else {
+          console.warn(`Could not find signature for link ${stringToReplace} in ${docFilePath}`);
+        }
+      }
+    }
+  }
+
+  // rewrite the file if necessary
+  if (isRewriteNecessary) writeFileSync(`${DOCS_BOOK_PATH}/${docFilePath}`, content);
+}
 
 const docFiles = getRelevantDocFiles();
 indexContractElements(docFiles);
-
-console.log(signaturesPerContract);
+for (let i = 0; i < docFiles.length; i++) {
+  fixDocFile(docFiles[i]);
+}
