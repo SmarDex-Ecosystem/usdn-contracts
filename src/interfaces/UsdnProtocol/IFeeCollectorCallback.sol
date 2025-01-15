343507a8dbf312938ba3534a3c7489fb9a864cd1
beeb <703631+beeb@users.noreply.github.com>
2025-01-15 09:20:40 +0100

    revert: "docs: fee collector interface"
    This reverts commit f659bc9e6fdf2ac39f06bc4f232285d22de9590f.

--- src/interfaces/UsdnProtocol/IFeeCollectorCallback.sol
+++ src/interfaces/UsdnProtocol/IFeeCollectorCallback.sol
@@ -4,14 +4,14 @@ pragma solidity >=0.8.0;
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 /**
- * @notice This interface can be implemented by the fee collector contract to receive a callback from the USDN protocol.
- * @dev The contract must implement the ERC-165 interface detection mechanism.
+ * @notice This interface can be implemented by the fee collector contract to receive a callback from the USDN protocol
+ * @dev The contract must implement the ERC-165 interface detection mechanism
  */
 interface IFeeCollectorCallback is IERC165 {
     /**
      * @notice This function is called by the USDN protocol on the fee collector contract when the fee threshold is
-     * reached, after the fee is sent to the fee collector.
-     * @param feeAmount The fee amount that was sent to the fee collector.
+     * reached, after the fee is sent to the fee collector
+     * @param feeAmount The fee amount that was sent to the fee collector
      */
     function feeCollectorCallback(uint256 feeAmount) external;
 }
