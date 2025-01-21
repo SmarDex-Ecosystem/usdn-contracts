0bf868204adfb92962e8fb28a9beeeed6eb9da14
samooyo <samidarnaud@gmail.com>
2025-01-21 13:36:28 +0100

    feat: write hson

--- script/utils/gnosisTxBuilder.ts
+++ script/utils/gnosisTxBuilder.ts
@@ -1,20 +1,56 @@
-import type { Address, GetContractReturnType, PublicClient } from 'viem';
-import { keccak256, toHex, http, createPublicClient, getContract } from 'viem';
+import type { Address, GetContractReturnType, Hex, PublicClient } from 'viem';
+import {
+  keccak256,
+  toHex,
+  http,
+  createPublicClient,
+  getContract,
+  isAddress,
+  pad,
+  stringToBytes,
+  bytesToHex,
+} from 'viem';
 import { IUsdnProtocolAbi } from '../../dist/abi';
 import { IOracleMiddlewareAbi } from '../../dist/abi';
+import { Command } from 'commander';
+import { writeFileSync } from 'node:fs';
 
-const USDN_ADDRESS = '0x0000000000000000000000000000000000000000';
-const USDN_PROTOCOL_ADDRESS = '0x0000000000000000000000000000000000000000';
-const ORACLE_MIDDLEWARE_ADDRESS = '0x0000000000000000000000000000000000000000';
-const SAFE_ADDRESS = '0x1E3e1128F6bC2264a19D7a065982696d356879c5';
+const SAFE_ADDRESS: Address = '0x1E3e1128F6bC2264a19D7a065982696d356879c5';
 const MINTER_ROLE = keccak256(toHex('MINTER_ROLE'));
 const REBASER_ROLE = keccak256(toHex('REBASER_ROLE'));
 const DEFAULT_ADMIN_ROLE = keccak256('0x00');
 
+main();
+
 async function main() {
+  const program = new Command();
+  program
+    .description('Create a batch transaction for Gnosis Safe to initialize the USDN protocol')
+    .requiredOption('-r, --rpc-url <URL>', 'The RPC URL')
+    .requiredOption('-p, --protocol <Address>', 'The address of the USDN protocol contract')
+    .requiredOption('-l, --long-amount <amount>', 'The wanted long amount')
+    .parse(process.argv);
+
+  let USDN_PROTOCOL_ADDRESS: Address;
+  const options = program.opts();
+  if (isAddress(options.protocol)) {
+    USDN_PROTOCOL_ADDRESS = options.protocol;
+  } else {
+    console.error('Invalid address');
+    process.exit(1);
+  }
+
   const client = createPublicClient({
-    transport: http('rpcUrl'), // TODO : replace with actual rpc url
+    transport: http(options.rpcUrl),
   });
+
+  try {
+    await client.getBlockNumber();
+  } catch {
+    console.error('Invalid RPC URL');
+    process.exit(1);
+  }
+  const longAmount = BigInt(options.longAmount);
 
   const protocol = getContract({
     address: USDN_PROTOCOL_ADDRESS,
@@ -22,12 +58,23 @@ async function main() {
     client: client,
   });
   const oracleMiddleware = getContract({
-    address: ORACLE_MIDDLEWARE_ADDRESS,
+    address: await protocol.read.getOracleMiddleware(),
     abi: IOracleMiddlewareAbi,
     client: client,
   });
 
-  const { depositAmount, desiredLiqPrice } = await getDepositAmountAndLiqPrice(100n, protocol, oracleMiddleware);
+  const usdnAddress = await protocol.read.getUsdn();
+  const { depositAmount, desiredLiqPrice } = await getDepositAmountAndLiqPrice(longAmount, protocol, oracleMiddleware);
+  const initializationTx = createInitializationTx(
+    usdnAddress,
+    depositAmount.toString(),
+    longAmount.toString(),
+    desiredLiqPrice.toString(),
+  );
+  const batchTx = batch(usdnAddress, SAFE_ADDRESS, USDN_PROTOCOL_ADDRESS, initializationTx);
+
+  // write to file
+  writeFileSync('batch.json', JSON.stringify(batchTx, null, 2));
 }
 
 async function getDepositAmountAndLiqPrice(
@@ -37,7 +84,7 @@ async function getDepositAmountAndLiqPrice(
 ): Promise<{ depositAmount: bigint; desiredLiqPrice: bigint }> {
   const liquidationPenalty = await protocol.read.getLiquidationPenalty();
   const tickSpacing = await protocol.read.getTickSpacing();
-  const price = (await oracleMiddleware.simulate.parseAndValidatePrice(['0x', BigInt(Date.now()), 1, '0x'])).result
+  const price = (await oracleMiddleware.simulate.parseAndValidatePrice([pad('0x'), BigInt(Date.now()), 1, '0x'])).result
     .price;
 
   // we want a leverage of ~2x so we get the current price from the middleware and divide it by two
@@ -62,11 +109,17 @@ async function getDepositAmountAndLiqPrice(
   };
 }
 
-function batch(safe: Address, transactions: TransactionData[]): TxBatch {
+function batch(
+  usdnAddress: Address,
+  safe: Address,
+  usdnProtocolAddress: Address,
+  initializationTx: TransactionData,
+): TxBatch {
   const allTx: TransactionData[] = [];
-  allTx.push(grantUsdnRoleToProtocol(MINTER_ROLE));
-  allTx.push(grantUsdnRoleToProtocol(REBASER_ROLE));
-  allTx.push(renounceUsdnAdminRoleFromSafe);
+  allTx.push(grantUsdnRoleToProtocol(usdnAddress, MINTER_ROLE, usdnProtocolAddress));
+  allTx.push(grantUsdnRoleToProtocol(usdnAddress, REBASER_ROLE, usdnProtocolAddress));
+  allTx.push(renounceUsdnAdminRoleFromSafe(usdnAddress));
+  allTx.push(initializationTx);
 
   const tx: TxBatch = {
     version: '1.0', // TODO: check version
@@ -76,17 +129,22 @@ function batch(safe: Address, transactions: TransactionData[]): TxBatch {
       createdFromSafeAddress: safe,
       name: 'Batch',
     },
-    transactions: transactions,
+    transactions: allTx,
   };
 
   return tx;
 }
 
-function createInitializationTx(depositAmount: string, longAmount: string, desiredLiqPrice: string): TransactionData {
+function createInitializationTx(
+  usdnAddress: Address,
+  depositAmount: string,
+  longAmount: string,
+  desiredLiqPrice: string,
+): TransactionData {
   return {
-    to: USDN_ADDRESS,
+    to: usdnAddress,
     value: '0',
-    data: '0x',
+    data: '',
     contractMethod: {
       name: 'initialize',
       payable: true,
@@ -122,11 +180,11 @@ function createInitializationTx(depositAmount: string, longAmount: string, desir
   };
 }
 
-function grantUsdnRoleToProtocol(role: string): TransactionData {
+function grantUsdnRoleToProtocol(usdnAddress: Address, role: string, usdnProtocolAddress: Address): TransactionData {
   return {
-    to: USDN_ADDRESS,
+    to: usdnAddress,
     value: '0',
-    data: '0x',
+    data: '',
     contractMethod: {
       name: 'grantRole',
       payable: false,
@@ -145,36 +203,38 @@ function grantUsdnRoleToProtocol(role: string): TransactionData {
     },
     contractInputsValues: {
       role: role,
-      account: USDN_PROTOCOL_ADDRESS,
+      account: usdnProtocolAddress,
     },
   };
 }
 
-const renounceUsdnAdminRoleFromSafe: TransactionData = {
-  to: USDN_ADDRESS,
-  value: '0',
-  data: '0x',
-  contractMethod: {
-    name: 'renounceRole',
-    payable: false,
-    inputs: [
-      {
-        internalType: 'bytes32',
-        name: 'role',
-        type: 'bytes32',
-      },
-      {
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
-  },
-  contractInputsValues: {
-    role: DEFAULT_ADMIN_ROLE,
-    account: SAFE_ADDRESS,
-  },
-};
+function renounceUsdnAdminRoleFromSafe(usdnAddress: Address): TransactionData {
+  return {
+    to: usdnAddress,
+    value: '0',
+    data: '',
+    contractMethod: {
+      name: 'renounceRole',
+      payable: false,
+      inputs: [
+        {
+          internalType: 'bytes32',
+          name: 'role',
+          type: 'bytes32',
+        },
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+    },
+    contractInputsValues: {
+      role: DEFAULT_ADMIN_ROLE,
+      account: SAFE_ADDRESS,
+    },
+  };
+}
 
 /////////////////////////////
 
