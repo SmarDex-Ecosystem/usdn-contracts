705df7b08a0033fed0b432c7a771d9f27071c7d6
samooyo <samidarnaud@gmail.com>
2025-01-30 13:36:52 +0100

    fix: import in script

--- script/02_DeployWusdnAdaptor.s.sol
+++ script/02_DeployWusdnAdaptor.s.sol
@@ -5,18 +5,18 @@ import { Script } from "forge-std/Script.sol";
 
 import { Usdn } from "../src/Usdn/Usdn.sol";
 import { IWusdn } from "../src/interfaces/Usdn/IWusdn.sol";
-import { WusdnAdaptor } from "../src/utils/WusdnAdaptor.sol";
+import { WusdnBalancerAdaptor } from "../src/utils/WusdnBalancerAdaptor.sol";
 
 contract DeployWusdnAdaptor is Script {
     IWusdn constant WUSDN = IWusdn(0x99999999999999Cc837C997B882957daFdCb1Af9);
 
-    function run() external returns (WusdnAdaptor wusdnAdaptor) {
+    function run() external returns (WusdnBalancerAdaptor wusdnBalancerAdaptor_) {
         string memory etherscanApiKey = vm.envOr("ETHERSCAN_API_KEY", string("XXXXXXXXXXXXXXXXX"));
         vm.setEnv("ETHERSCAN_API_KEY", etherscanApiKey);
 
         vm.startBroadcast();
 
-        wusdnAdaptor = new WusdnAdaptor(WUSDN);
+        wusdnBalancerAdaptor_ = new WusdnBalancerAdaptor(WUSDN);
 
         vm.stopBroadcast();
     }
