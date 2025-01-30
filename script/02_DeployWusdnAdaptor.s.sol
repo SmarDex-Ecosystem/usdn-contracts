933326dc37fe454433e60fc0aa9e7b3a2c1dc186
Paulalex85 <tessier.paulalexandre@orange.fr>
2025-01-30 16:56:30 +0100

    feat: mainnet balancer adapter deployment

--- script/02_DeployWusdnAdaptor.s.sol
+++ script/02_DeployWusdnAdaptor.s.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.26;
 
 import { Script } from "forge-std/Script.sol";
 
-import { Usdn } from "../src/Usdn/Usdn.sol";
 import { IWusdn } from "../src/interfaces/Usdn/IWusdn.sol";
 import { WusdnBalancerAdaptor } from "../src/utils/WusdnBalancerAdaptor.sol";
 
@@ -11,9 +10,6 @@ contract DeployWusdnAdaptor is Script {
     IWusdn constant WUSDN = IWusdn(0x99999999999999Cc837C997B882957daFdCb1Af9);
 
     function run() external returns (WusdnBalancerAdaptor wusdnBalancerAdaptor_) {
-        string memory etherscanApiKey = vm.envOr("ETHERSCAN_API_KEY", string("XXXXXXXXXXXXXXXXX"));
-        vm.setEnv("ETHERSCAN_API_KEY", etherscanApiKey);
-
         vm.startBroadcast();
 
         wusdnBalancerAdaptor_ = new WusdnBalancerAdaptor(WUSDN);
