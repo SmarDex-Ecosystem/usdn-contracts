4e0ed97b3a32b7254f60bb48ec28ba0c783be5e3
lilyanb <lilyan.bastien@gmail.com>
2025-02-04 14:11:33 +0100

    feat: use separate file

--- src/utils/SetRebaseHandlerManager.sol
+++ src/utils/SetRebaseHandlerManager.sol
@@ -6,25 +6,7 @@ import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 import { Usdn } from "../Usdn/Usdn.sol";
 import { IRebaseCallback } from "../interfaces/Usdn/IRebaseCallback.sol";
-
-interface ISetRebaseHandlerManager {
-    /**
-     * @notice Gets the USDN token.
-     * @return usdn_ The USDN token.
-     */
-    function USDN() external view returns (Usdn usdn_);
-
-    /**
-     * @notice Sets the rebase handler for the USDN token.
-     * @param newHandler The address of the new rebase handler.
-     */
-    function setRebaseHandler(IRebaseCallback newHandler) external;
-
-    /**
-     * @notice Revokes the DEFAULT_ADMIN_ROLE on the USDN token.
-     */
-    function renounceUsdnOwnership() external;
-}
+import { ISetRebaseHandlerManager } from "../interfaces/Utils/ISetRebaseHandlerManager.sol";
 
 /**
  * @title SetRebaseHandlerManager.
