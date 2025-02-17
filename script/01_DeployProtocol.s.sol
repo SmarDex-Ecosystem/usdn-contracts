bfb3443fc2a8189580f149329082fd136f75e922
blablalf <a.gaillard@ra2.tech>
2025-02-17 14:45:36 +0100

    fix: setVerifySignature to false

--- script/01_DeployProtocol.s.sol
+++ script/01_DeployProtocol.s.sol
@@ -180,6 +180,7 @@ contract DeployProtocol is Script {
                 wstEthOracleMiddleware_ = new MockWstEthOracleMiddleware(
                     pythAddress, pythFeedId, chainlinkPriceAddress, wstETHAddress, chainlinkPriceValidity
                 );
+                MockWstEthOracleMiddleware(address(wstEthOracleMiddleware_)).setVerifySignature(false);
                 uint256 initialWSTETHMockedPrice = vm.envOr("INITIAL_WSTETH_MOCKED_PRICE", uint256(0));
                 if (initialWSTETHMockedPrice > 0) {
                     MockWstEthOracleMiddleware(address(wstEthOracleMiddleware_)).setWstethMockedPrice(
