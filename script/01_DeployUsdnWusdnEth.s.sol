5bf95845f134cc75b0d2822a4b63294f812d8c91
kirienzo-eth <kirienzo.eth@gmail.com>
2025-03-13 16:20:46 +0100

    feat: first shot of the deployment script

---
+++ script/01_DeployUsdnWusdnEth.s.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { Script } from "forge-std/Script.sol";
+
+import { HugeUint } from "@smardex-solidity-libraries-1/HugeUint.sol";
+import { Options, Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
+
+import { UsdnWusdnEthConfig } from "./deploymentConfigs/UsdnWusdnEthConfig.sol";
+import { Utils } from "./utils/Utils.s.sol";
+
+import { LiquidationRewardsManager } from "../src/LiquidationRewardsManager/LiquidationRewardsManager.sol";
+import { WusdnToEthOracleMiddleware } from "../src/OracleMiddleware/WusdnToEthOracleMiddleware.sol";
+import { Rebalancer } from "../src/Rebalancer/Rebalancer.sol";
+import { UsdnNoRebase } from "../src/Usdn/UsdnNoRebase.sol";
+import { UsdnProtocolFallback } from "../src/UsdnProtocol/UsdnProtocolFallback.sol";
+import { UsdnProtocolImpl } from "../src/UsdnProtocol/UsdnProtocolImpl.sol";
+import { UsdnProtocolConstantsLibrary as Constants } from
+    "../src/UsdnProtocol/libraries/UsdnProtocolConstantsLibrary.sol";
+import { IWusdn } from "../src/interfaces/Usdn/IWusdn.sol";
+import { IUsdnProtocol } from "../src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
+import { IUsdnProtocolTypes as Types } from "../src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
+
+contract DeployUsdnWusdnEth is UsdnWusdnEthConfig, Script {
+    IWusdn immutable WUSDN;
+    Utils utils;
+
+    constructor() {
+        WUSDN = IWusdn(address(UNDERLYING_ASSET));
+        utils = new Utils();
+    }
+
+    /**
+     * @notice Deploy the USDN ecosystem with Wusdn as the underlying token.
+     * @return WusdnToEthOracleMiddleware_ The oracle middleware to get the price of the Wusdn in ETH.
+     * @return liquidationRewardsManager_ The liquidation rewards manager.
+     * @return rebalancer_ The rebalancer.
+     * @return usdnNoRebase_ The USDN token contract.
+     * @return usdnProtocol_ The USDN protocol contract.
+     */
+    function run()
+        external
+        returns (
+            WusdnToEthOracleMiddleware wusdnToEthOracleMiddleware_,
+            LiquidationRewardsManager liquidationRewardsManager_,
+            Rebalancer rebalancer_,
+            UsdnNoRebase usdnNoRebase_,
+            IUsdnProtocol usdnProtocol_
+        )
+    {
+        utils.validateProtocol("UsdnProtocolImpl", "UsdnProtocolFallback");
+
+        _setFeeCollector(msg.sender);
+
+        (wusdnToEthOracleMiddleware_, liquidationRewardsManager_, usdnNoRebase_) = _deployAndSetPeripheralContracts();
+
+        usdnProtocol_ = _deployProtocol(initStorage);
+
+        rebalancer_ = _setRebalancerAndHandleUsdnRoles(usdnProtocol_, usdnNoRebase_);
+
+        _initializeProtocol(usdnProtocol_, wusdnToEthOracleMiddleware_);
+
+        utils.validateProtocolConfig(usdnProtocol_, msg.sender);
+    }
+
+    /**
+     * @notice Deploy the oracle middleware, liquidation rewards manager and UsdnNoRebase contracts. Add then to the
+     * initialization struct.
+     * @dev As the USDN token doesn't rebase, there's no need to deploy the WUSDN contract, as wrapping is only useful
+     * to avoid messing with the token balances in smart contracts.
+     * @return wusdnToEthOracleMiddleware_ The oracle middleware that gets the price of the Wusdn in Eth.
+     * @return liquidationRewardsManager_ The liquidation rewards manager.
+     * @return usdnNoRebase_ The USDN contract.
+     */
+    function _deployAndSetPeripheralContracts()
+        internal
+        returns (
+            WusdnToEthOracleMiddleware wusdnToEthOracleMiddleware_,
+            LiquidationRewardsManager liquidationRewardsManager_,
+            UsdnNoRebase usdnNoRebase_
+        )
+    {
+        vm.startBroadcast();
+        // TODO needs the new LiquidationRewardsManager
+        liquidationRewardsManager_ = new LiquidationRewardsManager(WUSDN);
+        wusdnToEthOracleMiddleware_ = new WusdnToEthOracleMiddleware(
+            PYTH_ADDRESS, PYTH_ETH_FEED_ID, CHAINLINK_ETH_PRICE, address(WUSDN.USDN()), CHAINLINK_PRICE_VALIDITY
+        );
+        // TODO decide of a name/symbol
+        usdnNoRebase_ = new UsdnNoRebase("", "");
+        vm.stopBroadcast();
+
+        _setPeripheralContracts(wusdnToEthOracleMiddleware_, liquidationRewardsManager_, usdnNoRebase_);
+    }
+
+    /**
+     * @notice Deploy the USDN protocol.
+     * @param initStorage The initialization parameters struct.
+     * @return usdnProtocol_ The USDN protocol proxy.
+     */
+    function _deployProtocol(Types.InitStorage storage initStorage) internal returns (IUsdnProtocol usdnProtocol_) {
+        // we need to allow external library linking and immutable variables in the openzeppelin module
+        Options memory opts;
+        opts.unsafeAllow = "external-library-linking,state-variable-immutable";
+
+        vm.startBroadcast();
+
+        UsdnProtocolFallback protocolFallback = new UsdnProtocolFallback();
+        _setProtocolFallback(protocolFallback);
+
+        address proxy = Upgrades.deployUUPSProxy(
+            "UsdnProtocolImpl.sol", abi.encodeCall(UsdnProtocolImpl.initializeStorage, (initStorage)), opts
+        );
+
+        vm.stopBroadcast();
+
+        usdnProtocol_ = IUsdnProtocol(proxy);
+    }
+
+    /**
+     * @notice Set the rebalancer and give the minting and rebasing roles to the USDN protocol.
+     * @param usdnProtocol The USDN protocol.
+     * @return rebalancer_ The rebalancer.
+     */
+    function _setRebalancerAndHandleUsdnRoles(IUsdnProtocol usdnProtocol, UsdnNoRebase usdn)
+        internal
+        returns (Rebalancer rebalancer_)
+    {
+        vm.startBroadcast();
+
+        rebalancer_ = new Rebalancer(usdnProtocol);
+        usdnProtocol.grantRole(Constants.ADMIN_SET_EXTERNAL_ROLE, msg.sender);
+        usdnProtocol.grantRole(Constants.SET_EXTERNAL_ROLE, msg.sender);
+        usdnProtocol.setRebalancer(rebalancer_);
+
+        usdn.grantRole(usdn.MINTER_ROLE(), address(usdnProtocol));
+        usdn.grantRole(usdn.REBASER_ROLE(), address(usdnProtocol));
+
+        vm.stopBroadcast();
+    }
+
+    /**
+     * @notice Initialize the USDN protocol with a ~2x leverage long position.
+     * @param usdnProtocol The USDN protocol.
+     * @param wusdnToEthOracleMiddleware The WstETH oracle middleware.
+     */
+    function _initializeProtocol(IUsdnProtocol usdnProtocol, WusdnToEthOracleMiddleware wusdnToEthOracleMiddleware)
+        internal
+    {
+        uint24 liquidationPenalty = usdnProtocol.getLiquidationPenalty();
+        int24 tickSpacing = usdnProtocol.getTickSpacing();
+        uint256 price = wusdnToEthOracleMiddleware.parseAndValidatePrice(
+            "", uint128(block.timestamp), Types.ProtocolAction.Initialize, ""
+        ).price;
+
+        // we want a leverage of ~2x so we get the current price from the middleware and divide it by two
+        uint128 desiredLiqPrice = uint128(price / 2);
+        // get the liquidation price with the tick rounding
+        uint128 liqPriceWithoutPenalty = usdnProtocol.getLiqPriceFromDesiredLiqPrice(
+            desiredLiqPrice, price, 0, HugeUint.wrap(0), tickSpacing, liquidationPenalty
+        );
+        // get the total exposure of the wanted long position
+        uint256 positionTotalExpo =
+            FixedPointMathLib.fullMulDiv(INITIAL_LONG_AMOUNT, price, price - liqPriceWithoutPenalty);
+        // get the amount to deposit to reach a balanced state
+        uint256 depositAmount = positionTotalExpo - INITIAL_LONG_AMOUNT;
+
+        vm.startBroadcast();
+        require(result, "Failed to mint wstETH");
+
+        WUSDN.approve(address(usdnProtocol), depositAmount + INITIAL_LONG_AMOUNT);
+        usdnProtocol.initialize(uint128(depositAmount), uint128(INITIAL_LONG_AMOUNT), desiredLiqPrice, "");
+        vm.stopBroadcast();
+    }
+}
---
+++ script/deploymentConfigs/UsdnWusdnEthConfig.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { DeploymentConfig } from "./DeploymentConfig.sol";
+
+import { UsdnProtocolConstantsLibrary as Constants } from
+    "../../src/UsdnProtocol/libraries/UsdnProtocolConstantsLibrary.sol";
+
+import { ILiquidationRewardsManager } from
+    "../../src/interfaces/LiquidationRewardsManager/ILiquidationRewardsManager.sol";
+import { IOracleMiddleware } from "../../src/interfaces/OracleMiddleware/IOracleMiddleware.sol";
+import { IUsdn } from "../../src/interfaces/Usdn/IUsdn.sol";
+import { IWusdn } from "../../src/interfaces/Usdn/IWusdn.sol";
+import { IUsdnProtocolFallback } from "../../src/interfaces/UsdnProtocol/IUsdnProtocolFallback.sol";
+import { Sdex } from "../../test/utils/Sdex.sol";
+
+/// @notice Configuration contract for the USDN protocol backed with WUSDN deployment.
+contract UsdnWusdnEthConfig is DeploymentConfig {
+    address constant CHAINLINK_ETH_PRICE = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address constant PYTH_ADDRESS = 0x4305FB66699C3B2702D4d05CF36551390A4c69C6;
+    bytes32 constant PYTH_ETH_FEED_ID = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
+    uint256 constant CHAINLINK_PRICE_VALIDITY = 1 hours + 2 minutes;
+
+    constructor() {
+        // TODO decide of an initial amount
+        INITIAL_LONG_AMOUNT = 200 ether;
+        SDEX = Sdex(0x5DE8ab7E27f6E7A1fFf3E5B337584Aa43961BEeF);
+        UNDERLYING_ASSET = IWusdn(0x99999999999999Cc837C997B882957daFdCb1Af9);
+
+        initStorage.minLeverage = 10 ** Constants.LEVERAGE_DECIMALS + 10 ** (Constants.LEVERAGE_DECIMALS - 1); // x1.1
+        initStorage.maxLeverage = 25 * 10 ** Constants.LEVERAGE_DECIMALS; // x10
+        initStorage.lowLatencyValidatorDeadline = 15 minutes;
+        initStorage.onChainValidatorDeadline = 65 minutes; // slightly more than chainlink's heartbeat
+        initStorage.safetyMarginBps = 200; // 2%
+        initStorage.liquidationIteration = 1;
+        initStorage.protocolFeeBps = 800; // 8%
+        initStorage.rebalancerBonusBps = 8750; // 87.5%
+        initStorage.liquidationPenalty = 200; // 200 ticks -> ~2.02%
+        initStorage.emaPeriod = 16 hours;
+        initStorage.fundingSF = 75 * 10 ** (Constants.FUNDING_SF_DECIMALS - 2); // 0.75
+        initStorage.feeThreshold = 1 ether;
+        initStorage.openExpoImbalanceLimitBps = 400; // 4%
+        initStorage.withdrawalExpoImbalanceLimitBps = 600; // 6%
+        initStorage.depositExpoImbalanceLimitBps = 400; // 4%
+        initStorage.closeExpoImbalanceLimitBps = 600; // 6%
+        initStorage.rebalancerCloseExpoImbalanceLimitBps = 250; // 2.5%
+        initStorage.longImbalanceTargetBps = 300; // 3%
+        initStorage.positionFeeBps = 1; // 0.01%
+        initStorage.vaultFeeBps = 4; // 0.04%
+        initStorage.sdexRewardsRatioBps = 100; // 1%
+        initStorage.sdexBurnOnDepositRatio = 5e6; // 5%
+        initStorage.securityDepositValue = 0.15 ether;
+        initStorage.EMA = int256(3 * 10 ** (Constants.FUNDING_RATE_DECIMALS - 4)); // 0.0003
+        initStorage.tickSpacing = 100;
+        initStorage.sdex = SDEX;
+        initStorage.asset = UNDERLYING_ASSET;
+        // 1400 tokens (to roughly match the current minLongPosition of the wstETH/USD version)
+        initStorage.minLongPosition = 1400 * 10 ** UNDERLYING_ASSET.decimals();
+    }
+
+    /// @inheritdoc DeploymentConfig
+    function _setPeripheralContracts(
+        IOracleMiddleware oracleMiddleware,
+        ILiquidationRewardsManager liquidationRewardsManager,
+        IUsdn usdnNoRebase
+    ) internal override {
+        initStorage.oracleMiddleware = oracleMiddleware;
+        uint8 priceFeedDecimals = oracleMiddleware.getDecimals();
+        initStorage.liquidationRewardsManager = liquidationRewardsManager;
+        initStorage.usdn = usdnNoRebase;
+    }
+
+    /// @inheritdoc DeploymentConfig
+    function _setFeeCollector(address feeCollector) internal override {
+        initStorage.feeCollector = feeCollector;
+    }
+
+    /// @inheritdoc DeploymentConfig
+    function _setProtocolFallback(IUsdnProtocolFallback protocolFallback) internal override {
+        initStorage.protocolFallbackAddr = address(protocolFallback);
+    }
+}
