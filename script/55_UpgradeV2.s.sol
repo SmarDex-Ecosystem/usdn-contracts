eddab995ef1b03834efb3723e4791f914f4c8d43
beeb <703631+beeb@users.noreply.github.com>
2025-03-28 08:10:16 +0100

    chore: update upgrade script

--- script/55_UpgradeV2.s.sol
+++ script/55_UpgradeV2.s.sol
@@ -11,50 +11,29 @@ import { IUsdnProtocol } from "../src/interfaces/UsdnProtocol/IUsdnProtocol.sol"
 /**
  * @title Upgrade script
  * @notice This script is only made for upgrading the Usdn protocol from v1.0.x to v2.0.0.
- * @dev The deployer must already have the `PROXY_UPGRADE_ROLE` before launching this script.
+ * @dev The sender must already have the `PROXY_UPGRADE_ROLE` before launching this script.
  */
 contract Upgrade is Script {
-    address _deployerAddress;
-    IUsdnProtocol _usdnProtocol;
+    IUsdnProtocol constant USDN_PROTOCOL = IUsdnProtocol(0x656cB8C6d154Aad29d8771384089be5B5141f01a);
     bytes32 ImplAddrLocation = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     function run() external returns (UsdnProtocolImpl newUsdnProtocolImpl_) {
-        _handleEnvVariables();
-
         require(
-            _usdnProtocol.hasRole(Constants.PROXY_UPGRADE_ROLE, _deployerAddress),
-            "DEPLOYER_ADDRESS does not have the permission to upgrade the protocol"
+            USDN_PROTOCOL.hasRole(Constants.PROXY_UPGRADE_ROLE, msg.sender),
+            "Sender does not have the permission to upgrade the protocol"
         );
-        bytes32 oldImpl = vm.load(address(_usdnProtocol), ImplAddrLocation);
+        bytes32 oldImpl = vm.load(address(USDN_PROTOCOL), ImplAddrLocation);
 
-        vm.startBroadcast(_deployerAddress);
+        vm.startBroadcast();
 
         newUsdnProtocolImpl_ = new UsdnProtocolImpl();
-        _usdnProtocol.upgradeToAndCall(
+        USDN_PROTOCOL.upgradeToAndCall(
             address(newUsdnProtocolImpl_), abi.encodeWithSelector(UsdnProtocolImpl.initializeStorageV2.selector)
         );
-        bytes32 newImpl = vm.load(address(_usdnProtocol), ImplAddrLocation);
+        bytes32 newImpl = vm.load(address(USDN_PROTOCOL), ImplAddrLocation);
         require(oldImpl != newImpl, "Upgrade failed");
         require(address(uint160(uint256(newImpl))) == address(newUsdnProtocolImpl_), "Upgrade failed");
 
         vm.stopBroadcast();
-    }
-
-    /// @notice Handle the environment variables
-    function _handleEnvVariables() internal {
-        try vm.envAddress("DEPLOYER_ADDRESS") {
-            _deployerAddress = vm.envAddress("DEPLOYER_ADDRESS");
-        } catch {
-            revert("DEPLOYER_ADDRESS is required");
-        }
-
-        try vm.envAddress("USDN_PROTOCOL") {
-            _usdnProtocol = IUsdnProtocol(vm.envAddress("USDN_PROTOCOL"));
-        } catch {
-            revert("USDN_PROTOCOL is required");
-        }
-
-        string memory etherscanApiKey = vm.envOr("ETHERSCAN_API_KEY", string("XXXXXXXXXXXXXXXXX"));
-        vm.setEnv("ETHERSCAN_API_KEY", etherscanApiKey);
     }
 }
