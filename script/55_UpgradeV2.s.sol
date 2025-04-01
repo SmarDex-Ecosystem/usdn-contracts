b211f5cf4fd10a79108e329a8ccc27326e812743
beeb <703631+beeb@users.noreply.github.com>
2025-04-01 08:49:22 +0200

    docs: add how to calculate the storage slot

--- script/55_UpgradeV2.s.sol
+++ script/55_UpgradeV2.s.sol
@@ -19,6 +19,8 @@ import { IUsdnProtocol } from "../src/interfaces/UsdnProtocol/IUsdnProtocol.sol"
  */
 contract UpgradeV2 is UsdnWstethUsdConfig, Script {
     IUsdnProtocol constant USDN_PROTOCOL = IUsdnProtocol(0x656cB8C6d154Aad29d8771384089be5B5141f01a);
+
+    /// @dev this is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1.
     bytes32 constant IMPL_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     Utils utils;
