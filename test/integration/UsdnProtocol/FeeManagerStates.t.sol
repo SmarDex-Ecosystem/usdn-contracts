3987d7b1c3f8950345d597cf37b4cc1194dcd65f
fireboss777 <fireboss@tutanota.com>
2025-04-01 17:44:19 +0200

    test: fee manager tests natspecs

--- test/integration/UsdnProtocol/FeeManagerStates.t.sol
+++ test/integration/UsdnProtocol/FeeManagerStates.t.sol
@@ -7,7 +7,7 @@ import { IVerifierFeeManager } from "../Middlewares/utils/MockStreamVerifierProx
 
 /**
  * @custom:feature The Chainlink data streams fee manager for different states.
- * @custom:background The protocol
+ * @custom:background A deployed USDN protocol, an oracle middleware and a data streams proxy verifier.
  */
 contract TestFeeManagerStates is UsdnProtocolBaseIntegrationFixture {
     uint256 securityDeposit;
@@ -19,6 +19,12 @@ contract TestFeeManagerStates is UsdnProtocolBaseIntegrationFixture {
         securityDeposit = protocol.getSecurityDepositValue();
     }
 
+    /**
+     * @custom:scenario Tests the `initiateOpenPosition` function without oracle discount.
+     * @custom:given A linked fee manager without discount.
+     * @custom:when The function is called.
+     * @custom:then The transaction must be successful.
+     */
     function test_verifierWithFeeManagerNoDiscount() external {
         uint256 validationCost = oracleMiddleware.validationCost(payload, ProtocolAction.InitiateOpenPosition);
         assertGt(validationCost, 0, "The data streams validation cost must be positive");
@@ -26,6 +32,12 @@ contract TestFeeManagerStates is UsdnProtocolBaseIntegrationFixture {
         _initiatePosition(validationCost + securityDeposit);
     }
 
+    /**
+     * @custom:scenario Tests the `initiateOpenPosition` function with oracle full discount.
+     * @custom:given A linked fee manager with a full discount set for the middleware.
+     * @custom:when The function is called.
+     * @custom:then The transaction must be successful.
+     */
     function test_verifierWithFeeManagerAndFullDiscount() external {
         address nativeToken = mockFeeManager.i_nativeAddress();
         mockFeeManager.updateSubscriberDiscount(address(oracleMiddleware), report.feedId, nativeToken, 1e18);
@@ -33,12 +45,21 @@ contract TestFeeManagerStates is UsdnProtocolBaseIntegrationFixture {
         _initiatePosition(securityDeposit);
     }
 
+    /**
+     * @custom:scenario Tests the `initiateOpenPosition` function without fee manager.
+     * @custom:when The function is called.
+     * @custom:then The transaction must be successful.
+     */
     function test_verifierWithoutFeeManager() external {
         mockStreamVerifierProxy.setFeeManager(IVerifierFeeManager(address(0)));
 
         _initiatePosition(securityDeposit);
     }
 
+    /**
+     * @dev Initiates an open position though the USDN protocol with a data streams oracle middleware.
+     * @param initiateValue The ETH oracle fee value required to initiate an open position.
+     */
     function _initiatePosition(uint256 initiateValue) internal {
         (bool success,) = protocol.initiateOpenPosition{ value: initiateValue }(
             2 ether,
@@ -55,6 +76,7 @@ contract TestFeeManagerStates is UsdnProtocolBaseIntegrationFixture {
         assertTrue(success, "The open position must be initiated");
     }
 
+    /// @dev We doesn't accept any refund.
     receive() external payable {
         revert("Refund is not allowed");
     }
