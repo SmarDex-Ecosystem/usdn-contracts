fc87703652d8a17c7b25c548884d047ca87c5408
Fireboss <60253423+fireboss777@users.noreply.github.com>
2025-04-01 13:51:30 +0200

    feat(middleware): wsteth oracle with data streams (#898)
    * feat: wsteth oracle with data streams
    
    * docs: wstETH
    
    * docs: ETH

--- src/OracleMiddleware/WstEthOracleMiddlewareWithDataStreams.sol
+++ src/OracleMiddleware/WstEthOracleMiddlewareWithDataStreams.sol
@@ -6,38 +6,52 @@ import { IBaseOracleMiddleware } from "../interfaces/OracleMiddleware/IBaseOracl
 import { PriceInfo } from "../interfaces/OracleMiddleware/IOracleMiddlewareTypes.sol";
 import { IUsdnProtocolTypes as Types } from "../interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
 import { CommonOracleMiddleware } from "./CommonOracleMiddleware.sol";
-import { OracleMiddlewareWithPyth } from "./OracleMiddlewareWithPyth.sol";
+import { OracleMiddlewareWithDataStreams } from "./OracleMiddlewareWithDataStreams.sol";
 
 /**
- * @title Middleware Implementation For WstETH Price
- * @notice This contract is used to get the price of wstETH from the ETH price oracle.
+ * @title Middleware Implementation For WstETH Price With Chainlink Data Streams
+ * @notice This contract is used to get the price of wstETH from the ETH price or the wstETH price directly.
  */
-contract WstEthOracleMiddleware is OracleMiddlewareWithPyth {
+contract WstEthOracleMiddlewareWithDataStreams is OracleMiddlewareWithDataStreams {
     /// @notice The wstETH contract.
-    IWstETH internal immutable _wstEth;
+    IWstETH internal immutable _wstETH;
 
     /**
      * @param pythContract The address of the Pyth contract.
-     * @param pythPriceID The ID of the ETH Pyth price feed.
-     * @param chainlinkPriceFeed The address of the ETH Chainlink price feed.
+     * @param pythFeedId The ETH/USD Pyth price feed ID for the asset.
+     * @param chainlinkPriceFeed The address of the Chainlink ETH/USD price feed.
      * @param wstETH The address of the wstETH contract.
      * @param chainlinkTimeElapsedLimit The duration after which a Chainlink price is considered stale.
+     * @param chainlinkProxyVerifierAddress The address of the Chainlink proxy verifier contract.
+     * @param chainlinkStreamId The supported Chainlink wstETH/USD data stream ID.
      */
     constructor(
         address pythContract,
-        bytes32 pythPriceID,
+        bytes32 pythFeedId,
         address chainlinkPriceFeed,
         address wstETH,
-        uint256 chainlinkTimeElapsedLimit
-    ) OracleMiddlewareWithPyth(pythContract, pythPriceID, chainlinkPriceFeed, chainlinkTimeElapsedLimit) {
-        _wstEth = IWstETH(wstETH);
+        uint256 chainlinkTimeElapsedLimit,
+        address chainlinkProxyVerifierAddress,
+        bytes32 chainlinkStreamId
+    )
+        OracleMiddlewareWithDataStreams(
+            pythContract,
+            pythFeedId,
+            chainlinkPriceFeed,
+            chainlinkTimeElapsedLimit,
+            chainlinkProxyVerifierAddress,
+            chainlinkStreamId
+        )
+    {
+        _wstETH = IWstETH(wstETH);
     }
 
     /**
      * @inheritdoc IBaseOracleMiddleware
-     * @notice Parses and validates `data`, returns the corresponding price data by applying ETH/wstETH ratio.
+     * @notice Parses and validates `data`, returns the corresponding price data,
+     * applying ETH/wstETH ratio if the price is in ETH.
      * @dev The data format is specific to the middleware and is simply forwarded from the user transaction's calldata.
-     * Wsteth price is calculated as follows: `ethPrice x stEthPerToken / 1 ether`.
+     * If needed, the wstETH price is calculated as follows: `ethPrice x stEthPerToken / 1 ether`.
      * A fee amounting to exactly {validationCost} (with the same `data` and `action`) must be sent or the transaction
      * will revert.
      * @param actionId A unique identifier for the current action. This identifier can be used to link an `Initiate`
@@ -56,13 +70,19 @@ contract WstEthOracleMiddleware is OracleMiddlewareWithPyth {
         Types.ProtocolAction action,
         bytes calldata data
     ) public payable virtual override(IBaseOracleMiddleware, CommonOracleMiddleware) returns (PriceInfo memory) {
-        PriceInfo memory ethPrice = super.parseAndValidatePrice(actionId, targetTimestamp, action, data);
-        uint256 stEthPerToken = _wstEth.stEthPerToken();
+        PriceInfo memory oraclePrice = super.parseAndValidatePrice(actionId, targetTimestamp, action, data);
+
+        if (data.length > 32 && !_isPythData(data)) {
+            // the price is already in wstETH/USD from the Chainlink data stream
+            return oraclePrice;
+        }
+
+        uint256 stEthPerToken = _wstETH.stEthPerToken();
 
         return PriceInfo({
-            price: ethPrice.price * stEthPerToken / 1 ether,
-            neutralPrice: ethPrice.neutralPrice * stEthPerToken / 1 ether,
-            timestamp: ethPrice.timestamp
+            price: oraclePrice.price * stEthPerToken / 1 ether,
+            neutralPrice: oraclePrice.neutralPrice * stEthPerToken / 1 ether,
+            timestamp: oraclePrice.timestamp
         });
     }
 }
