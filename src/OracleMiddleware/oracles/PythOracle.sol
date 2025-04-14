e431623c81cc2de17b89e3a661d71f4ed220817b
Sam <46531012+samooyo@users.noreply.github.com>
2025-04-14 14:26:14 +0200

    refactor: relax abstract contracts version (#908)

--- src/OracleMiddleware/oracles/PythOracle.sol
+++ src/OracleMiddleware/oracles/PythOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
 
 import { IPyth } from "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
 import { PythStructs } from "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
