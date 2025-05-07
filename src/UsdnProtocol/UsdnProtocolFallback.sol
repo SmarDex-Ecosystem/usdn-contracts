46965b0be5a31a9b391f85e19a748621980de4a9
Kirienzo <72258256+KirienzoEth@users.noreply.github.com>
2025-05-07 08:22:53 +0200

    refactor: re-organize the inheritance graph for `IUsdnProtocol` to include events and errors (#917)
    * refactor: re-organize the inheritance graph for `IUsdnProtocol` to include events and errors
    
    There was a linearization graph issue because of an inconsistency with the inheritance of
    `IUsdnProtocolErrors` and `IUsdnProtocolEvents` in the implementation and fallback interfaces. This
    new organization should allow `IUsdnProtocol` to contain both the errors and the events without
    breaking the linearization graph.
    
    * refactor(usdnprotocolimpl): remove unused imports
    
    * refactor: reorganize again to only implements the errors and events interfaces where needed

--- src/UsdnProtocol/UsdnProtocolFallback.sol
+++ src/UsdnProtocol/UsdnProtocolFallback.sol
@@ -27,9 +27,9 @@ import { UsdnProtocolUtilsLibrary as Utils } from "./libraries/UsdnProtocolUtils
 import { UsdnProtocolVaultLibrary as Vault } from "./libraries/UsdnProtocolVaultLibrary.sol";
 
 contract UsdnProtocolFallback is
+    IUsdnProtocolFallback,
     IUsdnProtocolErrors,
     IUsdnProtocolEvents,
-    IUsdnProtocolFallback,
     InitializableReentrancyGuard,
     PausableUpgradeable,
     AccessControlDefaultAdminRulesUpgradeable
