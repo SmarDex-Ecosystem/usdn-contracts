f5a3f4aba5752f27174a4506361cfb13b818cc45
samooyo <samidarnaud@gmail.com>
2025-05-13 09:56:21 +0200

    docs: ticker

--- src/OracleMiddleware/WusdnToEthOracleMiddlewareWithDataStreams.sol
+++ src/OracleMiddleware/WusdnToEthOracleMiddlewareWithDataStreams.sol
@@ -9,7 +9,7 @@ import { CommonOracleMiddleware, OracleMiddlewareWithDataStreams } from "./Oracl
 
 /**
  * @title Middleware Implementation For Short ETH Protocol
- * @notice This contract is used to get the "inverse" price in ETH/WUSDN denomination, so that it can be used for a
+ * @notice This contract is used to get the "inverse" price in number of ETH per WUSDN, so that it can be used for a
  * shorting version of the USDN protocol with WUSDN as the underlying asset.
  * @dev This version uses Pyth or Chainlink Data Streams for liquidations, and only Chainlink Data Streams for
  * validation actions.
