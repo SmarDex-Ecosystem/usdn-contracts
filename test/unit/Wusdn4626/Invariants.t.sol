84f10fa8bc7ca7899e8edae4063790d601f062b2
samooyo <samidarnaud@gmail.com>
2025-08-07 11:02:07 +0200

    refactor: less calls

--- test/unit/Wusdn4626/Invariants.t.sol
+++ test/unit/Wusdn4626/Invariants.t.sol
@@ -6,6 +6,11 @@ import { Wusdn4626Fixture } from "./utils/Fixtures.sol";
 contract TestWusdn4626Invariants is Wusdn4626Fixture {
     function setUp() public override {
         super.setUp();
+        address user1 = wusdn4626.USER_1();
+        address user2 = wusdn4626.USER_2();
+        address user3 = wusdn4626.USER_3();
+        address user4 = wusdn4626.USER_4();
+
         targetContract(address(wusdn4626));
 
         bytes4[] memory wusdn4626Selectors = new bytes4[](4);
@@ -15,23 +20,23 @@ contract TestWusdn4626Invariants is Wusdn4626Fixture {
         wusdn4626Selectors[3] = wusdn4626.redeemTest.selector;
         targetSelector(FuzzSelector({ addr: address(wusdn4626), selectors: wusdn4626Selectors }));
 
-        targetSender(wusdn4626.USER_1());
-        targetSender(wusdn4626.USER_2());
-        targetSender(wusdn4626.USER_3());
-        targetSender(wusdn4626.USER_4());
+        targetSender(user1);
+        targetSender(user2);
+        targetSender(user3);
+        targetSender(user4);
 
-        usdn.mint(wusdn4626.USER_1(), 1_000_000 ether);
-        usdn.mint(wusdn4626.USER_2(), 1_000_000 ether);
-        usdn.mint(wusdn4626.USER_3(), 1_000_000 ether);
-        usdn.mint(wusdn4626.USER_4(), 1_000_000 ether);
+        usdn.mint(user1, 1_000_000 ether);
+        usdn.mint(user2, 1_000_000 ether);
+        usdn.mint(user3, 1_000_000 ether);
+        usdn.mint(user4, 1_000_000 ether);
 
-        vm.prank(wusdn4626.USER_1());
+        vm.prank(user1);
         usdn.approve(address(wusdn4626), type(uint256).max);
-        vm.prank(wusdn4626.USER_2());
+        vm.prank(user2);
         usdn.approve(address(wusdn4626), type(uint256).max);
-        vm.prank(wusdn4626.USER_3());
+        vm.prank(user3);
         usdn.approve(address(wusdn4626), type(uint256).max);
-        vm.prank(wusdn4626.USER_4());
+        vm.prank(user4);
         usdn.approve(address(wusdn4626), type(uint256).max);
     }
 
