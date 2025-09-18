74a9ff2602ee58eb9e5a7f35e97d555b6de7dd5a
samooyo <samidarnaud@gmail.com>
2025-09-18 11:33:13 +0200

    test(USDNr): withdrawYield

--- test/unit/USDNr/WithdrawYield.t.sol
+++ test/unit/USDNr/WithdrawYield.t.sol
@@ -1,46 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
 import { UsdnrTokenFixture } from "./utils/Fixtures.sol";
 
 import { IUsdnr } from "../../../src/interfaces/Usdn/IUsdnr.sol";
 
 /// @custom:feature The `withdrawYield` function of the `USDNr` contract
 contract TestUsdnrWithdrawYield is UsdnrTokenFixture {
+    uint256 initialDeposit = 100 ether;
+
     function setUp() public override {
         super.setUp();
 
-        usdn.mint(address(this), 100 ether);
+        usdn.mint(address(this), initialDeposit);
         usdn.approve(address(usdnr), type(uint256).max);
+        usdnr.wrap(initialDeposit);
     }
 
     /**
-     * @custom:scenario Wrap USDN to USDNr
-     * @custom:when The wrap function is called with an amount of USDN
-     * @custom:then The user balance of USDNr increases by the same amount
-     * @custom:then The total supply of USDNr increases by the same amount
-     * @custom:then The total wrapped USDN increases by the same amount
+     * @custom:scenario Withdraw yield from the USDNr contract
+     * @custom:when The `withdrawYield` function is called by the owner
+     * @custom:then The yield is successfully withdrawn
      */
-    function test_usdnrWrap() public {
-        uint256 amount = 10 ether;
-        uint256 initialUsdnrBalance = usdnr.balanceOf(address(this));
-        uint256 initialUsdnrTotalSupply = usdnr.totalSupply();
-        uint256 usdnContractBalance = usdn.balanceOf(address(usdnr));
+    function test_withdrawYield() public {
+        usdn.rebase(usdn.divisor() * 90 / 100);
+        uint256 yield = usdn.sharesOf(address(usdnr)) / usdn.divisor() - initialDeposit;
+        assertGt(usdn.balanceOf(address(usdnr)), initialDeposit);
 
-        usdnr.wrap(amount);
+        usdnr.withdrawYield(address(this));
 
-        assertEq(usdnr.balanceOf(address(this)), initialUsdnrBalance + amount, "user USDNr balance");
-        assertEq(usdnr.totalSupply(), initialUsdnrTotalSupply + amount, "total USDNr supply");
-        assertEq(usdn.balanceOf(address(usdnr)), usdnContractBalance + amount, "USDN balance in USDNr");
+        assertEq(usdn.balanceOf(address(this)), yield);
+        assertEq(usdn.balanceOf(address(usdnr)), initialDeposit);
     }
 
     /**
-     * @custom:scenario Revert when the wrap function is called with zero amount
-     * @custom:when The wrap function is called with zero amount
-     * @custom:then The transaction should revert with the error {USDNrZeroAmount}
+     * @custom:scenario Withdraw yield to a specific recipient
+     * @custom:when The `withdrawYield` function is called by the owner with a specific recipient address
+     * @custom:then The yield is successfully withdrawn to the specified recipient
      */
-    function test_revertWhen_usdnrWrapZeroAmount() public {
-        vm.expectRevert(IUsdnr.USDNrZeroAmount.selector);
-        usdnr.wrap(0);
+    function test_withdrawYieldRecipient() public {
+        uint256 balanceBefore = usdn.balanceOf(address(this));
+        usdn.rebase(usdn.divisor() * 90 / 100);
+        uint256 yield = usdn.sharesOf(address(usdnr)) / usdn.divisor() - initialDeposit;
+        assertGt(usdn.balanceOf(address(usdnr)), initialDeposit);
+
+        usdnr.withdrawYield(address(1));
+
+        assertEq(usdn.balanceOf(address(1)), yield);
+        assertEq(usdn.balanceOf(address(this)), balanceBefore);
+        assertEq(usdn.balanceOf(address(usdnr)), initialDeposit);
+    }
+
+    /**
+     * @custom:scenario Revert when non-owner tries to withdraw yield
+     * @custom:when The `withdrawYield` function is called by a non-owner address
+     * @custom:then The transaction reverts with an "Ownable: caller is not the owner" error
+     */
+    function test_revertWhen_withdrawYieldNotOwner() public {
+        address nonOwnerUser = address(1);
+
+        vm.prank(nonOwnerUser);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, (nonOwnerUser)));
+        usdnr.withdrawYield(nonOwnerUser);
+    }
+
+    /**
+     * @custom:scenario Revert when trying to withdraw yield with no yield available
+     * @custom:when The `withdrawYield` function is called by the owner when there is no yield available
+     * @custom:then The transaction reverts with a "USDNrNoYield" error
+     */
+    function test_revertWhen_withdrawYieldNoYield() public {
+        assertEq(usdn.balanceOf(address(usdnr)), usdnr.totalSupply());
+        vm.expectRevert(IUsdnr.USDNrNoYield.selector);
+        usdnr.withdrawYield(address(this));
+    }
+
+    /**
+     * @custom:scenario Revert when trying to withdraw yield to the zero address
+     * @custom:when The `withdrawYield` function is called by the owner with the zero address as the recipient
+     * @custom:then The transaction reverts with a "USDNrZeroRecipient" error
+     */
+    function test_revertWhen_withdrawYieldZeroRecipient() public {
+        vm.expectRevert(IUsdnr.USDNrZeroRecipient.selector);
+        usdnr.withdrawYield(address(0));
     }
 }
