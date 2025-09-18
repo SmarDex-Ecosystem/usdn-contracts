d3e0b6291e38d79fa0d0fe95b0e9a1a4ab38a527
Alfred GAILLARD <a.gaillard@ra2.tech>
2025-09-18 14:16:54 +0200

    refactor: remove a broadcast line

--- script/fork/ForkCore.s.sol
+++ script/fork/ForkCore.s.sol
@@ -49,9 +49,8 @@ abstract contract ForkCore is Script {
         CHAINLINK_ETH_PRICE_FORK = _chainlinkPrice;
         PYTH_ETH_FEED_ID_FORK = _pythFeedId;
         CHAINLINK_PRICE_VALIDITY_FORK = _chainlinkPriceValidity;
-        vm.startBroadcast();
+        vm.broadcast();
         (, SENDER_BASE,) = vm.readCallers();
-        vm.stopBroadcast();
     }
 
     /**
