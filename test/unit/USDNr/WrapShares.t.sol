980e81afc50b052b617168f1ccce22a517126a09
samooyo <samidarnaud@gmail.com>
2025-09-19 15:23:35 +0200

    test(USDNr): wrap shares with recipient

--- test/unit/USDNr/WrapShares.t.sol
+++ test/unit/USDNr/WrapShares.t.sol
@@ -36,6 +36,38 @@ contract TestUsdnrWrapShares is UsdnrTokenFixture {
     }
 
     /**
+     * @custom:scenario Wrap USDN shares to another address
+     * @custom:when The wrapShares function is called with a recipient address
+     * @custom:then The recipient balance of USDNr increases by the amount
+     * @custom:and The total supply of USDNr increases by the amount
+     * @custom:and The user balance of USDN decreases by the amount
+     * @custom:and The total wrapped USDN increases by the amount
+     */
+    function test_wrapSharesToAnotherAddress() public {
+        uint256 amount = 10 ether;
+        uint256 sharesAmount = usdn.convertToShares(amount);
+        address recipient = address(1);
+
+        uint256 initialUsdnrBalance = usdnr.balanceOf(address(this));
+        uint256 initialUsdnrRecipientBalance = usdnr.balanceOf(recipient);
+        uint256 initialUsdnrTotalSupply = usdnr.totalSupply();
+
+        uint256 initialUsdnBalance = usdn.sharesOf(address(this));
+        uint256 initialUsdnContractBalance = usdn.sharesOf(address(usdnr));
+
+        usdnr.wrapShares(sharesAmount, recipient);
+
+        assertEq(usdnr.balanceOf(address(this)), initialUsdnrBalance, "user USDNr balance");
+        assertEq(usdnr.balanceOf(recipient), initialUsdnrRecipientBalance + amount, "recipient USDNr balance");
+        assertEq(usdnr.totalSupply(), initialUsdnrTotalSupply + amount, "total USDNr supply");
+
+        assertEq(
+            usdn.sharesOf(address(usdnr)), initialUsdnContractBalance + sharesAmount, "USDN shares balance in USDNr"
+        );
+        assertEq(usdn.sharesOf(address(this)), initialUsdnBalance - sharesAmount, "user USDN shares balance");
+    }
+
+    /**
      * @custom:scenario Revert when the wrapShares function is called with zero amount
      * @custom:when The wrapShares function is called with zero amount
      * @custom:then The transaction should revert with the error {USDNrZeroAmount}
