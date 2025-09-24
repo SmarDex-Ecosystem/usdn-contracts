dca9981f101e9f65a8110da395b8faf2679383bc
Sam <46531012+samooyo@users.noreply.github.com>
2025-09-24 14:18:53 +0200

    feat(USDNr): add deployment script (#936)
    * feat: add the USDNr contract
    
    * docs: natspecs
    
    * test(USDNr): wrap function
    
    * test(usdnr): unwrap function
    
    * style: typo
    
    * refactor(USDNr): remove totalWrapped
    
    * feat(USDNr): add deployment script
    
    * docs: add deployment natspec
    
    * refactor(USDnr): return the deployed contract in the script
    
    * docs: update scripts README

---
+++ script/DeployUsdnr.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { Script } from "forge-std/Script.sol";
+
+import { Usdnr } from "../src/Usdn/Usdnr.sol";
+import { IUsdn } from "../src/interfaces/Usdn/IUsdn.sol";
+
+contract DeployUsdnr is Script {
+    /**
+     * @notice Deploy the USDNr contract
+     * @param usdn The address of the USDN contract
+     * @return usdnr_ The address of the deployed USDNr contract
+     */
+    function run(IUsdn usdn, address owner) external returns (Usdnr usdnr_) {
+        vm.broadcast();
+        usdnr_ = new Usdnr(usdn, owner);
+    }
+}
