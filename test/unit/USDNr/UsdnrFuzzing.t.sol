2d8a1480baed64e0a10666fd3e71cc1e3e58c2af
fireboss777 <fireboss@tutanota.com>
2025-09-24 17:39:57 +0200

    test: refactor params

--- test/unit/USDNr/UsdnrFuzzing.t.sol
+++ test/unit/USDNr/UsdnrFuzzing.t.sol
@@ -12,14 +12,14 @@ contract TestUsdnrFuzzing is UsdnrTokenFixture {
      * @custom:when The user unwrap their usdnr to usdn
      * @custom:then The user's usdn balance must be equal to the initial usdn balance
      * @custom:and The user's usdnr balance must be zero
-     * @param initialUsdnBalance The initial usdn balance to mint to the user
      * @param seed0 The seed to bound the first divisor
-     * @param seed1 The seed to bound the second divisor
+     * @param seed1 The seed to bound the initial usdn balance to mint to the user
+     * @param seed2 The seed to bound the second divisor
      */
-    function testFuzz_wrapRebaseUnwrap(uint256 initialUsdnBalance, uint256 seed0, uint256 seed1) public {
+    function testFuzz_wrapRebaseUnwrap(uint256 seed0, uint256 seed1, uint256 seed2) public {
         uint256 newDivisor = _bound(seed0, usdn.MIN_DIVISOR(), usdn.MAX_DIVISOR());
         usdn.rebase(newDivisor);
-        initialUsdnBalance = _bound(initialUsdnBalance, 1, usdn.maxTokens());
+        uint256 initialUsdnBalance = _bound(seed1, 1, usdn.maxTokens());
         usdn.mint(address(this), initialUsdnBalance);
 
         /* -------------------------------------------------------------------------- */
@@ -43,7 +43,7 @@ contract TestUsdnrFuzzing is UsdnrTokenFixture {
         /*                                   REBASE                                   */
         /* -------------------------------------------------------------------------- */
 
-        newDivisor = _bound(seed1, usdn.MIN_DIVISOR(), usdn.divisor());
+        newDivisor = _bound(seed2, usdn.MIN_DIVISOR(), usdn.divisor());
         usdn.rebase(newDivisor);
 
         /* -------------------------------------------------------------------------- */
